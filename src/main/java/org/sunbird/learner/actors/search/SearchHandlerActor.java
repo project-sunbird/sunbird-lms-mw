8ce54c3ef0b95457f52cdab82c63a3a8bc8be5a9
manzarul.haque <manzarul.haque>
2017-07-10 15:20:34 +0530

    issue #356 feat: chnaegs for composite search

---
+++ src/main/java/org/sunbird/learner/actors/search/SearchHandlerActor.java
@@ -0,0 +1,66 @@
+package org.sunbird.learner.actors.search;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.sunbird.common.ElasticSearchUtil;
+import org.sunbird.common.exception.ProjectCommonException;
+import org.sunbird.common.models.response.Response;
+import org.sunbird.common.models.util.ActorOperations;
+import org.sunbird.common.models.util.JsonKey;
+import org.sunbird.common.models.util.LogHelper;
+import org.sunbird.common.models.util.ProjectUtil;
+import org.sunbird.common.request.Request;
+import org.sunbird.common.responsecode.ResponseCode;
+import org.sunbird.dto.SearchDTO;
+import org.sunbird.learner.util.Util;
+
+import akka.actor.UntypedAbstractActor;
+
+/**
+ * This class will handle search operation for all different type
+ * of index and types
+ * @author Manzarul
+ *
+ */
+
+public class SearchHandlerActor extends UntypedAbstractActor {
+    private LogHelper logger = LogHelper.getInstance(SearchHandlerActor.class.getName());
+
+	@Override
+	public void onReceive(Object message) throws Throwable {
+		 if (message instanceof Request) {
+	            logger.debug("CompositeSearch  onReceive called");
+	            Request actorMessage = (Request) message;
+	            if (actorMessage.getOperation().equalsIgnoreCase(ActorOperations.COMPOSITE_SEARCH.getValue())) {
+	              Map<String , Object> searchQueryMap = actorMessage.getRequest();
+	              Object objectType = searchQueryMap.get(JsonKey.OBJECT_TYPE);
+	              String [] types ;
+	              if(objectType != null && objectType instanceof List) {
+ 	                  List<String> list =(List)objectType;
+ 	                  types = list.toArray(new String[list.size()]);
+	               }
+	              SearchDTO searchDto = Util.createSearchDto(searchQueryMap);
+                  Map<String, Object> result = null; //ElasticSearchUtil.(searchDto, ProjectUtil.EsIndex.sunbird, types);
+                  Response response = new Response();
+                  if(result !=null) {
+                  response.put(JsonKey.RESPONSE, result);
+                  } else {
+                       result = new HashMap<String, Object>();
+                       response.put(JsonKey.RESPONSE, result);    
+                  }
+                  sender().tell(response, self());
+	            } else {
+	                logger.info("UNSUPPORTED OPERATION");
+	                ProjectCommonException exception = new ProjectCommonException(ResponseCode.invalidOperationName.getErrorCode(), ResponseCode.invalidOperationName.getErrorMessage(), ResponseCode.CLIENT_ERROR.getResponseCode());
+	                sender().tell(exception, self());
+	            }
+	        } else {
+	            logger.info("UNSUPPORTED MESSAGE");
+	            ProjectCommonException exception = new ProjectCommonException(ResponseCode.invalidRequestData.getErrorCode(), ResponseCode.invalidRequestData.getErrorMessage(), ResponseCode.CLIENT_ERROR.getResponseCode());
+	            sender().tell(exception, self());
+	        }		
+	}
+
+}

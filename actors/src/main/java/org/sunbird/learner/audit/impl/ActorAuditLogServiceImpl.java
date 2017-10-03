8cae3541d90081af945c6624e1290503da4d1935
vijethas <vijetha.nayak@riflexions.com>
2017-10-03 14:17:31 +0530

    Issue #SB-131 feat:Initial commit for audit Log

---
+++ actors/src/main/java/org/sunbird/learner/audit/impl/ActorAuditLogServiceImpl.java
@@ -0,0 +1,19 @@
+package org.sunbird.learner.audit.impl;
+
+import java.util.Map;
+
+import org.sunbird.learner.audit.AuditLogService;
+
+public class ActorAuditLogServiceImpl implements AuditLogService{
+
+  @Override
+  public void process(Map<String, Object> requestedData) {
+    
+  }
+
+  @Override
+  public void save(Map<String, Object> requestedData) {
+    
+  }
+
+}
---
+++ actors/src/main/java/org/sunbird/learner/audit/impl/AuditLogGenerator.java
@@ -0,0 +1,11 @@
+package org.sunbird.learner.audit.impl;
+
+import java.util.Map;
+
+public class AuditLogGenerator {
+  
+  public Map<String,Object> generateLogs(Map<String, Object> requestData){
+    return requestData;
+  }
+
+}

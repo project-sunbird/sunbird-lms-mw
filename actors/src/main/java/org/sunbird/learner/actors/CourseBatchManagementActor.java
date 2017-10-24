77d2f25b4b997a809937ce3baad6a215be36e898
AMIT KUMAR <amit.kumar@tarento.com>
2017-10-24 12:51:02 +0530

    Issue #SB-367 feat:test case changes

--- actors/src/main/java/org/sunbird/learner/actors/CourseBatchManagementActor.java
+++ actors/src/main/java/org/sunbird/learner/actors/CourseBatchManagementActor.java
@@ -5,7 +5,6 @@ import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;

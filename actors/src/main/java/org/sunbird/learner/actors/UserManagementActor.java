749faa2b11e9961c4deaf143329c93e60c59bd0a
vijethas <vijetha.nayak@riflexions.com>
2017-11-07 15:53:03 +0530

    Issue #SB-302 feat:Profile Visibility changes

--- actors/src/main/java/org/sunbird/learner/actors/UserManagementActor.java
+++ actors/src/main/java/org/sunbird/learner/actors/UserManagementActor.java
@@ -201,14 +201,13 @@ public class UserManagementActor extends UntypedAbstractActor {
         }
       }
     }
-    Map<String,String> privateFieldMap = new HashMap<>();
+    Map<String,String> privateFieldMap = (Map<String, String>) esResult.get(JsonKey.PROFILE_VISIBILITY);
     if (privateList != null) {
       for (String key : privateList) {
         privateFieldMap.put(key, JsonKey.PRIVATE);
       }
     }
-    if (publicList != null && privateFieldMap.isEmpty()) {
-      privateFieldMap = (Map<String, String>) esResult.get(JsonKey.PROFILE_VISIBILITY);
+    if (publicList != null) {
       for (String key : publicList) {
         privateFieldMap.remove(key);
       }

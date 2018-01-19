4723cfa8768f8f966b1204518a07a21a39f76fc3
AMIT KUMAR <amit.kumar@tarento.com>
2018-01-19 12:18:56 +0530

    SB-1305 feat: code cleanup

--- actors/src/test/java/org/sunbird/learner/RemoteActorSystemTest.java
+++ actors/src/test/java/org/sunbird/learner/RemoteActorSystemTest.java
@@ -11,23 +11,23 @@ import org.sunbird.learner.util.actorutility.impl.RemoteActorSystem;
 
 public class RemoteActorSystemTest {
 
-  
-  private static Class t  = null;    
+
+  private static Class t = null;
+
   @BeforeClass
   public static void setUp() {
     try {
-      t  = Class.forName("org.sunbird.learner.Application");
+      t = Class.forName("org.sunbird.learner.Application");
     } catch (ClassNotFoundException e) {
       ProjectLogger.log(e.getMessage(), e);
     }
   }
-  
+
   @Test
   public void instanceCreationTest() {
-    Exception exp = null ;
+    Exception exp = null;
     try {
-      Constructor<Application> constructor =
-          t.getDeclaredConstructor();
+      Constructor<Application> constructor = t.getDeclaredConstructor();
       constructor.setAccessible(true);
       Application application = constructor.newInstance();
       Assert.assertNotNull(application);
@@ -36,23 +36,23 @@ public class RemoteActorSystemTest {
     }
     Assert.assertNull(exp);
   }
-  
+
   @Test
-  public void remoteActorTest () {
-    Exception exp = null ;
+  public void remoteActorTest() {
+    Exception exp = null;
     try {
       t.getDeclaredMethod("startRemoteActorSystem");
     } catch (Exception e) {
-    exp = e;
-   }
-    Assert.assertNull(exp);  
+      exp = e;
+    }
+    Assert.assertNull(exp);
   }
-  
-  @Test 
-  public void remoteActorTest1 () {
+
+  @Test
+  public void remoteActorTest1() {
     ActorSystemFactory.createRemoteActorSystem();
     Object obj = ActorSystemFactory.getActorSystem();
     Assert.assertTrue(obj instanceof RemoteActorSystem);
   }
-  
+
 }
--- actors/src/test/java/org/sunbird/metrics/actors/CourseMetricsActorTest.java
+++ actors/src/test/java/org/sunbird/metrics/actors/CourseMetricsActorTest.java
@@ -30,7 +30,7 @@ public class CourseMetricsActorTest {
   private static ActorSystem system;
   private static final Props props = Props.create(CourseMetricsActor.class);
   private static String userId = "dnk298voopir80249";
-  private static String batchId ="jkwf6t3r083fp4h";
+  private static String batchId = "jkwf6t3r083fp4h";
   private static final String orgId = "vdckcyigc68569";
 
   @BeforeClass
@@ -43,211 +43,221 @@ public class CourseMetricsActorTest {
     insertUserDataToES();
   }
 
-  private static void insertUserDataToES(){
-    Map<String , Object> userMap = new HashMap<>();
-    userMap.put(JsonKey.USER_ID , userId);
-    userMap.put(JsonKey.FIRST_NAME , "alpha");
-    userMap.put(JsonKey.ID , userId);
+  private static void insertUserDataToES() {
+    Map<String, Object> userMap = new HashMap<>();
+    userMap.put(JsonKey.USER_ID, userId);
+    userMap.put(JsonKey.FIRST_NAME, "alpha");
+    userMap.put(JsonKey.ID, userId);
     userMap.put(JsonKey.ROOT_ORG_ID, "ORG_001");
-    userMap.put(JsonKey.USERNAME , "alpha-beta");
+    userMap.put(JsonKey.USERNAME, "alpha-beta");
     userMap.put(JsonKey.REGISTERED_ORG_ID, orgId);
-    userMap.put(JsonKey.EMAIL , "arvind.glaitm108@gmail.com");
-    ElasticSearchUtil.createData(EsIndex.sunbird.getIndexName(),EsType.user.getTypeName() , userId , userMap);
+    userMap.put(JsonKey.EMAIL, "arvind.glaitm108@gmail.com");
+    ElasticSearchUtil.createData(EsIndex.sunbird.getIndexName(), EsType.user.getTypeName(), userId,
+        userMap);
   }
 
-  private static void insertBatchDataToES(){
-    Map<String , Object> batchMap = new HashMap<>();
-    batchMap.put(JsonKey.ID , batchId);
-    ElasticSearchUtil.createData(EsIndex.sunbird.getIndexName(),EsType.course.getTypeName() , batchId , batchMap);
+  private static void insertBatchDataToES() {
+    Map<String, Object> batchMap = new HashMap<>();
+    batchMap.put(JsonKey.ID, batchId);
+    ElasticSearchUtil.createData(EsIndex.sunbird.getIndexName(), EsType.course.getTypeName(),
+        batchId, batchMap);
   }
 
-  private static void insertUserCoursesDataToES(){
-    Map<String , Object> userCoursesMap = new HashMap<>();
-    userCoursesMap.put(JsonKey.ID , batchId+JsonKey.PRIMARY_KEY_DELIMETER+userId);
-    userCoursesMap.put(JsonKey.BATCH_ID , batchId);
+  private static void insertUserCoursesDataToES() {
+    Map<String, Object> userCoursesMap = new HashMap<>();
+    userCoursesMap.put(JsonKey.ID, batchId + JsonKey.PRIMARY_KEY_DELIMETER + userId);
+    userCoursesMap.put(JsonKey.BATCH_ID, batchId);
     userCoursesMap.put(JsonKey.USER_ID, userId);
-    userCoursesMap.put(JsonKey.REGISTERED_ORG_ID , orgId);
-    userCoursesMap.put(JsonKey.PROGRESS , 1);
-    ElasticSearchUtil.createData(EsIndex.sunbird.getIndexName(),EsType.usercourses.getTypeName() , batchId+JsonKey.PRIMARY_KEY_DELIMETER+userId , userCoursesMap);
+    userCoursesMap.put(JsonKey.REGISTERED_ORG_ID, orgId);
+    userCoursesMap.put(JsonKey.PROGRESS, 1);
+    ElasticSearchUtil.createData(EsIndex.sunbird.getIndexName(), EsType.usercourses.getTypeName(),
+        batchId + JsonKey.PRIMARY_KEY_DELIMETER + userId, userCoursesMap);
   }
 
-  private static void insertOrgDataToES(){
-    Map<String , Object> orgMap = new HashMap<>();
-    orgMap.put(JsonKey.ID , orgId);
-    orgMap.put(JsonKey.ORGANISATION_NAME , "IIM");
-    orgMap.put(JsonKey.ORGANISATION_ID , orgId);
-    ElasticSearchUtil.createData(EsIndex.sunbird.getIndexName(),EsType.organisation.getTypeName() , batchId , orgMap);
+  private static void insertOrgDataToES() {
+    Map<String, Object> orgMap = new HashMap<>();
+    orgMap.put(JsonKey.ID, orgId);
+    orgMap.put(JsonKey.ORGANISATION_NAME, "IIM");
+    orgMap.put(JsonKey.ORGANISATION_ID, orgId);
+    ElasticSearchUtil.createData(EsIndex.sunbird.getIndexName(), EsType.organisation.getTypeName(),
+        batchId, orgMap);
   }
 
   @Test
-  public void testCourseProgress(){
+  public void testCourseProgress() {
 
     TestKit probe = new TestKit(system);
     ActorRef subject = system.actorOf(props);
 
     Request actorMessage = new Request();
-    actorMessage.put(JsonKey.REQUESTED_BY , userId);
-    actorMessage.put(JsonKey.BATCH_ID , batchId);
-    actorMessage.put(JsonKey.PERIOD , "fromBegining");
+    actorMessage.put(JsonKey.REQUESTED_BY, userId);
+    actorMessage.put(JsonKey.BATCH_ID, batchId);
+    actorMessage.put(JsonKey.PERIOD, "fromBegining");
     actorMessage.setOperation(ActorOperations.COURSE_PROGRESS_METRICS.getValue());
 
     subject.tell(actorMessage, probe.getRef());
-    Response res = probe.expectMsgClass(duration("100 second"),Response.class);
+    Response res = probe.expectMsgClass(duration("100 second"), Response.class);
     Assert.assertTrue(null != res.get(JsonKey.RESPONSE));
   }
 
   @Test
-  public void testUnsupportedMessageType(){
+  public void testUnsupportedMessageType() {
 
     TestKit probe = new TestKit(system);
     ActorRef subject = system.actorOf(props);
 
     subject.tell("Invalid Oject Type", probe.getRef());
-    ProjectCommonException exc  = probe.expectMsgClass(duration("100 second"),ProjectCommonException.class);
+    ProjectCommonException exc =
+        probe.expectMsgClass(duration("100 second"), ProjectCommonException.class);
     Assert.assertTrue(null != exc);
   }
 
   @Test
-  public void testCourseProgress001(){
+  public void testCourseProgress001() {
 
     TestKit probe = new TestKit(system);
     ActorRef subject = system.actorOf(props);
 
     Request actorMessage = new Request();
-    actorMessage.put(JsonKey.REQUESTED_BY , userId);
-    actorMessage.put(JsonKey.BATCH_ID , batchId);
-    actorMessage.put(JsonKey.PERIOD , "7d");
+    actorMessage.put(JsonKey.REQUESTED_BY, userId);
+    actorMessage.put(JsonKey.BATCH_ID, batchId);
+    actorMessage.put(JsonKey.PERIOD, "7d");
     actorMessage.setOperation(ActorOperations.COURSE_PROGRESS_METRICS.getValue());
 
     subject.tell(actorMessage, probe.getRef());
-    Response res = probe.expectMsgClass(duration("100 second"),Response.class);
+    Response res = probe.expectMsgClass(duration("100 second"), Response.class);
     Assert.assertTrue(null != res.get(JsonKey.RESPONSE));
   }
 
   @Test
-  public void testCourseProgressWithInvalidBatch(){
+  public void testCourseProgressWithInvalidBatch() {
 
     TestKit probe = new TestKit(system);
     ActorRef subject = system.actorOf(props);
 
     Request actorMessage = new Request();
-    actorMessage.put(JsonKey.REQUESTED_BY , userId);
-    actorMessage.put(JsonKey.BATCH_ID , batchId+"8ud38cuy");
-    actorMessage.put(JsonKey.PERIOD , "fromBegining");
+    actorMessage.put(JsonKey.REQUESTED_BY, userId);
+    actorMessage.put(JsonKey.BATCH_ID, batchId + "8ud38cuy");
+    actorMessage.put(JsonKey.PERIOD, "fromBegining");
     actorMessage.setOperation(ActorOperations.COURSE_PROGRESS_METRICS.getValue());
 
     subject.tell(actorMessage, probe.getRef());
-    ProjectCommonException exc = probe.expectMsgClass(duration("100 second"),ProjectCommonException.class);
+    ProjectCommonException exc =
+        probe.expectMsgClass(duration("100 second"), ProjectCommonException.class);
     Assert.assertTrue(null != exc);
   }
 
   @Test
-  public void testCourseProgressWithInvalidBatchIdNull(){
+  public void testCourseProgressWithInvalidBatchIdNull() {
 
     TestKit probe = new TestKit(system);
     ActorRef subject = system.actorOf(props);
 
     Request actorMessage = new Request();
-    actorMessage.put(JsonKey.REQUESTED_BY , userId);
-    actorMessage.put(JsonKey.BATCH_ID , null);
-    actorMessage.put(JsonKey.PERIOD , "fromBegining");
+    actorMessage.put(JsonKey.REQUESTED_BY, userId);
+    actorMessage.put(JsonKey.BATCH_ID, null);
+    actorMessage.put(JsonKey.PERIOD, "fromBegining");
     actorMessage.setOperation(ActorOperations.COURSE_PROGRESS_METRICS.getValue());
 
     subject.tell(actorMessage, probe.getRef());
-    ProjectCommonException exc = probe.expectMsgClass(duration("100 second"),ProjectCommonException.class);
+    ProjectCommonException exc =
+        probe.expectMsgClass(duration("100 second"), ProjectCommonException.class);
     Assert.assertTrue(null != exc);
   }
 
   @Test
-  public void testCourseProgressWithInvalidOperationName(){
+  public void testCourseProgressWithInvalidOperationName() {
 
     TestKit probe = new TestKit(system);
     ActorRef subject = system.actorOf(props);
 
     Request actorMessage = new Request();
-    actorMessage.put(JsonKey.REQUESTED_BY , userId);
-    actorMessage.put(JsonKey.BATCH_ID , null);
-    actorMessage.put(JsonKey.PERIOD , "fromBegining");
-    actorMessage.setOperation(ActorOperations.COURSE_PROGRESS_METRICS.getValue()+"-Invalid");
+    actorMessage.put(JsonKey.REQUESTED_BY, userId);
+    actorMessage.put(JsonKey.BATCH_ID, null);
+    actorMessage.put(JsonKey.PERIOD, "fromBegining");
+    actorMessage.setOperation(ActorOperations.COURSE_PROGRESS_METRICS.getValue() + "-Invalid");
 
     subject.tell(actorMessage, probe.getRef());
-    ProjectCommonException exc = probe.expectMsgClass(duration("100 second"),ProjectCommonException.class);
+    ProjectCommonException exc =
+        probe.expectMsgClass(duration("100 second"), ProjectCommonException.class);
     Assert.assertTrue(null != exc);
   }
 
   @Test
-  public void testCourseProgressReport(){
+  public void testCourseProgressReport() {
 
     TestKit probe = new TestKit(system);
     ActorRef subject = system.actorOf(props);
 
     Request actorMessage = new Request();
-    actorMessage.put(JsonKey.REQUESTED_BY , userId);
-    actorMessage.put(JsonKey.BATCH_ID , batchId);
-    actorMessage.put(JsonKey.PERIOD , "fromBegining");
+    actorMessage.put(JsonKey.REQUESTED_BY, userId);
+    actorMessage.put(JsonKey.BATCH_ID, batchId);
+    actorMessage.put(JsonKey.PERIOD, "fromBegining");
     actorMessage.put(JsonKey.FORMAT, "csv");
     actorMessage.setOperation(ActorOperations.COURSE_PROGRESS_METRICS_REPORT.getValue());
 
     subject.tell(actorMessage, probe.getRef());
-    Response res = probe.expectMsgClass(duration("100 second"),Response.class);
+    Response res = probe.expectMsgClass(duration("100 second"), Response.class);
     Assert.assertTrue(null != res.get(JsonKey.RESPONSE));
   }
 
   @Test
-  public void testCourseProgressReport001(){
+  public void testCourseProgressReport001() {
 
     TestKit probe = new TestKit(system);
     ActorRef subject = system.actorOf(props);
 
     Request actorMessage = new Request();
-    actorMessage.put(JsonKey.REQUESTED_BY , userId);
-    actorMessage.put(JsonKey.BATCH_ID , batchId);
-    actorMessage.put(JsonKey.PERIOD , "7d");
+    actorMessage.put(JsonKey.REQUESTED_BY, userId);
+    actorMessage.put(JsonKey.BATCH_ID, batchId);
+    actorMessage.put(JsonKey.PERIOD, "7d");
     actorMessage.put(JsonKey.FORMAT, "csv");
     actorMessage.setOperation(ActorOperations.COURSE_PROGRESS_METRICS_REPORT.getValue());
 
     subject.tell(actorMessage, probe.getRef());
-    Response res = probe.expectMsgClass(duration("100 second"),Response.class);
+    Response res = probe.expectMsgClass(duration("100 second"), Response.class);
     Assert.assertTrue(null != res.get(JsonKey.RESPONSE));
   }
 
   @Test
-  public void testCourseProgressReportWithInvalidBatch(){
+  public void testCourseProgressReportWithInvalidBatch() {
 
     TestKit probe = new TestKit(system);
     ActorRef subject = system.actorOf(props);
 
     Request actorMessage = new Request();
-    actorMessage.put(JsonKey.REQUESTED_BY , userId);
-    actorMessage.put(JsonKey.BATCH_ID , batchId+"ug8er73");
-    actorMessage.put(JsonKey.PERIOD , "fromBegining");
+    actorMessage.put(JsonKey.REQUESTED_BY, userId);
+    actorMessage.put(JsonKey.BATCH_ID, batchId + "ug8er73");
+    actorMessage.put(JsonKey.PERIOD, "fromBegining");
     actorMessage.put(JsonKey.FORMAT, "csv");
     actorMessage.setOperation(ActorOperations.COURSE_PROGRESS_METRICS_REPORT.getValue());
 
     subject.tell(actorMessage, probe.getRef());
-    ProjectCommonException exc = probe.expectMsgClass(duration("100 second"),ProjectCommonException.class);
+    ProjectCommonException exc =
+        probe.expectMsgClass(duration("100 second"), ProjectCommonException.class);
     Assert.assertTrue(null != exc);
 
   }
 
   @Test
-  public void testCourseProgressReportWithInvalidBatchIdNull(){
+  public void testCourseProgressReportWithInvalidBatchIdNull() {
 
     TestKit probe = new TestKit(system);
     ActorRef subject = system.actorOf(props);
 
     Request actorMessage = new Request();
-    actorMessage.put(JsonKey.REQUESTED_BY , userId);
-    actorMessage.put(JsonKey.BATCH_ID , "");
-    actorMessage.put(JsonKey.PERIOD , "fromBegining");
+    actorMessage.put(JsonKey.REQUESTED_BY, userId);
+    actorMessage.put(JsonKey.BATCH_ID, "");
+    actorMessage.put(JsonKey.PERIOD, "fromBegining");
     actorMessage.put(JsonKey.FORMAT, "csv");
     actorMessage.setOperation(ActorOperations.COURSE_PROGRESS_METRICS_REPORT.getValue());
 
     subject.tell(actorMessage, probe.getRef());
-    ProjectCommonException exc = probe.expectMsgClass(duration("100 second"),ProjectCommonException.class);
+    ProjectCommonException exc =
+        probe.expectMsgClass(duration("100 second"), ProjectCommonException.class);
     Assert.assertTrue(null != exc);
   }
-  
+
   @SuppressWarnings({"unchecked", "deprecation"})
   @Test
   public void testCourseConsumption() {
@@ -257,14 +267,15 @@ public class CourseMetricsActorTest {
     Request actorMessage = new Request();
     actorMessage.put(JsonKey.COURSE_ID, "mclr309f39");
     actorMessage.put(JsonKey.PERIOD, "7d");
-    actorMessage.put(JsonKey.REQUESTED_BY , userId);
+    actorMessage.put(JsonKey.REQUESTED_BY, userId);
     actorMessage.setOperation(ActorOperations.COURSE_CREATION_METRICS.getValue());
 
     subject.tell(actorMessage, probe.getRef());
     Response res = probe.expectMsgClass(duration("1500 second"), Response.class);
     Map<String, Object> data = res.getResult();
     Assert.assertEquals("7d", data.get(JsonKey.PERIOD));
-    Assert.assertEquals("mclr309f39", ((Map<String, Object>) data.get("course")).get(JsonKey.COURSE_ID));
+    Assert.assertEquals("mclr309f39",
+        ((Map<String, Object>) data.get("course")).get(JsonKey.COURSE_ID));
     Map<String, Object> series = (Map<String, Object>) data.get(JsonKey.SERIES);
     Assert.assertTrue(series.containsKey("course.consumption.time_spent"));
     Assert.assertTrue(series.containsKey("course.consumption.content.users.count"));
@@ -278,7 +289,7 @@ public class CourseMetricsActorTest {
     Assert.assertTrue(snapshot.containsKey("course.consumption.time_spent_completion_count"));
   }
 
-  
+
   @SuppressWarnings("deprecation")
   @Test
   public void testCourseConsumptionInvalidUserData() {
@@ -288,14 +299,15 @@ public class CourseMetricsActorTest {
     Request actorMessage = new Request();
     actorMessage.put(JsonKey.COURSE_ID, "mclr309f39_INVALID");
     actorMessage.put(JsonKey.PERIOD, "7d");
-    actorMessage.put(JsonKey.REQUESTED_BY , userId + "Invalid");
+    actorMessage.put(JsonKey.REQUESTED_BY, userId + "Invalid");
     actorMessage.setOperation(ActorOperations.COURSE_CREATION_METRICS.getValue());
 
     subject.tell(actorMessage, probe.getRef());
-    ProjectCommonException e = probe.expectMsgClass(duration("10 second"), ProjectCommonException.class);
+    ProjectCommonException e =
+        probe.expectMsgClass(duration("10 second"), ProjectCommonException.class);
     Assert.assertEquals("UNAUTHORIZE_USER", e.getCode());
   }
-  
+
   @SuppressWarnings("deprecation")
   @Test
   public void testCourseConsumptionInvalidPeriod() {
@@ -308,7 +320,8 @@ public class CourseMetricsActorTest {
     actorMessage.setOperation(ActorOperations.COURSE_CREATION_METRICS.getValue());
 
     subject.tell(actorMessage, probe.getRef());
-    ProjectCommonException e = probe.expectMsgClass(duration("10 second"), ProjectCommonException.class);
+    ProjectCommonException e =
+        probe.expectMsgClass(duration("10 second"), ProjectCommonException.class);
     Assert.assertEquals("INVALID_PERIOD", e.getCode());
   }
 

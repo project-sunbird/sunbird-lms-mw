dcc42b53e850c97eaf6188f7d808c024f4d34a24
manzarul <manzarul07@gmail.com>
2018-03-12 15:42:02 +0530

    issues #SB-2078 feat: adding interface to make call for badger server from actor

---
+++ actors/core/src/main/java/org/sunbird/learner/actors/badges/service/impl/BadgrServiceImpl.java
@@ -0,0 +1,86 @@
+package org.sunbird.learner.actors.badges.service.impl;
+
+import org.sunbird.common.models.response.Response;
+import org.sunbird.common.request.Request;
+import org.sunbird.learner.actors.badges.service.BadgingService;
+
+/**
+ * 
+ * @author Manzarul
+ *
+ */
+public class BadgrServiceImpl implements BadgingService {
+
+	@Override
+	public Response createIssuer(Request request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response getIssuerDetails(Request request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response getIssuerList(Request request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response removeIssuer(Request request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response createBadgeClass(Request request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response getBadgeClassDetails(Request request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response getBadgeClassList(Request request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response removeBadgeClass(Request request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response badgeAssertion(Request request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response getAssertionDetails(Request request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response getAssertionList(Request request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response revokeAssertion(Request request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

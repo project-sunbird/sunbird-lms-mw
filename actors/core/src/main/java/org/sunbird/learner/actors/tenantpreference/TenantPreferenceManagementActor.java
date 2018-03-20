226b72f1c8e16bc49c4ecc2c9ed91c695a596cda
Rayulu Villa <rayulu@Rayulus-MacBook-Pro.local>
2018-03-20 19:58:30 +0530

    Issue #SB-1779 : implement tenant preferences API

--- actors/core/src/main/java/org/sunbird/learner/actors/tenantpreference/TenantPreferenceManagementActor.java
+++ actors/core/src/main/java/org/sunbird/learner/actors/tenantpreference/TenantPreferenceManagementActor.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.sunbird.actor.core.BaseActor;
 import org.sunbird.actor.router.ActorConfig;
 import org.sunbird.cassandra.CassandraOperation;
@@ -24,7 +25,7 @@ import org.sunbird.helper.ServiceFactory;
 import org.sunbird.learner.util.Util;
 
 /**
- * Class for T&C . Created by arvind on 27/10/17.
+ * Class for Tenant preferences . Created by arvind on 27/10/17.
  */
 
 @ActorConfig(tasks = { "createTanentPreference", "updateTenantPreference", "getTenantPreference",
@@ -36,6 +37,7 @@ public class TenantPreferenceManagementActor extends BaseActor {
 	private Util.DbInfo userDbInfo = Util.dbInfoMap.get(JsonKey.USER_DB);
 	private Util.DbInfo orgDbInfo = Util.dbInfoMap.get(JsonKey.ORG_DB);
 	private SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+	private static final String DEFAULT_WILDCARD_ORG_ID = "*";
 
 	@Override
 	public void onReceive(Request request) throws Throwable {
@@ -53,195 +55,204 @@ public class TenantPreferenceManagementActor extends BaseActor {
 	}
 
 	/**
-	 * Method to get the all Tenal preference of the given root org id .
+	 * Method to get Tenant preference of the given root org id .
 	 * 
 	 * @param actorMessage
 	 */
+	@SuppressWarnings("unchecked")
 	private void getTenantPreference(Request actorMessage) {
 
-		ProjectLogger.log("TenantPreferenceManagementActor-createTenantPreference called");
-
 		String orgId = (String) actorMessage.getRequest().get(JsonKey.ORG_ID);
+		validateRequest(actorMessage);
+		ProjectLogger.log("TenantPreferenceManagementActor-getTenantPreference called for org: " + orgId);
+		List<String> keys = (List<String>) actorMessage.getRequest().get(JsonKey.KEYS);
 
-		if (ProjectUtil.isStringNullOREmpty(orgId)) {
-			// throw invalid ord id ,org id should not be null or empty .
-			throw new ProjectCommonException(ResponseCode.invalidOrgId.getErrorCode(),
-					ResponseCode.invalidOrgId.getErrorMessage(), ResponseCode.CLIENT_ERROR.getResponseCode());
-		}
-
+		Map<String, Map<String, Object>> orgPrefMap = getPreferenceMap(getPreferencesFromDB(orgId));
+		Map<String, Map<String, Object>> defaultPrefMap = getPreferenceMap(getPreferencesFromDB(DEFAULT_WILDCARD_ORG_ID));
+		
 		Response finalResponse = new Response();
-
-		Response response1 = cassandraOperation.getRecordsByProperty(tenantPreferenceDbInfo.getKeySpace(),
-				tenantPreferenceDbInfo.getTableName(), JsonKey.ORG_ID, orgId);
-		List<Map<String, Object>> list = (List<Map<String, Object>>) response1.get(JsonKey.RESPONSE);
-
-		if (list.isEmpty()) {
-			// throw exception org does not exist...
-			throw new ProjectCommonException(ResponseCode.invalidOrgId.getErrorCode(),
-					ResponseCode.invalidOrgId.getErrorMessage(), ResponseCode.CLIENT_ERROR.getResponseCode());
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		if (null != keys && !keys.isEmpty()) {
+			for (String key : keys) {
+				if (null != orgPrefMap.get(key))
+					list.add(orgPrefMap.get(key));
+				else if (null != defaultPrefMap.get(key))
+					list.add(defaultPrefMap.get(key));
+			}
+		} else {
+			if (!orgPrefMap.isEmpty())
+				list.addAll(orgPrefMap.values());
+			else if (!defaultPrefMap.isEmpty())
+				list.addAll(defaultPrefMap.values());
 		}
-
 		finalResponse.getResult().put(JsonKey.TENANT_PREFERENCE, list);
 		sender().tell(finalResponse, self());
-
 	}
-
+	
 	/**
 	 * Method to update the Tenant preference on basis of id or (role and org id).
 	 * 
 	 * @param actorMessage
 	 */
+	@SuppressWarnings("unchecked")
 	private void updateTenantPreference(Request actorMessage) {
 
-		ProjectLogger.log("TenantPreferenceManagementActor-createTenantPreference called");
-		List<Map<String, Object>> reqList = (List<Map<String, Object>>) actorMessage.getRequest()
-				.get(JsonKey.TENANT_PREFERENCE);
-
+		String orgId = (String) actorMessage.getRequest().get(JsonKey.ROOT_ORG_ID);
+		ProjectLogger.log("TenantPreferenceManagementActor-updateTenantPreference called for org: " + orgId);
+		validateRequest(actorMessage);
+		
 		Response finalResponse = new Response();
 		List<Map<String, Object>> responseList = new ArrayList<>();
-		String orgId = (String) actorMessage.getRequest().get(JsonKey.ROOT_ORG_ID);
-
-		if (ProjectUtil.isStringNullOREmpty(orgId)) {
-			// throw invalid ord id ,org id should not be null or empty .
-			throw new ProjectCommonException(ResponseCode.invalidOrgId.getErrorCode(),
-					ResponseCode.invalidOrgId.getErrorMessage(), ResponseCode.CLIENT_ERROR.getResponseCode());
-		}
-		if (reqList.isEmpty()) {
-			// no need to do anything throw exception invalid request data as list is empty
-			throw new ProjectCommonException(ResponseCode.invalidRequestData.getErrorCode(),
-					ResponseCode.invalidRequestData.getErrorMessage(), ResponseCode.CLIENT_ERROR.getResponseCode());
-		}
-		Response response1 = cassandraOperation.getRecordsByProperty(tenantPreferenceDbInfo.getKeySpace(),
-				tenantPreferenceDbInfo.getTableName(), JsonKey.ORG_ID, orgId);
-		List<Map<String, Object>> list = (List<Map<String, Object>>) response1.get(JsonKey.RESPONSE);
+		List<Map<String, Object>> reqList = (List<Map<String, Object>>) actorMessage.getRequest()
+				.get(JsonKey.TENANT_PREFERENCE);
+		List<Map<String, Object>> preferencesList = getPreferencesFromDB(orgId);
 
 		for (Map<String, Object> map : reqList) {
-
-			Map<String, Object> dbMap = null;
-			String id = (String) map.get(JsonKey.ID);
-
-			if (ProjectUtil.isStringNullOREmpty(id)) {
-
-				Map<String, Object> responseMap = new HashMap<>();
-				responseMap.put(JsonKey.ID, id);
-				responseMap.put(JsonKey.STATUS, JsonKey.FAILURE);
-				responseList.add(responseMap);
-			} else {
-				boolean flag = false;
-				for (Map<String, Object> m : list) {
-					if (id.equals((String) m.get(JsonKey.ID))) {
-						dbMap = m;
-						flag = true;
+			Map<String, Object> preferenceObj = null;
+			String key = (String) map.get(JsonKey.KEY);
+			String data = (String) map.get(JsonKey.DATA);
+			// skip update if either key or data is empty
+			if (StringUtils.isNotBlank(key) && StringUtils.isNotBlank(data)) {
+				boolean found = false;
+				for (Map<String, Object> m : preferencesList) {
+					// check if preference key exists for the org
+					if (key.equals((String) m.get(JsonKey.KEY))) {
+						preferenceObj = m;
+						found = true;
 						break;
 					}
 				}
-				if (!flag) {
-					// means data not found
-					Map<String, Object> responseMap = new HashMap<>();
-					responseMap.put(JsonKey.ID, id);
-					responseMap.put(JsonKey.STATUS, "Invalid Id");
-					responseList.add(responseMap);
+				// if preference is not found
+				if (!found) 
+					responseList.add(getResponseMap(orgId, key, "Preference setting not found for key: " + key + " for the org: " + orgId));
+				
+				// if preference is found
+				if (null != preferenceObj) {
+					preferenceObj.put(JsonKey.KEY, key);
+					preferenceObj.put(JsonKey.DATA, data);
+					cassandraOperation.updateRecord(tenantPreferenceDbInfo.getKeySpace(),
+							tenantPreferenceDbInfo.getTableName(), preferenceObj);
+					responseList.add(getResponseMap(orgId, key, null));
 				}
-			}
-
-			if (null != dbMap) {
-				if (map.containsKey(JsonKey.ROLE)) {
-					dbMap.put(JsonKey.ROLE, map.get(JsonKey.ROLE));
-				}
-				if (map.containsKey(JsonKey.DATA)) {
-					dbMap.put(JsonKey.DATA, map.get(JsonKey.DATA));
-				}
-				cassandraOperation.updateRecord(tenantPreferenceDbInfo.getKeySpace(),
-						tenantPreferenceDbInfo.getTableName(), dbMap);
-				Map<String, Object> responseMap = new HashMap<>();
-				responseMap.put(JsonKey.ID, id);
-				responseMap.put(JsonKey.STATUS, JsonKey.SUCCESS);
-				responseList.add(responseMap);
 			}
 
 		}
 		finalResponse.getResult().put(JsonKey.RESPONSE, responseList);
 		sender().tell(finalResponse, self());
-
 	}
-
+	
 	/**
-	 * Method to create tenant preference on basis of role and org , if already
-	 * exist it will not create new one .
+	 * Method to create tenant preference for an org , if already
+	 * exists it will not create new one.
 	 * 
 	 * @param actorMessage
 	 */
+	@SuppressWarnings("unchecked")
 	private void createTenantPreference(Request actorMessage) {
 
-		ProjectLogger.log("TenantPreferenceManagementActor-createTenantPreference called");
-		List<Map<String, Object>> req = (List<Map<String, Object>>) actorMessage.getRequest()
-				.get(JsonKey.TENANT_PREFERENCE);
-
+		String orgId = (String) actorMessage.getRequest().get(JsonKey.ROOT_ORG_ID);
+		ProjectLogger.log("TenantPreferenceManagementActor-createTenantPreference called for org: " + orgId);
+		validateRequest(actorMessage);
+		List<Map<String, Object>> preferencesList = getPreferencesFromDB(orgId);
 		Response finalResponse = new Response();
 		List<Map<String, Object>> responseList = new ArrayList<>();
-		String orgId = (String) actorMessage.getRequest().get(JsonKey.ROOT_ORG_ID);
-
-		if (ProjectUtil.isStringNullOREmpty(orgId)) {
-			// throw invalid ord id ,org id should not be null or empty .
-			throw new ProjectCommonException(ResponseCode.invalidOrgId.getErrorCode(),
-					ResponseCode.invalidOrgId.getErrorMessage(), ResponseCode.CLIENT_ERROR.getResponseCode());
-		}
-		// check whether org exist or not
-		Response result = cassandraOperation.getRecordById(orgDbInfo.getKeySpace(), orgDbInfo.getTableName(), orgId);
-		List<Map<String, Object>> orglist = (List<Map<String, Object>>) result.get(JsonKey.RESPONSE);
-		if (orglist.isEmpty()) {
-			throw new ProjectCommonException(ResponseCode.invalidOrgId.getErrorCode(),
-					ResponseCode.invalidOrgId.getErrorMessage(), ResponseCode.CLIENT_ERROR.getResponseCode());
-		}
-		if (req.isEmpty()) {
-			// no need to do anything throw exception invalid request data as list is empty
-			throw new ProjectCommonException(ResponseCode.invalidRequestData.getErrorCode(),
-					ResponseCode.invalidRequestData.getErrorMessage(), ResponseCode.CLIENT_ERROR.getResponseCode());
-		}
-
-		Response response1 = cassandraOperation.getRecordsByProperty(tenantPreferenceDbInfo.getKeySpace(),
-				tenantPreferenceDbInfo.getTableName(), JsonKey.ORG_ID, orgId);
-		List<Map<String, Object>> list = (List<Map<String, Object>>) response1.get(JsonKey.RESPONSE);
-
+		List<Map<String, Object>> req = (List<Map<String, Object>>) actorMessage.getRequest().get(JsonKey.TENANT_PREFERENCE);
 		for (Map<String, Object> map : req) {
-			String role = (String) map.get(JsonKey.ROLE);
+			String key = (String) map.get(JsonKey.KEY);
+			boolean skip = false;
 
-			if (ProjectUtil.isStringNullOREmpty(orgId) || ProjectUtil.isStringNullOREmpty(role)) {
-				finalResponse.getResult().put(role, "OrgId , role can not be null .");
+			// abort creation of preferences if any of key is empty or blank
+			if (StringUtils.isBlank(key)) {
+				responseList.add(getResponseMap(orgId, key, "Preference key is null"));
+				skip = true;
 			}
-			// check whether already tenant pref has created for the given org and role if
-			// yes show error
-			boolean flag = true;
-			for (Map<String, Object> m : list) {
-				if (role.equalsIgnoreCase((String) m.get(JsonKey.ROLE))) {
-					Map<String, Object> responseMap = new HashMap<>();
-					responseMap.put(JsonKey.ROLE, role);
-					responseMap.put(JsonKey.STATUS, "Tenant preference already exist for role ");
-					responseList.add(responseMap);
-					flag = false;
+			
+			// check whether already tenant preference exists for the given org id
+			for (Map<String, Object> m : preferencesList) {
+				if (key.equalsIgnoreCase((String) m.get(JsonKey.KEY))) {
+					responseList.add(getResponseMap(orgId, key, "Preference already exists for key: " + key));
+					// skip creation of the preference if already exists for the org
+					skip = true;
 					break;
 				}
 			}
 
-			if (flag) {
+			// create the preference if not already exists
+			if (!skip) {
 				Map<String, Object> dbMap = new HashMap<String, Object>();
-				String id = ProjectUtil.getUniqueIdFromTimestamp(actorMessage.getEnv());
-				dbMap.put(JsonKey.ID, id);
+				dbMap.put(JsonKey.ID, ProjectUtil.getUniqueIdFromTimestamp(actorMessage.getEnv()));
 				dbMap.put(JsonKey.ORG_ID, orgId);
-				dbMap.put(JsonKey.ROLE, role);
+				dbMap.put(JsonKey.KEY, key);
 				dbMap.put(JsonKey.DATA, (String) map.get(JsonKey.DATA));
 				cassandraOperation.insertRecord(tenantPreferenceDbInfo.getKeySpace(),
 						tenantPreferenceDbInfo.getTableName(), dbMap);
-				Map<String, Object> responseMap = new HashMap<>();
-				responseMap.put(JsonKey.ID, id);
-				responseMap.put(JsonKey.ROLE, role);
-				responseMap.put(JsonKey.STATUS, JsonKey.SUCCESS);
-				responseList.add(responseMap);
-				finalResponse.getResult().put(role, JsonKey.SUCCESS);
+				responseList.add(getResponseMap(orgId, key, null));
+				finalResponse.getResult().put(key, JsonKey.SUCCESS);
 			}
 		}
 		finalResponse.getResult().put(JsonKey.RESPONSE, responseList);
 		sender().tell(finalResponse, self());
+	}
+	
+	private Map<String, Object> getResponseMap(String orgId, String key, String error) {
+		Map<String, Object> responseMap = new HashMap<>();
+		responseMap.put(JsonKey.ORG_ID, orgId);
+		responseMap.put(JsonKey.KEY, key);
+		if (StringUtils.isNotBlank(error)) {
+			responseMap.put(JsonKey.STATUS, JsonKey.FAILED);
+			responseMap.put(JsonKey.ERROR, "Preference setting not found for key: " + key + " for the org: " + orgId);
+		} else
+			responseMap.put(JsonKey.STATUS, JsonKey.SUCCESS);
+		return responseMap;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private List<Map<String, Object>> getPreferencesFromDB(String orgId) {
+		Response tenantPreferences = cassandraOperation.getRecordsByProperty(tenantPreferenceDbInfo.getKeySpace(),
+				tenantPreferenceDbInfo.getTableName(), JsonKey.ORG_ID, orgId);
+		List<Map<String, Object>> preferencesList = (List<Map<String, Object>>) tenantPreferences.get(JsonKey.RESPONSE);
+		return preferencesList;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void validateRequest(Request actorMessage) {
+		List<Map<String, Object>> req = (List<Map<String, Object>>) actorMessage.getRequest()
+				.get(JsonKey.TENANT_PREFERENCE);
+
+		String orgId = (String) actorMessage.getRequest().get(JsonKey.ROOT_ORG_ID);
+		if (StringUtils.isBlank(orgId)) {
+			// throw invalid ord id ,org id should not be null or empty .
+			throw new ProjectCommonException(ResponseCode.invalidOrgId.getErrorCode(),
+					ResponseCode.invalidOrgId.getErrorMessage(), ResponseCode.CLIENT_ERROR.getResponseCode());
+		}
+		
+		// skipping this check for now to allow storing of channel based preferences
+		/*
+		// check if the org id is either a valid org in the system or is the default wild card org (*)
+		if (!StringUtils.equalsIgnoreCase(DEFAULT_WILDCARD_ORG_ID, orgId)) {
+			// check whether org exist or not
+			Response result = cassandraOperation.getRecordById(orgDbInfo.getKeySpace(), orgDbInfo.getTableName(), orgId);
+			List<Map<String, Object>> orglist = (List<Map<String, Object>>) result.get(JsonKey.RESPONSE);
+			if (null == orglist || orglist.isEmpty())
+				throw new ProjectCommonException(ResponseCode.invalidOrgId.getErrorCode(),
+						ResponseCode.invalidOrgId.getErrorMessage(), ResponseCode.CLIENT_ERROR.getResponseCode());
+		}*/
+
+		// no need to do anything throw exception invalid request data as list is empty
+		if (null == req || req.isEmpty())
+			throw new ProjectCommonException(ResponseCode.invalidRequestData.getErrorCode(),
+					ResponseCode.invalidRequestData.getErrorMessage(), ResponseCode.CLIENT_ERROR.getResponseCode());
+	}
+	
+	private Map<String, Map<String, Object>> getPreferenceMap(List<Map<String, Object>> preferencesList) {
+		Map<String, Map<String, Object>> keyMap = new HashMap<String, Map<String, Object>>();
+		if (null != preferencesList && !preferencesList.isEmpty()) {
+			for (Map<String, Object> m : preferencesList) {
+				String key = (String) m.get(JsonKey.KEY);
+				keyMap.put(key, m);
+			}
+		}
+		return keyMap;
 	}
 
 	/**

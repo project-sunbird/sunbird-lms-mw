3a115bfb123ddc309214fbb4b1c9d21e8cebc55b
manzarul <manzarul07@gmail.com>
2018-03-24 14:23:22 +0530

    Issues #SB-766 feat: chnages in telemetry storage

--- actors/telemetry/src/main/java/org/sunbird/util/lmaxdisruptor/ESEventConsumer.java
+++ actors/telemetry/src/main/java/org/sunbird/util/lmaxdisruptor/ESEventConsumer.java
@@ -1,7 +1,6 @@
 package org.sunbird.util.lmaxdisruptor;
 
 
-import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -20,22 +19,9 @@ public class ESEventConsumer implements EventHandler<TelemetryEvent> {
 
 	public void onEvent(TelemetryEvent writeEvent, long sequence, boolean endOfBatch) throws Exception {
 		if (writeEvent != null && writeEvent.getData().getRequest() != null) {
-		 ElasticSearchUtil.bulkInsertData(createIndex(), ProjectUtil.EsType.telemetry.getTypeName(),
+		 ElasticSearchUtil.bulkInsertData(ProjectUtil.createIndex(), ProjectUtil.EsType.telemetry.getTypeName(),
 					(List<Map<String,Object>>)writeEvent.getData().getRequest().getRequest().get("events"));
 		}
 	}
 
-	/**
-	 * This method will create index for Elastic search as follow
-	 * "telemetry.raw.yyyy.mm"
-	 * 
-	 * @return
-	 */
-	private static String createIndex() {
-		Calendar cal = Calendar.getInstance();
-		return new StringBuffer().append(INDEX_NAME).append("." + cal.get(Calendar.YEAR))
-				.append("." + ((cal.get(Calendar.MONTH) + 1) > 9 ? (cal.get(Calendar.MONTH) + 1)
-						: "0" + (cal.get(Calendar.MONTH) + 1)))
-				.toString();
-	}
 }

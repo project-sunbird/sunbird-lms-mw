240b2c2986cc5610b09dda6b3af7d9d728a688d2
Manojvv <manojv@ilimi.in>
2018-03-29 12:13:05 +0530

    Issue #85 chore: lms-mw  image version updated

--- metadata.sh
+++ metadata.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # return version
-echo '{"name":"actor-service","version":"0.0.5","org":"sunbird","hubuser":"purplesunbird"}'
+echo '{"name":"actor-service","version":"1.5.0","org":"sunbird","hubuser":"purplesunbird"}'

e345a31273b6fda775bc2a0e096b417c2b75aafa
AMIT KUMAR <amit.kumar@tarento.com>
2018-04-02 12:14:33 +0530

    Issue #SB-0 feat: changed the value of threshold value of telemetry

--- actors/telemetry/src/main/java/org/sunbird/telemetry/util/TelemetryFlush.java
+++ actors/telemetry/src/main/java/org/sunbird/telemetry/util/TelemetryFlush.java
@@ -1,5 +1,7 @@
 package org.sunbird.telemetry.util;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,79 +15,79 @@ import org.sunbird.common.models.util.PropertiesCache;
 import org.sunbird.common.request.Request;
 import org.sunbird.util.lmaxdisruptor.LMAXWriter;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 /**
  * Created by arvind on 11/1/18.
  */
 public class TelemetryFlush {
 
-	private Queue<String> queue = new ConcurrentLinkedQueue<>();
-	private int thresholdSize = 20;
-	private static ObjectMapper mapper = new ObjectMapper();
-	private static TelemetryFlush telemetryFlush;
+    private Queue<String> queue = new ConcurrentLinkedQueue<>();
+    private int thresholdSize = 500;
+    private static ObjectMapper mapper = new ObjectMapper();
+    private static TelemetryFlush telemetryFlush;
 
-	public static TelemetryFlush getInstance() {
-		if (telemetryFlush == null) {
-			synchronized (TelemetryFlush.class) {
-				if (telemetryFlush == null) {
-					telemetryFlush = new TelemetryFlush();
-				}
-			}
-		}
-		return telemetryFlush;
-	}
+    public static TelemetryFlush getInstance() {
+        if (telemetryFlush == null) {
+            synchronized (TelemetryFlush.class) {
+                if (telemetryFlush == null) {
+                    telemetryFlush = new TelemetryFlush();
+                }
+            }
+        }
+        return telemetryFlush;
+    }
 
-	public TelemetryFlush() {
-		String queueThreshold = PropertiesCache.getInstance().getProperty(JsonKey.TELEMETRY_QUEUE_THRESHOLD_VALUE);
-		if (!StringUtils.isBlank(queueThreshold)
-				&& !queueThreshold.equalsIgnoreCase(JsonKey.TELEMETRY_QUEUE_THRESHOLD_VALUE)) {
-			try {
-				this.thresholdSize = Integer.parseInt(queueThreshold.trim());
-			} catch (Exception ex) {
-				ProjectLogger.log("Threshold size from config is not integer", ex);
-			}
-		}
-	}
+    public TelemetryFlush() {
+        String queueThreshold = System.getenv(JsonKey.TELEMETRY_QUEUE_THRESHOLD_VALUE);
+        if (StringUtils.isBlank(queueThreshold)) {
+            queueThreshold = PropertiesCache.getInstance()
+                    .getProperty(JsonKey.TELEMETRY_QUEUE_THRESHOLD_VALUE);
+        }
+        if (!StringUtils.isBlank(queueThreshold)
+                && !queueThreshold.equalsIgnoreCase(JsonKey.TELEMETRY_QUEUE_THRESHOLD_VALUE)) {
+            try {
+                this.thresholdSize = Integer.parseInt(queueThreshold.trim());
+            } catch (Exception ex) {
+                ProjectLogger.log("Threshold size from config is not integer", ex);
+            }
+        }
+    }
 
-	public void flushTelemetry(String message) {
-		writeToQueue(message);
-	}
+    public void flushTelemetry(String message) {
+        writeToQueue(message);
+    }
 
-	private void writeToQueue(String message) {
-		queue.offer(message);
-		if (queue.size() >= thresholdSize) {
-			List<String> list = new ArrayList<>();
-			for (int i = 1; i <= thresholdSize; i++) {
-				String obj = queue.poll();
-				if (obj == null) {
-					break;
-				} else {
-					list.add(obj);
-				}
-			}
-			Request req = createTelemetryRequest(list);
-			LMAXWriter.getInstance().submitMessage(req);
-		}
-	}
+    private void writeToQueue(String message) {
+        queue.offer(message);
+        if (queue.size() >= thresholdSize) {
+            List<String> list = new ArrayList<>();
+            for (int i = 1; i <= thresholdSize; i++) {
+                String obj = queue.poll();
+                if (obj == null) {
+                    break;
+                } else {
+                    list.add(obj);
+                }
+            }
+            Request req = createTelemetryRequest(list);
+            LMAXWriter.getInstance().submitMessage(req);
+        }
+    }
 
-	private Request createTelemetryRequest(List<String> eventList) {
-		Request req = null;
-		try {
-			List<Map<String, Object>> jsonList = mapper.readValue(eventList.toString(),
-					new TypeReference<List<Map<String, Object>>>() {
-					});
-			Map<String, Object> map = new HashMap<>();
-			map.put(JsonKey.ETS, System.currentTimeMillis());
-			map.put(JsonKey.EVENTS, jsonList);
-			req = new Request();
-			req.getRequest().putAll(map);
-			return req;
-		} catch (Exception e) {
-			ProjectLogger.log("Failed to create request for telemetry flush.", e);
-		}
-		return req;
-	}
+    private Request createTelemetryRequest(List<String> eventList) {
+        Request req = null;
+        try {
+            List<Map<String, Object>> jsonList = mapper.readValue(eventList.toString(),
+                    new TypeReference<List<Map<String, Object>>>() {});
+            Map<String, Object> map = new HashMap<>();
+            map.put(JsonKey.ETS, System.currentTimeMillis());
+            map.put(JsonKey.EVENTS, jsonList);
+            req = new Request();
+            req.getRequest().putAll(map);
+            return req;
+        } catch (Exception e) {
+            ProjectLogger.log("Failed to create request for telemetry flush.", e);
+        }
+        return req;
+    }
 
 }
--- actors/telemetry/src/main/java/org/sunbird/util/lmaxdisruptor/EkstepEventConsumer.java
+++ actors/telemetry/src/main/java/org/sunbird/util/lmaxdisruptor/EkstepEventConsumer.java
@@ -1,12 +1,13 @@
 package org.sunbird.util.lmaxdisruptor;
 
+import com.google.gson.Gson;
+import com.lmax.disruptor.EventHandler;
 import java.io.ByteArrayOutputStream;
 import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.zip.GZIPOutputStream;
-
 import org.apache.commons.lang3.StringUtils;
 import org.sunbird.common.models.response.HttpUtilResponse;
 import org.sunbird.common.models.util.HttpUtil;
@@ -17,9 +18,6 @@ import org.sunbird.common.models.util.PropertiesCache;
 import org.sunbird.common.request.Request;
 import org.sunbird.common.request.TelemetryV3Request;
 
-import com.google.gson.Gson;
-import com.lmax.disruptor.EventHandler;
-
 /**
  * This class will send telemetry data to Ekstep.
  * 
@@ -28,97 +26,102 @@ import com.lmax.disruptor.EventHandler;
  */
 public class EkstepEventConsumer implements EventHandler<Request> {
 
-	@Override
-	@SuppressWarnings("unchecked")
-	public void onEvent(Request request, long sequence, boolean endOfBatch) throws Exception {
-		if (request != null) {
-			Map<String, Object> reqMap = request.getRequest();
-			Map<String, String> headers = (Map<String, String>) reqMap.get(JsonKey.HEADER);
-			if (null == headers) {
-				headers = getHeaders();
-			}
-			if ("gzip".equalsIgnoreCase((String) headers.get(JsonKey.CONTENT_ENCODING))) {
-				if (null != reqMap.get(JsonKey.FILE)) {
-					sendTelemetryToEkstep((byte[]) reqMap.get(JsonKey.FILE), request);
-				}
-			} else {
-				try {
-					Gson gson = new Gson();
-					String response = HttpUtil.sendPostRequest(getTelemetryUrl(),
-							gson.toJson(getEkstepTelemetryRequest(request)), headers);
-					ProjectLogger.log(response + " processed.", LoggerEnum.INFO.name());
-				} catch (Exception e) {
-					ProjectLogger.log(e.getMessage(), e);
-					e.printStackTrace();
-					ProjectLogger.log("Failure Data==" + request);
-				}
-			}
-		}
-	}
-	
-	private Map<String, String> getHeaders() {
-		Map<String, String> headers = new HashMap<String, String>();
-		String authKey = System.getenv("ekstep_authorization");
-		headers.put("authorization", JsonKey.BEARER + authKey);
-		headers.put("Content-Type", "application/json");
-		return headers;
-	}
+    @Override
+    @SuppressWarnings("unchecked")
+    public void onEvent(Request request, long sequence, boolean endOfBatch) throws Exception {
+        if (request != null) {
+            Map<String, Object> reqMap = request.getRequest();
+            Map<String, String> headers = (Map<String, String>) reqMap.get(JsonKey.HEADER);
+            if (null == headers) {
+                headers = getHeaders();
+            }
+            if ("gzip".equalsIgnoreCase(headers.get(JsonKey.CONTENT_ENCODING))) {
+                if (null != reqMap.get(JsonKey.FILE)) {
+                    sendTelemetryToEkstep((byte[]) reqMap.get(JsonKey.FILE), request);
+                }
+            } else {
+                try {
+                    Gson gson = new Gson();
+                    String response = HttpUtil.sendPostRequest(getTelemetryUrl(),
+                            gson.toJson(getEkstepTelemetryRequest(request)), headers);
+                    ProjectLogger.log(response + " processed.", LoggerEnum.INFO.name());
+                } catch (Exception e) {
+                    ProjectLogger.log(e.getMessage(), e);
+                    ProjectLogger.log("Failure Data==" + request);
+                }
+            }
+        }
+    }
 
-	private void sendTelemetryToEkstep(byte[] bs, Request request) {
-		Map<String, String> headers = (Map<String, String>) request.get(JsonKey.HEADER);
-		headers.put(JsonKey.CONTENT_ENCODING, "gzip");
-		headers.remove("content-type");
-		headers.put("Content-Type", "application/zip");
+    private Map<String, String> getHeaders() {
+        Map<String, String> headers = new HashMap<String, String>();
+        String authKey = System.getenv("ekstep_authorization");
+        headers.put("authorization", JsonKey.BEARER + authKey);
+        headers.put("Content-Type", "application/json");
+        return headers;
+    }
 
-		try {
-			byte[] gzipBytes = compressedBytes(bs);
-			HttpUtilResponse response = HttpUtil.postInputStream(gzipBytes, headers, getTelemetryUrl());
-			ProjectLogger.log(response.getStatusCode() + " " + response.getBody(), LoggerEnum.INFO.name());
-		} catch (Exception e) {
-			ProjectLogger.log(e.getMessage(), e);
-			ProjectLogger.log("Failure Data==" + request);
-		}
-	}
+    private void sendTelemetryToEkstep(byte[] bs, Request request) {
+        Map<String, String> headers = (Map<String, String>) request.get(JsonKey.HEADER);
+        headers.put(JsonKey.CONTENT_ENCODING, "gzip");
+        headers.remove("content-type");
+        headers.put("Content-Type", "application/zip");
 
-	private byte[] compressedBytes(byte[] bs) throws Exception {
-		ByteArrayOutputStream byteStream = new ByteArrayOutputStream(bs.length);
-		GZIPOutputStream zipStream = new GZIPOutputStream(byteStream);
-		try {
-			zipStream.write(bs);
-		} finally {
-			zipStream.close();
-		}
-		return byteStream.toByteArray();
-	}
+        try {
+            byte[] gzipBytes = compressedBytes(bs);
+            HttpUtilResponse response =
+                    HttpUtil.postInputStream(gzipBytes, headers, getTelemetryUrl());
+            ProjectLogger.log(response.getStatusCode() + " " + response.getBody(),
+                    LoggerEnum.INFO.name());
+        } catch (Exception e) {
+            ProjectLogger.log(e.getMessage(), e);
+            ProjectLogger.log("Failure Data==" + request);
+        }
+    }
 
-	/**
-	 * This method will return telemetry url.
-	 * 
-	 * @return
-	 */
-	private String getTelemetryUrl() {
-		String telemetryBaseUrl = System.getenv(JsonKey.EKSTEP_BASE_URL);
-		if (StringUtils.isBlank(telemetryBaseUrl)) {
-			telemetryBaseUrl = PropertiesCache.getInstance().getProperty(JsonKey.EKSTEP_BASE_URL);
-		}
-		telemetryBaseUrl = telemetryBaseUrl
-				+ PropertiesCache.getInstance().getProperty(JsonKey.EKSTEP_TELEMETRY_API_URL);
-		ProjectLogger.log("telemetry url==" + telemetryBaseUrl);
-		return telemetryBaseUrl;
-	}
+    private byte[] compressedBytes(byte[] bs) throws Exception {
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream(bs.length);
+        GZIPOutputStream zipStream = new GZIPOutputStream(byteStream);
+        try {
+            zipStream.write(bs);
+        } finally {
+            zipStream.close();
+        }
+        return byteStream.toByteArray();
+    }
 
-	private TelemetryV3Request getEkstepTelemetryRequest(Request request) {
-		TelemetryV3Request telemetryV3Request = new TelemetryV3Request();
-		if (request.getRequest().get(JsonKey.ETS) != null
-				&& request.getRequest().get(JsonKey.ETS) instanceof BigInteger) {
-			telemetryV3Request.setEts(((BigInteger) request.getRequest().get(JsonKey.ETS)).longValue());
-		}
-		if (request.getRequest().get(JsonKey.EVENTS) != null && request.getRequest().get(JsonKey.EVENTS) instanceof List
-				&& !(((List) request.getRequest().get(JsonKey.EVENTS)).isEmpty())) {
-			List<Map<String, Object>> events = (List<Map<String, Object>>) request.getRequest().get(JsonKey.EVENTS);
-			telemetryV3Request.setEvents(events);
-			ProjectLogger.log("Sending events to Ekstep - count: " + events.size(), LoggerEnum.INFO.name());
-		}
-		return telemetryV3Request;
-	}
+    /**
+     * This method will return telemetry url.
+     * 
+     * @return
+     */
+    private String getTelemetryUrl() {
+        String telemetryBaseUrl = System.getenv(JsonKey.EKSTEP_BASE_URL);
+        if (StringUtils.isBlank(telemetryBaseUrl)) {
+            telemetryBaseUrl = PropertiesCache.getInstance().getProperty(JsonKey.EKSTEP_BASE_URL);
+        }
+        telemetryBaseUrl = telemetryBaseUrl
+                + PropertiesCache.getInstance().getProperty(JsonKey.EKSTEP_TELEMETRY_API_URL);
+        ProjectLogger.log("telemetry url==" + telemetryBaseUrl);
+        return telemetryBaseUrl;
+    }
+
+    private TelemetryV3Request getEkstepTelemetryRequest(Request request) {
+        TelemetryV3Request telemetryV3Request = new TelemetryV3Request();
+        if (request.getRequest().get(JsonKey.ETS) != null
+                && request.getRequest().get(JsonKey.ETS) instanceof BigInteger) {
+            telemetryV3Request
+                    .setEts(((BigInteger) request.getRequest().get(JsonKey.ETS)).longValue());
+        }
+        if (request.getRequest().get(JsonKey.EVENTS) != null
+                && request.getRequest().get(JsonKey.EVENTS) instanceof List
+                && !(((List) request.getRequest().get(JsonKey.EVENTS)).isEmpty())) {
+            List<Map<String, Object>> events =
+                    (List<Map<String, Object>>) request.getRequest().get(JsonKey.EVENTS);
+            telemetryV3Request.setEvents(events);
+            ProjectLogger.log("Sending events to Ekstep - count: " + events.size(),
+                    LoggerEnum.INFO.name());
+        }
+        return telemetryV3Request;
+    }
 }

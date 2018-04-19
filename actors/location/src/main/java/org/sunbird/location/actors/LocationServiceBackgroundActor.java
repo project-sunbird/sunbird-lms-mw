a42e88d099303f7ea7784cfe8b40dfad469c48cc
AMIT KUMAR <amit.kumar@tarento.com>
2018-04-19 11:50:06 +0530

    Issue # SB-2706 feat: created location module at actor service

--- actors/location/src/main/java/org/sunbird/location/actors/LocationServiceBackgroundActor.java
+++ actors/location/src/main/java/org/sunbird/location/actors/LocationServiceBackgroundActor.java
@@ -2,9 +2,11 @@ package org.sunbird.location.actors;
 
 import java.util.Map;
 import org.sunbird.actor.core.BaseActor;
+import org.sunbird.actor.router.ActorConfig;
 import org.sunbird.common.ElasticSearchUtil;
 import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.models.util.ProjectLogger;
+import org.sunbird.common.models.util.ProjectUtil;
 import org.sunbird.common.request.Request;
 
 /**
@@ -12,6 +14,10 @@ import org.sunbird.common.request.Request;
  *
  * @author Amit Kumar
  */
+@ActorConfig(
+  tasks = {},
+  asyncTasks = {"upsertLocationDataToES"}
+)
 public class LocationServiceBackgroundActor extends BaseActor {
 
   @Override
@@ -32,9 +38,17 @@ public class LocationServiceBackgroundActor extends BaseActor {
     Map<String, Object> location = (Map<String, Object>) request.getRequest().get(JsonKey.LOCATION);
     String opType = (String) request.getRequest().get(JsonKey.OPERATION_TYPE);
     if (JsonKey.INSERT.equalsIgnoreCase(opType)) {
-      ElasticSearchUtil.createData("index", "type", "identifier", location);
+      ElasticSearchUtil.createData(
+          ProjectUtil.EsIndex.sunbird.getIndexName(),
+          ProjectUtil.EsType.location.getTypeName(),
+          (String) location.get(JsonKey.ID),
+          location);
     } else if (JsonKey.UPDATE.equalsIgnoreCase(opType)) {
-      ElasticSearchUtil.updateData("index", "type", "identifier", location);
+      ElasticSearchUtil.updateData(
+          ProjectUtil.EsIndex.sunbird.getIndexName(),
+          ProjectUtil.EsType.location.getTypeName(),
+          (String) location.get(JsonKey.ID),
+          location);
     }
   }
 }

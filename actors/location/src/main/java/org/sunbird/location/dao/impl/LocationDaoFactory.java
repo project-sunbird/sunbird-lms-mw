df8628792f638e7cbbb72edff7096e8c3eb8d530
AMIT KUMAR <amit.kumar@tarento.com>
2018-04-25 15:44:38 +0530

    Issue # SB-2760 feat: created location module at actor service

--- actors/location/src/main/java/org/sunbird/location/dao/impl/LocationDaoFactory.java
+++ actors/location/src/main/java/org/sunbird/location/dao/impl/LocationDaoFactory.java
@@ -2,8 +2,10 @@ package org.sunbird.location.dao.impl;
 
 import org.sunbird.location.dao.LocationDao;
 
+/** @author Amit Kumar */
 public class LocationDaoFactory {
 
+  /** private default constructor. */
   private LocationDaoFactory() {}
 
   private static LocationDao locationDao;
@@ -12,6 +14,11 @@ public class LocationDaoFactory {
     locationDao = new LocationDaoImpl();
   }
 
+  /**
+   * This method will provide singleton instance for LocationDaoImpl.
+   *
+   * @return LocationDao
+   */
   public static LocationDao getInstance() {
     return locationDao;
   }

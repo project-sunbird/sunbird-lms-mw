88da4df24ced0b848928c9cb542a9a229f07b3cf
arvindyadav108 <arvind.glaitm108@gmail.com>
2018-05-14 18:37:56 +0530

    Issue # SB-3109 Developement - Metash and jenkins changes for branch release-1.7 .

--- metadata.sh
+++ metadata.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # return version
-echo '{"name":"actor-service","version":"1.6.0","org":"sunbird","hubuser":"purplesunbird"}'
+echo '{"name":"actor-service","version":"1.7.0","org":"sunbird","hubuser":"purplesunbird"}'

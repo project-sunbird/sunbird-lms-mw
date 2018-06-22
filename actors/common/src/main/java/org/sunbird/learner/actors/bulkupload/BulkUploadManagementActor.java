55cb3a36e7bd45c70bde9b0acbe8aa4aa5026560
AMIT KUMAR <amit.kumar@tarento.com>
2018-06-22 15:21:09 +0530

    Issue # SB-4009 feat:read user bulk upload size

--- actors/common/src/main/java/org/sunbird/learner/actors/bulkupload/BulkUploadManagementActor.java
+++ actors/common/src/main/java/org/sunbird/learner/actors/bulkupload/BulkUploadManagementActor.java
@@ -350,7 +350,10 @@ public class BulkUploadManagementActor extends BaseBulkUploadActor {
             (Integer.parseInt(
                 ProjectUtil.getConfigValue(JsonKey.BULK_UPLOAD_USER_DATA_SIZE).trim()));
 
-        ProjectLogger.log("bulk upload user data size read from config file " + userDataSize);
+        ProjectLogger.log(
+            "BulkUploadManagementActor:processBulkUserUpload : bulk upload user data size"
+                + userDataSize,
+            LoggerEnum.INFO.name());
       }
       validateFileSizeAgainstLineNumbers(userDataSize, userList.size());
       if (!userList.isEmpty()) {

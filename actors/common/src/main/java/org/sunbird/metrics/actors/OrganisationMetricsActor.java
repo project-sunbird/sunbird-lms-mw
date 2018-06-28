0696407f52a394b7abf77f6d6fc6484255e33e29
Manzarul <manzarul07@gmail.com>
2018-06-28 18:35:24 +0530

    Issues #SB-4437 fix: changes some of consumption response attribute

--- actors/common/src/main/java/org/sunbird/metrics/actors/OrganisationMetricsActor.java
+++ actors/common/src/main/java/org/sunbird/metrics/actors/OrganisationMetricsActor.java
@@ -1,5 +1,7 @@
 package org.sunbird.metrics.actors;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -9,7 +11,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.sunbird.actor.router.ActorConfig;
@@ -30,9 +31,6 @@ import org.sunbird.common.request.Request;
 import org.sunbird.common.responsecode.ResponseCode;
 import org.sunbird.helper.ServiceFactory;
 import org.sunbird.learner.util.Util;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ActorConfig(
   tasks = {
@@ -571,7 +569,9 @@ public class OrganisationMetricsActor extends BaseMetricsActor {
   private String getOrgConsumptionData(
       Request actorMessage, String periodStr, String orgHashId, String channel) throws IOException {
     String requestStr = getOrgMetricsRequest(actorMessage, periodStr, orgHashId, null, channel);
-    ProjectLogger.log("OrganisationMetricsActor:getOrgConsumptionData Requested data : " + requestStr ,LoggerEnum.INFO.name());
+    ProjectLogger.log(
+        "OrganisationMetricsActor:getOrgConsumptionData Requested data : " + requestStr,
+        LoggerEnum.INFO.name());
     String ekStepResponse = makePostRequest(JsonKey.EKSTEP_METRICS_API_URL, requestStr);
     return orgConsumptionResponseGenerator(periodStr, ekStepResponse);
   }
@@ -626,9 +626,10 @@ public class OrganisationMetricsActor extends BaseMetricsActor {
         }
         if (metricsDate.equalsIgnoreCase(bucketDate)) {
           Double totalTimeSpent = (Double) res.get("m_total_ts");
-          Integer totalUsers = (Integer) res.get("m_total_users_count");
+          // reading  m_total_sessions instead of m_total_users_count
+          Integer totalSessions = (Integer) res.get("m_total_sessions");
           resData.put(VALUE, totalTimeSpent);
-          userData.put(VALUE, totalUsers);
+          userData.put(VALUE, totalSessions);
         }
         if (index < consumptionBucket.size() && index < userBucket.size()) {
           index++;
@@ -663,7 +664,8 @@ public class OrganisationMetricsActor extends BaseMetricsActor {
       Map<String, Object> snapshot = new LinkedHashMap<>();
       Map<String, Object> dataMap = new HashMap<>();
       dataMap.put(JsonKey.NAME, "Number of visits by users");
-      dataMap.put(VALUE, resultData.get("m_total_users_count"));
+      // reading m_total_sessions instead of m_total_users_count
+      dataMap.put(VALUE, resultData.get("m_total_sessions"));
       snapshot.put("org.consumption.content.session.count", dataMap);
       dataMap = new LinkedHashMap<>();
       dataMap.put(JsonKey.NAME, "Content consumption time");

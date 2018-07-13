21d35adf97dd76e9fc2e09a35d9239a0c7e7e962
Karthik Kumarasamy <kkarthik4cs@gmail.com>
2018-07-13 14:45:34 +0530

    Issue #SB-5226 fix: Added more testcases for  Assign user-roles

--- actors/user/src/test/java/org/sunbird/user/UserAssignRoleTest.java
+++ actors/user/src/test/java/org/sunbird/user/UserAssignRoleTest.java
@@ -7,8 +7,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -30,6 +32,7 @@ import org.sunbird.common.models.util.ActorOperations;
 import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.request.Request;
 import org.sunbird.helper.ServiceFactory;
+import org.sunbird.learner.util.DataCacheHandler;
 import org.sunbird.user.actors.UserManagementActor;
 
 import akka.actor.ActorRef;
@@ -40,19 +43,19 @@ import scala.concurrent.duration.FiniteDuration;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ ServiceFactory.class, ElasticSearchUtil.class, CassandraOperationImpl.class, org.sunbird.common.models.util.datasecurity.impl.ServiceFactory.class})
+@PrepareForTest({ ServiceFactory.class, ElasticSearchUtil.class, CassandraOperationImpl.class, DataCacheHandler.class, org.sunbird.common.models.util.datasecurity.impl.ServiceFactory.class})
 @PowerMockIgnore({ "javax.management.*", "javax.net.ssl.*", "javax.security.*" })
 @SuppressStaticInitializationFor("org.sunbird.common.ElasticSearchUtil")
 public class UserAssignRoleTest {
 
 	private static final FiniteDuration ACTOR_MAX_WAIT_DURATION = duration("120 second");
-	private static String externalId = String.valueOf(System.currentTimeMillis());
-	private static String provider = String.valueOf(System.currentTimeMillis() + 10);
 	private static String ID = "id001";
 	private static String orgId = "testOrg001";
 	private static String userId = "testUser001";
+	private static String externalId = "testExternal001";
+	private static String provider = "testProvider001";
 	private static String hashtagId = "hashTagId001";
-	private static String defaultRole = "PUBLIC";
+	private static List<String> ALL_ROLES = Arrays.asList("CONTENT_CREATOR", "COURSE_MENTOR", "BOOK_CREATOR", "BOOK_REVIEWER", "OFFICIAL_TEXTBOOK_BADGE_ISSUER","TEACHER_BADGE_ISSUER","ANNOUNCEMENT_SENDER", "CONTENT_REVIEWER", "FLAG_REVIEWER", "PUBLIC");  
 	private static Map<String, Object> user_org = new HashMap<>();
 
 	private static ActorSystem system;
@@ -70,7 +73,9 @@ public class UserAssignRoleTest {
 		user_org.put(JsonKey.ORGANISATION_ID, orgId);
 		user_org.put(JsonKey.USER_ID, userId);
 		user_org.put(JsonKey.HASHTAGID, hashtagId);
-		user_org.put(JsonKey.ROLES, Arrays.asList("CONTENT_CREATOR", "PUBLIC"));
+		user_org.put(JsonKey.EXTERNAL_ID, externalId);
+		user_org.put(JsonKey.PROVIDER, provider);
+		user_org.put(JsonKey.ROLES, Arrays.asList("CONTENT_CREATOR", "COURSE_MENTOR", "BOOK_CREATOR", "BOOK_REVIEWER", "PUBLIC"));
 
 		response = new Response();
 		Map<String, Object> responseMap = new HashMap<>();
@@ -79,16 +84,22 @@ public class UserAssignRoleTest {
 		response.getResult().putAll(responseMap);
 
 		esRespone.put(JsonKey.CONTENT, Arrays.asList(user_org));
-		mockClasses();
 	}
-
-	private static void mockClasses() throws Exception {
+	
+	
+	@Before
+	public void mockClasses() throws Exception {
+		
 		PowerMockito.mockStatic(ServiceFactory.class);
 		cassandraOperation = PowerMockito.mock(CassandraOperationImpl.class);
 		PowerMockito.when(ServiceFactory.getInstance()).thenReturn(cassandraOperation);
-		initCassandraForSuccess();
 		
-		
+		Map<String, Object> roleMap = new HashMap<>();
+		for(String role:ALL_ROLES)
+			roleMap.put(role, role);
+	    PowerMockito.mockStatic(DataCacheHandler.class);
+	    PowerMockito.when(DataCacheHandler.getRoleMap()).thenReturn(roleMap);
+	    
 		PowerMockito.mockStatic(ElasticSearchUtil.class);
 		PowerMockito.when(ElasticSearchUtil.getDataByIdentifier(Mockito.any(), Mockito.any(),
 				Mockito.any())).thenReturn(user_org);
@@ -109,36 +120,8 @@ public class UserAssignRoleTest {
 		PowerMockito.when(cassandraOperation.updateRecord(Mockito.any(),Mockito.any(),Mockito.any())).thenReturn(updateResponse);
 	}
 	
-	private static void initCassandraForFailure() {
-		Response dummy =new Response(); 
-		Map<String, Object> responseMap = new HashMap<>();
-		responseMap.put(Constants.RESPONSE, Collections.EMPTY_LIST);
-		dummy.getResult().putAll(responseMap);
-		PowerMockito.when(cassandraOperation.getRecordsByProperties(Mockito.any(), Mockito.any(), Mockito.any()))
-		.thenReturn(dummy);
-
-	}
-
 	@Test
-	public void testAssignRolesWithInvalidUserId() throws Exception {
-		TestKit probe = new TestKit(system);
-		ActorRef subject = system.actorOf(props);
-		Request reqObj = new Request();
-		reqObj.setOperation(ActorOperations.ASSIGN_ROLES.getValue());
-		Map<String, Object> request = new HashMap<String, Object>();
-		request.put(JsonKey.USER_ID, "dummy1");
-		request.put(JsonKey.ORGANISATION_ID, orgId);
-		request.put(JsonKey.ROLES, new ArrayList<>());
-		reqObj.setRequest(request);
-		
-		initCassandraForFailure();
-		subject.tell(reqObj, probe.getRef());
-		ProjectCommonException ex = probe.expectMsgClass(ACTOR_MAX_WAIT_DURATION, ProjectCommonException.class);
-		assertTrue(null != ex);
-	}
-	
-	@Test
-	public void testAssignRolesSuccess() throws Exception {
+	public void testAssignInvalidRolesFailure() throws Exception {
 		TestKit probe = new TestKit(system);
 		ActorRef subject = system.actorOf(props);
 		Request reqObj = new Request();
@@ -146,7 +129,26 @@ public class UserAssignRoleTest {
 		Map<String, Object> request = new HashMap<String, Object>();
 		request.put(JsonKey.USER_ID, userId);
 		request.put(JsonKey.ORGANISATION_ID, orgId);
-		request.put(JsonKey.ROLES, new ArrayList<>());
+		request.put(JsonKey.ROLES, new ArrayList<>(Arrays.asList("DUMMY_ROLE")));
+		reqObj.setRequest(request);
+		
+		initCassandraForSuccess();
+		subject.tell(reqObj, probe.getRef());
+		ProjectCommonException ex = probe.expectMsgClass(ACTOR_MAX_WAIT_DURATION, ProjectCommonException.class);
+		assertTrue(null != ex);
+	}
+	
+	@Test
+	public void testAssignValidRolesSuccess() throws Exception {
+		TestKit probe = new TestKit(system);
+		ActorRef subject = system.actorOf(props);
+		Request reqObj = new Request();
+		reqObj.setOperation(ActorOperations.ASSIGN_ROLES.getValue());
+		Map<String, Object> request = new HashMap<String, Object>();
+		request.put(JsonKey.USER_ID, userId);
+		request.put(JsonKey.ORGANISATION_ID, orgId);
+		List<String> roles= new ArrayList<>(Arrays.asList("CONTENT_CREATOR", "COURSE_MENTOR"));
+		request.put(JsonKey.ROLES, roles);
 		reqObj.setRequest(request);
 		
 		initCassandraForSuccess();
@@ -154,4 +156,44 @@ public class UserAssignRoleTest {
 		Response res = probe.expectMsgClass(ACTOR_MAX_WAIT_DURATION, Response.class);
 		assertTrue(null != res);
 	}
+		
+	@Test
+	public void testAssignEmptyRoleSuccess() throws Exception {
+		TestKit probe = new TestKit(system);
+		ActorRef subject = system.actorOf(props);
+		Request reqObj = new Request();
+		reqObj.setOperation(ActorOperations.ASSIGN_ROLES.getValue());
+		Map<String, Object> request = new HashMap<String, Object>();
+		request.put(JsonKey.USER_ID, userId);
+		request.put(JsonKey.ORGANISATION_ID, orgId);
+		List<String> roles= new ArrayList<>();
+		request.put(JsonKey.ROLES, roles);
+		reqObj.setRequest(request);
+		
+		initCassandraForSuccess();
+		subject.tell(reqObj, probe.getRef());
+		Response res = probe.expectMsgClass(ACTOR_MAX_WAIT_DURATION, Response.class);
+		assertTrue(null != res);
+	}
+	
+	@Test
+	public void testAssignValidRolesSuccessWithoutOrgID() throws Exception {
+		TestKit probe = new TestKit(system);
+		ActorRef subject = system.actorOf(props);
+		Request reqObj = new Request();
+		reqObj.setOperation(ActorOperations.ASSIGN_ROLES.getValue());
+		Map<String, Object> request = new HashMap<String, Object>();
+		request.put(JsonKey.USER_ID, userId);
+		request.put(JsonKey.EXTERNAL_ID, externalId);
+		request.put(JsonKey.PROVIDER, provider);
+		List<String> roles= new ArrayList<>(Arrays.asList("CONTENT_CREATOR", "COURSE_MENTOR"));
+		request.put(JsonKey.ROLES, roles);
+		reqObj.setRequest(request);
+		
+		initCassandraForSuccess();
+		subject.tell(reqObj, probe.getRef());
+		Response res = probe.expectMsgClass(ACTOR_MAX_WAIT_DURATION, Response.class);
+		assertTrue(null != res);
+	}
+
 }

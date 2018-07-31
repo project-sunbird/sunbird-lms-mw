682671ea551f5cbc0d14c6daba0dc4e7ea5b389b
loganathan.shanmugam@tarento.com <loganathan.shanmugam@tarento.com>
2018-07-31 09:44:28 +0530

    Issue #SB-4881 fix: test cases and code refactor

--- actors/init/src/main/java/org/sunbird/init/actors/SystemInitActor.java
+++ actors/init/src/main/java/org/sunbird/init/actors/SystemInitActor.java
@@ -2,9 +2,10 @@ package org.sunbird.init.actors;
 
 import static org.sunbird.learner.util.Util.isNotNull;
 import static org.sunbird.learner.util.Util.isNull;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
 import org.apache.commons.lang3.StringUtils;
 import org.sunbird.actor.core.BaseActor;
 import org.sunbird.actor.router.ActorConfig;
@@ -63,58 +64,58 @@ public class SystemInitActor extends BaseActor {
    */
   @SuppressWarnings("unchecked")
   private void systemInitRootOrg(Request actorMessage) {
-      ProjectLogger.log("systemInitRootOrg method call started");
-      SystemSetting initStatus = getRootOrgInitialisationStatus();
-      if (isNull(initStatus) || isNull(initStatus.getValue())) {
-        setRootOrgInitialisationStatus(JsonKey.STARTED);
-        checkAndCreateRootOrg(actorMessage);
-        setRootOrgInitialisationStatus(JsonKey.COMPLETED);
-      } else if (initStatus.getValue().equals(JsonKey.STARTED)) {
-        checkAndCreateRootOrg(actorMessage);
-        setRootOrgInitialisationStatus(JsonKey.COMPLETED);
-      } else if (initStatus.getValue().equals(JsonKey.COMPLETED)) {
-        ProjectCommonException.throwClientErrorException(
-                ResponseCode.systemAlreadyInitialised,
-                ResponseCode.systemAlreadyInitialised.getErrorMessage());
-      } else {
-        ProjectCommonException.throwServerErrorException(
-                ResponseCode.internalError,
-                ResponseCode.internalError.getErrorMessage());
-      }
+    ProjectLogger.log("systemInitRootOrg method call started");
+    SystemSetting initStatus = getRootOrgInitialisationStatus();
+    if (isNull(initStatus) || isNull(initStatus.getValue())) {
+      setRootOrgInitialisationStatus(JsonKey.STARTED);
+      checkAndCreateRootOrg(actorMessage);
+      setRootOrgInitialisationStatus(JsonKey.COMPLETED);
+    } else if (initStatus.getValue().equals(JsonKey.STARTED)) {
+      checkAndCreateRootOrg(actorMessage);
+      setRootOrgInitialisationStatus(JsonKey.COMPLETED);
+    } else if (initStatus.getValue().equals(JsonKey.COMPLETED)) {
+      ProjectCommonException.throwClientErrorException(
+          ResponseCode.systemAlreadyInitialised,
+          ResponseCode.systemAlreadyInitialised.getErrorMessage());
+    } else {
+      ProjectLogger.log(
+          "SystemInitActor: systemInitRootOrg - error in rootOrg Init status "
+              + initStatus.getValue(),
+          LoggerEnum.DEBUG.name());
+      ProjectCommonException.throwServerErrorException(
+          ResponseCode.internalError, ResponseCode.internalError.getErrorMessage());
+    }
   }
-
-/**
- * This methods checks cassandra and returns rootOrg info if exists
- *
- * @return Map<String,Object> contains the orgData
- */
-private Map<String, Object>  getRootOrgIfExists(){
-  Util.DbInfo orgDbInfo = Util.dbInfoMap.get(JsonKey.ORG_DB);
-  Map<String, Object> queryMap = new HashMap<String, Object>();
-  queryMap.put(JsonKey.IS_ROOT_ORG,true);
-  Response response =
-          cassandraOperation.getRecordsByProperties(
-                  orgDbInfo.getKeySpace(),
-                  orgDbInfo.getTableName(),
-                  queryMap);
-  List<Map<String, Object>> orgList =
-          (List<Map<String, Object>>) response.get(JsonKey.RESPONSE);
-  if(orgList.size() > 0){
-    return orgList.get(0);
-  }
-  return null;
-}
 
   /**
- * This method checks for rootOrg exists in es or not
- * 
- * @return Boolean true if exists else false
- */
-  private boolean isRootOrgIndexExists() {
+   * This methods checks cassandra and returns rootOrg info if exists
+   *
+   * @return Map<String,Object> contains the orgData
+   */
+  private Map<String, Object> getRootOrgIfExists() {
+    Util.DbInfo orgDbInfo = Util.dbInfoMap.get(JsonKey.ORG_DB);
+    Map<String, Object> queryMap = new HashMap<String, Object>();
+    queryMap.put(JsonKey.IS_ROOT_ORG, true);
+    Response response =
+        cassandraOperation.getRecordsByProperties(
+            orgDbInfo.getKeySpace(), orgDbInfo.getTableName(), queryMap);
+    List<Map<String, Object>> orgList = (List<Map<String, Object>>) response.get(JsonKey.RESPONSE);
+    if (orgList.size() > 0) {
+      return orgList.get(0);
+    }
+    return null;
+  }
+
+  /**
+   * This method checks for rootOrg exists in es or not
+   *
+   * @return Boolean true if exists else false
+   */
+  private boolean getRootOrgInfoFromIndex() {
     Map<String, Object> filters = new HashMap<>();
     filters.put(JsonKey.IS_ROOT_ORG, true);
     Map<String, Object> esResult =
-            doElasticSearchComplexSearch(
+        doElasticSearchComplexSearch(
             filters, EsIndex.sunbird.getIndexName(), EsType.organisation.getTypeName());
     if (isNotNull(esResult)
         && esResult.containsKey(JsonKey.CONTENT)
@@ -131,10 +132,13 @@ private Map<String, Object>  getRootOrgIfExists(){
    * @param actorMessage actor data got create rootOrg
    * @return request object with org data
    */
-
-  private Map<String, Object> getFirstRootOrgParams(Request actorMessage){
+  private Map<String, Object> getFirstRootOrgParams(Request actorMessage) {
     Map<String, Object> req =
-            (Map<String, Object>) actorMessage.getRequest().get(JsonKey.ORGANISATION);
+        (Map<String, Object>) actorMessage.getRequest().get(JsonKey.ORGANISATION);
+    String uniqueId = ProjectUtil.getUniqueIdFromTimestamp(actorMessage.getEnv());
+    req.put(JsonKey.ID, uniqueId);
+    req.put(JsonKey.ROOT_ORG_ID, uniqueId);
+    req.put(JsonKey.HASHTAGID, uniqueId);
     req.put(JsonKey.CREATED_BY, JsonKey.INITIALISER);
     req.put(JsonKey.CREATED_DATE, ProjectUtil.getFormattedDate());
     req.put(JsonKey.STATUS, ProjectUtil.OrgStatus.ACTIVE.getValue());
@@ -145,9 +149,10 @@ private Map<String, Object>  getRootOrgIfExists(){
 
   /**
    * This method saves the orgData into elastic search
+   *
    * @param req request data to save organisation into ES
    */
-  private void saveRootOrgInfoToElastic(Map<String, Object> req) {
+  private void saveRootOrgInfoToIndex(Map<String, Object> req) {
     Request orgIndexReq = new Request();
     orgIndexReq.getRequest().put(JsonKey.ORGANISATION, req);
     orgIndexReq.setOperation(ActorOperations.INSERT_ORG_INFO_ELASTIC.getValue());
@@ -156,44 +161,42 @@ private Map<String, Object>  getRootOrgIfExists(){
   }
 
   /**
- * This method creates rootOrg if it is not exists already
- * 
- * @param actorMessage instance of Request class with rootOrg data.
- */
+   * This method creates rootOrg if it is not exists already
+   *
+   * @param actorMessage instance of Request class with rootOrg data.
+   */
   private void checkAndCreateRootOrg(Request actorMessage) {
     Map<String, Object> rootOrgDetails = getRootOrgIfExists();
     Response result = new Response();
     Map<String, Object> req = getFirstRootOrgParams(actorMessage);
-    String uniqueId = ProjectUtil.getUniqueIdFromTimestamp(actorMessage.getEnv());
+    String uniqueId = (String) req.get(JsonKey.ID);
     if (isNull(rootOrgDetails)) {
       validateChannelForUniqueness(req);
-      req.put(JsonKey.ID, uniqueId);
-      req.put(JsonKey.ROOT_ORG_ID, uniqueId);
-      req.put(JsonKey.HASHTAGID, uniqueId);
       Util.DbInfo orgDbInfo = Util.dbInfoMap.get(JsonKey.ORG_DB);
       String slug = Slug.makeSlug((String) req.get(JsonKey.CHANNEL), true);
       if (isSlugUnique(slug)) {
         req.put(JsonKey.SLUG, slug);
       } else {
         ProjectCommonException.throwClientErrorException(
-                ResponseCode.slugIsNotUnique, ResponseCode.slugIsNotUnique.getErrorMessage());
+            ResponseCode.slugIsNotUnique, ResponseCode.slugIsNotUnique.getErrorMessage());
       }
 
       boolean isChannelRegistered = Util.registerChannel(req);
       if (!isChannelRegistered) {
         ProjectCommonException.throwServerErrorException(
-                ResponseCode.channelRegFailed, ResponseCode.channelRegFailed.getErrorMessage());
+            ResponseCode.channelRegFailed, ResponseCode.channelRegFailed.getErrorMessage());
       }
-      result = cassandraOperation.insertRecord(orgDbInfo.getKeySpace(), orgDbInfo.getTableName(), req);
+      result =
+          cassandraOperation.insertRecord(orgDbInfo.getKeySpace(), orgDbInfo.getTableName(), req);
       ProjectLogger.log("Org data saved into cassandra.Created org id is ----." + uniqueId);
       result.getResult().put(JsonKey.ORGANISATION_ID, uniqueId);
       sender().tell(result, self());
-      saveRootOrgInfoToElastic(req);
+      saveRootOrgInfoToIndex(req);
     } else {
       result.getResult().put(JsonKey.ORGANISATION_ID, rootOrgDetails.get(JsonKey.ID));
       sender().tell(result, self());
-      if (!isRootOrgIndexExists()) {
-        saveRootOrgInfoToElastic(rootOrgDetails);
+      if (!getRootOrgInfoFromIndex()) {
+        saveRootOrgInfoToIndex(rootOrgDetails);
       }
     }
   }
@@ -210,7 +213,7 @@ private Map<String, Object>  getRootOrgIfExists(){
       filters.put(JsonKey.SLUG, slug);
       filters.put(JsonKey.IS_ROOT_ORG, true);
       Map<String, Object> esResult =
-              doElasticSearchComplexSearch(
+          doElasticSearchComplexSearch(
               filters, EsIndex.sunbird.getIndexName(), EsType.organisation.getTypeName());
       if (isNotNull(esResult)
           && esResult.containsKey(JsonKey.CONTENT)
@@ -221,34 +224,34 @@ private Map<String, Object>  getRootOrgIfExists(){
     return false;
   }
 
-/**
- * This method sets the 'isRootOrgInitialised' flag in system settings
- * 
- * @param status value of initialisation status 'started' or 'completed'
- */
+  /**
+   * This method sets the 'isRootOrgInitialised' flag in system settings
+   *
+   * @param status value of initialisation status 'started' or 'completed'
+   */
   private void setRootOrgInitialisationStatus(String status) {
-    ProjectLogger.log("SystemInitActor: setRootOrgInitialisationStatus called", LoggerEnum.DEBUG.name());
+    ProjectLogger.log(
+        "SystemInitActor: setRootOrgInitialisationStatus called", LoggerEnum.DEBUG.name());
     for (int i = 0; i <= WRITE_SETTINGS_RETRIES_ALLOWED; i++) {
       try {
         this.systemSetting =
-                new SystemSetting(
-                        JsonKey.IS_ROOT_ORG_INITIALISED, JsonKey.IS_ROOT_ORG_INITIALISED, status);
+            new SystemSetting(
+                JsonKey.IS_ROOT_ORG_INITIALISED, JsonKey.IS_ROOT_ORG_INITIALISED, status);
         Response response = systemSettingService.setSetting(systemSetting);
         ProjectLogger.log(
-                "Insert operation result for initialised status =  "
-                        + response.getResult().get(JsonKey.RESPONSE),
-                LoggerEnum.DEBUG.name());
+            "Insert operation result for initialised status =  "
+                + response.getResult().get(JsonKey.RESPONSE),
+            LoggerEnum.DEBUG.name());
         break;
       } catch (Exception e) {
         ProjectLogger.log(
-                "Exception while updating rootOrg init status. "+e.getMessage(),
-                LoggerEnum.DEBUG.name());
+            "Exception while updating rootOrg init status. " + e.getMessage(),
+            LoggerEnum.DEBUG.name());
         if (i == WRITE_SETTINGS_RETRIES_ALLOWED) {
           ProjectLogger.log(
-                  "Max retries reached while updating rootOrg init status.",
-                  LoggerEnum.DEBUG.name());
+              "Max retries reached while updating rootOrg init status.", LoggerEnum.DEBUG.name());
           ProjectCommonException.throwServerErrorException(
-                  ResponseCode.SERVER_ERROR, ResponseCode.SERVER_ERROR.getErrorMessage());
+              ResponseCode.SERVER_ERROR, ResponseCode.SERVER_ERROR.getErrorMessage());
         }
       }
     }
@@ -257,11 +260,12 @@ private Map<String, Object>  getRootOrgIfExists(){
   /**
    * This method gets the isRootOrgInitalised System setting if it already exists
    *
-   *  @return SystemSetting instance of SystemSetting class with id,field,value
+   * @return SystemSetting instance of SystemSetting class with id,field,value
    */
   private SystemSetting getRootOrgInitialisationStatus() {
     SystemSetting initSetting = null;
-    ProjectLogger.log("SystemInitActor: getRootOrgInitialisationStatus called", LoggerEnum.DEBUG.name());
+    ProjectLogger.log(
+        "SystemInitActor: getRootOrgInitialisationStatus called", LoggerEnum.DEBUG.name());
     try {
       initSetting = systemSettingService.readSetting(JsonKey.IS_ROOT_ORG_INITIALISED);
     } catch (Exception e) {
@@ -282,19 +286,18 @@ private Map<String, Object>  getRootOrgIfExists(){
     filters.put(JsonKey.CHANNEL, channel);
     filters.put(JsonKey.IS_ROOT_ORG, true);
     Map<String, Object> esResult =
-            doElasticSearchComplexSearch(
-                    filters, EsIndex.sunbird.getIndexName(), EsType.organisation.getTypeName());
+        doElasticSearchComplexSearch(
+            filters, EsIndex.sunbird.getIndexName(), EsType.organisation.getTypeName());
     if (isNotNull(esResult)
-            && esResult.containsKey(JsonKey.CONTENT)
-            && isNotNull(esResult.get(JsonKey.CONTENT))
-            && ((List) esResult.get(JsonKey.CONTENT)).size() > 0) {
+        && esResult.containsKey(JsonKey.CONTENT)
+        && isNotNull(esResult.get(JsonKey.CONTENT))
+        && ((List) esResult.get(JsonKey.CONTENT)).size() > 0) {
       ProjectLogger.log("Channel validation failed");
       ProjectCommonException.throwClientErrorException(
-              ResponseCode.channelUniquenessInvalid,
-              ResponseCode.channelUniquenessInvalid.getErrorMessage());
+          ResponseCode.channelUniquenessInvalid,
+          ResponseCode.channelUniquenessInvalid.getErrorMessage());
     }
   }
-
 
   private Map<String, Object> doElasticSearchComplexSearch(
       Map<String, Object> filters, String index, String type) {
--- actors/init/src/test/java/org/sunbird/init/actors/SystemInitActorTest.java
+++ actors/init/src/test/java/org/sunbird/init/actors/SystemInitActorTest.java
@@ -7,102 +7,260 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.testkit.javadsl.TestKit;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
-import org.junit.Test;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.sunbird.actor.service.SunbirdMWService;
 import org.sunbird.cassandra.CassandraOperation;
 import org.sunbird.cassandraimpl.CassandraOperationImpl;
 import org.sunbird.common.ElasticSearchUtil;
+import org.sunbird.common.exception.ProjectCommonException;
+import org.sunbird.common.models.response.Response;
+import org.sunbird.common.models.util.ActorOperations;
+import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.models.util.ProjectUtil;
 import org.sunbird.common.request.Request;
+import org.sunbird.dto.SearchDTO;
 import org.sunbird.helper.ServiceFactory;
+import org.sunbird.init.model.SystemSetting;
 import org.sunbird.init.service.SystemSettingService;
 import org.sunbird.init.service.impl.SystemSettingServiceImpl;
 import org.sunbird.learner.util.Util;
 import scala.concurrent.duration.FiniteDuration;
 
-@Ignore
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({
-  SystemSettingServiceImpl.class,
   ElasticSearchUtil.class,
-  ProjectUtil.class,
-  SunbirdMWService.class,
-  ServiceFactory.class
+  CassandraOperationImpl.class,
+  ServiceFactory.class,
+  SystemSettingServiceImpl.class
 })
 @PowerMockIgnore({"javax.management.*", "javax.net.ssl.*", "javax.security.*"})
 public class SystemInitActorTest {
-
   private static final FiniteDuration ACTOR_MAX_WAIT_DURATION = duration("100 second");
   private ObjectMapper mapper = new ObjectMapper();
-  private static ActorSystem system;
-  private static CassandraOperation cassandraImpl;
-  private static Props props = null;
-  private static Util.DbInfo orgDB = null;
-  private String orgId = "";
-  private static SystemSettingService systemSettingService;
-  private static SystemSettingService systemSettingServiceImpl;
-  private static TestKit probe;
-  private static ActorRef subject;
-  private static Request actorMessage;
+  private ActorSystem system;
+  private Props props;
+  private TestKit probe;
+  private ActorRef subject;
+  private Request actorMessage;
+  private CassandraOperation cassandraOperation;
+  private SystemSettingService systemSettingService;
+  private SystemSetting systemSetting;
 
-  @BeforeClass
-  public static void setUp() {
+  @Before
+  public void setUp() {
     system = ActorSystem.create("system");
     probe = new TestKit(system);
     PowerMockito.mockStatic(ElasticSearchUtil.class);
-    PowerMockito.mockStatic(ProjectUtil.class);
     PowerMockito.mockStatic(ServiceFactory.class);
-    PowerMockito.mockStatic(SunbirdMWService.class);
-    systemSettingServiceImpl = PowerMockito.mock(SystemSettingServiceImpl.class);
-    cassandraImpl = PowerMockito.mock(CassandraOperationImpl.class);
+    cassandraOperation = PowerMockito.mock(CassandraOperationImpl.class);
+    systemSettingService = PowerMockito.mock(SystemSettingServiceImpl.class);
     props = Props.create(SystemInitActor.class);
     subject = system.actorOf(props);
     actorMessage = new Request();
+    systemSetting = new SystemSetting();
   }
 
   @Test
-  public void testSystemInitRootOrg() {}
+  public void systemInitRootOrgTest() throws IOException {
+    Response response = new Response();
+    Map<String, Object> respData = new HashMap<String, Object>();
+    respData.put(JsonKey.ID, "0125571436950241280");
+    response.put(JsonKey.RESPONSE, JsonKey.SUCCESS);
+    response.put(JsonKey.RESPONSE, respData);
+    Map<String, Object> data = new HashMap<String, Object>();
+    Map<String, Object> orgData = new HashMap<String, Object>();
+    orgData.put(JsonKey.ORG_NAME, "orgTest");
+    orgData.put(JsonKey.CHANNEL, "orgTestChannel");
+    data.put(JsonKey.ORGANISATION, orgData);
+
+    Util.DbInfo orgDbInfo = Util.dbInfoMap.get(JsonKey.ORG_DB);
+    Map<String, Object> queryMap = new HashMap<String, Object>();
+    queryMap.put(JsonKey.IS_ROOT_ORG, true);
+
+    Map<String, Object> filters = new HashMap<>();
+    filters.put(JsonKey.CHANNEL, orgData.get(JsonKey.CHANNEL));
+    filters.put(JsonKey.IS_ROOT_ORG, true);
+    PowerMockito.when(systemSettingService.readSetting(JsonKey.IS_ROOT_ORG_INITIALISED))
+        .thenReturn(null);
+    PowerMockito.when(ServiceFactory.getInstance()).thenReturn(cassandraOperation);
+    PowerMockito.when(
+            cassandraOperation.getRecordsByProperties(
+                orgDbInfo.getKeySpace(), orgDbInfo.getTableName(), queryMap))
+        .thenReturn(null);
+    PowerMockito.when(
+            cassandraOperation.insertRecord(
+                orgDbInfo.getKeySpace(), orgDbInfo.getTableName(), orgData))
+        .thenReturn(response);
+    SearchDTO searchDTO = new SearchDTO();
+    searchDTO.getAdditionalProperties().put(JsonKey.FILTERS, filters);
+    Map<String, Object> esData = new HashMap<String, Object>();
+    PowerMockito.when(
+            ElasticSearchUtil.complexSearch(
+                searchDTO,
+                ProjectUtil.EsIndex.sunbird.getIndexName(),
+                ProjectUtil.EsType.organisation.getTypeName()))
+        .thenReturn(null);
+    actorMessage.setOperation(ActorOperations.SYSTEM_INIT_ROOT_ORG.getValue());
+    actorMessage.getRequest().putAll(data);
+    subject.tell(actorMessage, probe.getRef());
+    Object resp = probe.expectMsgAnyClassOf(ACTOR_MAX_WAIT_DURATION, Response.class);
+    Assert.assertTrue(null != resp);
+  }
 
   @Test
-  public void testSystemInitRootOrgWithoutData() {}
+  public void systemInitRootOrgAlreadyInitialisedTest() throws IOException {
+
+    Map<String, Object> data = new HashMap<String, Object>();
+    Map<String, Object> orgData = new HashMap<String, Object>();
+    orgData.put(JsonKey.ORG_NAME, "orgTest");
+    orgData.put(JsonKey.CHANNEL, "orgTestChannel");
+    data.put(JsonKey.ORGANISATION, orgData);
+
+    Util.DbInfo orgDbInfo = Util.dbInfoMap.get(JsonKey.ORG_DB);
+    Map<String, Object> queryMap = new HashMap<String, Object>();
+    queryMap.put(JsonKey.IS_ROOT_ORG, true);
+
+    Map<String, Object> filters = new HashMap<>();
+    filters.put(JsonKey.CHANNEL, orgData.get(JsonKey.CHANNEL));
+    filters.put(JsonKey.IS_ROOT_ORG, true);
+    systemSetting.setId(JsonKey.IS_ROOT_ORG_INITIALISED);
+    systemSetting.setField(JsonKey.IS_ROOT_ORG_INITIALISED);
+    systemSetting.setValue(JsonKey.COMPLETED);
+    PowerMockito.when(systemSettingService.readSetting(JsonKey.IS_ROOT_ORG_INITIALISED))
+        .thenReturn(systemSetting);
+    PowerMockito.when(ServiceFactory.getInstance()).thenReturn(cassandraOperation);
+    PowerMockito.when(
+            cassandraOperation.getRecordsByProperties(
+                orgDbInfo.getKeySpace(), orgDbInfo.getTableName(), queryMap))
+        .thenReturn(null);
+    SearchDTO searchDTO = new SearchDTO();
+    searchDTO.getAdditionalProperties().put(JsonKey.FILTERS, filters);
+    Map<String, Object> esData = new HashMap<String, Object>();
+    PowerMockito.when(
+            ElasticSearchUtil.complexSearch(
+                searchDTO,
+                ProjectUtil.EsIndex.sunbird.getIndexName(),
+                ProjectUtil.EsType.organisation.getTypeName()))
+        .thenReturn(null);
+    actorMessage.setOperation(ActorOperations.SYSTEM_INIT_ROOT_ORG.getValue());
+    actorMessage.getRequest().putAll(data);
+    subject.tell(actorMessage, probe.getRef());
+    ProjectCommonException resp =
+        probe.expectMsgClass(ACTOR_MAX_WAIT_DURATION, ProjectCommonException.class);
+    Assert.assertTrue(null != resp);
+  }
 
   @Test
-  public void testSystemInitRootOrgwithSettingsInitStarted() {}
+  public void systemInitRootOrgStatusStartedOrgNotExistsTest() throws IOException {
+    Response response = new Response();
+    Map<String, Object> respData = new HashMap<String, Object>();
+    respData.put(JsonKey.ID, "0125571436950241280");
+    response.put(JsonKey.RESPONSE, JsonKey.SUCCESS);
+    response.put(JsonKey.RESPONSE, respData);
+    Map<String, Object> data = new HashMap<String, Object>();
+    Map<String, Object> orgData = new HashMap<String, Object>();
+    orgData.put(JsonKey.ORG_NAME, "orgTest");
+    orgData.put(JsonKey.CHANNEL, "orgTestChannel");
+    data.put(JsonKey.ORGANISATION, orgData);
+
+    Util.DbInfo orgDbInfo = Util.dbInfoMap.get(JsonKey.ORG_DB);
+    Map<String, Object> queryMap = new HashMap<String, Object>();
+    queryMap.put(JsonKey.IS_ROOT_ORG, true);
+
+    Map<String, Object> filters = new HashMap<>();
+    filters.put(JsonKey.CHANNEL, orgData.get(JsonKey.CHANNEL));
+    filters.put(JsonKey.IS_ROOT_ORG, true);
+    systemSetting.setId(JsonKey.IS_ROOT_ORG_INITIALISED);
+    systemSetting.setField(JsonKey.IS_ROOT_ORG_INITIALISED);
+    systemSetting.setValue(JsonKey.STARTED);
+    PowerMockito.when(systemSettingService.readSetting(JsonKey.IS_ROOT_ORG_INITIALISED))
+        .thenReturn(systemSetting);
+    PowerMockito.when(ServiceFactory.getInstance()).thenReturn(cassandraOperation);
+    PowerMockito.when(
+            cassandraOperation.getRecordsByProperties(
+                orgDbInfo.getKeySpace(), orgDbInfo.getTableName(), queryMap))
+        .thenReturn(null);
+    PowerMockito.when(
+            cassandraOperation.insertRecord(
+                orgDbInfo.getKeySpace(), orgDbInfo.getTableName(), orgData))
+        .thenReturn(response);
+    SearchDTO searchDTO = new SearchDTO();
+    searchDTO.getAdditionalProperties().put(JsonKey.FILTERS, filters);
+    Map<String, Object> esData = new HashMap<String, Object>();
+    PowerMockito.when(
+            ElasticSearchUtil.complexSearch(
+                searchDTO,
+                ProjectUtil.EsIndex.sunbird.getIndexName(),
+                ProjectUtil.EsType.organisation.getTypeName()))
+        .thenReturn(null);
+    actorMessage.setOperation(ActorOperations.SYSTEM_INIT_ROOT_ORG.getValue());
+    actorMessage.getRequest().putAll(data);
+    subject.tell(actorMessage, probe.getRef());
+    Response resp = probe.expectMsgClass(ACTOR_MAX_WAIT_DURATION, Response.class);
+    Assert.assertTrue(null != resp);
+  }
 
   @Test
-  public void testSystemInitRootOrgwithSettingsInitCompleted() {}
+  public void systemInitRootOrgStatusStartedOrgExistsTest() throws IOException {
+    Response response = new Response();
+    Map<String, Object> respData = new HashMap<String, Object>();
+    respData.put(JsonKey.ID, "0125571436950241280");
+    response.put(JsonKey.RESPONSE, JsonKey.SUCCESS);
+    response.put(JsonKey.RESPONSE, respData);
+    Map<String, Object> data = new HashMap<String, Object>();
+    Map<String, Object> orgData = new HashMap<String, Object>();
+    orgData.put(JsonKey.ORG_NAME, "orgTest");
+    orgData.put(JsonKey.CHANNEL, "orgTestChannel");
+    data.put(JsonKey.ORGANISATION, orgData);
 
-  @Test
-  public void testIsRootOrgExists() {}
+    Util.DbInfo orgDbInfo = Util.dbInfoMap.get(JsonKey.ORG_DB);
+    Map<String, Object> queryMap = new HashMap<String, Object>();
+    queryMap.put(JsonKey.IS_ROOT_ORG, true);
 
-  @Test
-  public void testIsRootOrgExistsFalse() {}
+    Map<String, Object> filters = new HashMap<>();
+    filters.put(JsonKey.CHANNEL, orgData.get(JsonKey.CHANNEL));
+    filters.put(JsonKey.IS_ROOT_ORG, true);
+    systemSetting.setId(JsonKey.IS_ROOT_ORG_INITIALISED);
+    systemSetting.setField(JsonKey.IS_ROOT_ORG_INITIALISED);
+    systemSetting.setValue(JsonKey.STARTED);
 
-  @Test
-  public void testCheckAndCreateRootOrgWithOrgNotExists() {}
+    Response orgRespData = new Response();
+    orgRespData.put(JsonKey.ID, "0125571436950241280");
+    orgRespData.put(JsonKey.ORG_NAME, orgData.get(JsonKey.ORG_NAME));
+    orgRespData.put(JsonKey.CHANNEL, orgData.get(JsonKey.CHANNEL));
+    orgRespData.put(JsonKey.IS_ROOT_ORG, true);
 
-  @Test
-  public void testCheckAndCreateRootOrgWithOrgExists() {}
-
-  @Test
-  public void testsetInitialisationStatus() {}
-
-  @Test
-  public void testsetInitialisationStatusWithRetry() {}
-
-  @Test
-  public void testsetInitialisationStatusWithRetryFail() {}
-
-  @Test
-  public void testgetInitialisationStatus() {}
-
-  @Test
-  public void testgetInitialisationStatusError() {}
+    PowerMockito.when(systemSettingService.readSetting(JsonKey.IS_ROOT_ORG_INITIALISED))
+        .thenReturn(systemSetting);
+    PowerMockito.when(ServiceFactory.getInstance()).thenReturn(cassandraOperation);
+    PowerMockito.when(
+            cassandraOperation.getRecordsByProperties(
+                orgDbInfo.getKeySpace(), orgDbInfo.getTableName(), queryMap))
+        .thenReturn(orgRespData);
+    PowerMockito.when(
+            cassandraOperation.insertRecord(
+                orgDbInfo.getKeySpace(), orgDbInfo.getTableName(), orgData))
+        .thenReturn(response);
+    SearchDTO searchDTO = new SearchDTO();
+    searchDTO.getAdditionalProperties().put(JsonKey.FILTERS, filters);
+    Map<String, Object> esData = new HashMap<String, Object>();
+    PowerMockito.when(
+            ElasticSearchUtil.complexSearch(
+                searchDTO,
+                ProjectUtil.EsIndex.sunbird.getIndexName(),
+                ProjectUtil.EsType.organisation.getTypeName()))
+        .thenReturn(null);
+    actorMessage.setOperation(ActorOperations.SYSTEM_INIT_ROOT_ORG.getValue());
+    actorMessage.getRequest().putAll(data);
+    subject.tell(actorMessage, probe.getRef());
+    Response resp = probe.expectMsgClass(ACTOR_MAX_WAIT_DURATION, Response.class);
+    Assert.assertTrue(null != resp);
+  }
 }

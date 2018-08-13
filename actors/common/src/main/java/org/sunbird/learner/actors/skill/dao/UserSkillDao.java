5ab7192fe2c44b071ea2d20122c21a4587fadaf7
Rajat <rajat@mantralabsglobal.com>
2018-08-13 13:50:00 +0530

    review changes

--- actors/common/src/main/java/org/sunbird/learner/actors/skill/dao/UserSkillDao.java
+++ actors/common/src/main/java/org/sunbird/learner/actors/skill/dao/UserSkillDao.java
@@ -1,25 +1,13 @@
 package org.sunbird.learner.actors.skill.dao;
 
-
 import java.util.List;
 import java.util.Map;
 
 public interface UserSkillDao {
 
-    /**
-     * This method will add skills for user
-     * ProjectCommonException.
-     *
-     *
-     */
-    void addUserSkill(Map<String, Object> userSkill);
+  /** This method will add skills for user ProjectCommonException. */
+  void add(Map<String, Object> userSkill);
 
-    /**
-     * This method will delete skill for user
-     * ProjectCommonException.
-     *
-     *
-     */
-    boolean deleteUserSkill(List<String> identifierList);
-
+  /** This method will delete skill for user ProjectCommonException. */
+  boolean delete(List<String> identifierList);
 }
--- actors/common/src/main/java/org/sunbird/learner/actors/skill/dao/impl/UserSkillDaoImpl.java
+++ actors/common/src/main/java/org/sunbird/learner/actors/skill/dao/impl/UserSkillDaoImpl.java
@@ -1,36 +1,35 @@
 package org.sunbird.learner.actors.skill.dao.impl;
 
+import java.util.List;
+import java.util.Map;
 import org.sunbird.cassandra.CassandraOperation;
 import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.helper.ServiceFactory;
 import org.sunbird.learner.actors.skill.dao.UserSkillDao;
 import org.sunbird.learner.util.Util;
 
-import java.util.List;
-import java.util.Map;
-
 public class UserSkillDaoImpl implements UserSkillDao {
 
-    private CassandraOperation cassandraOperation = ServiceFactory.getInstance();
-    private Util.DbInfo userSkillDbInfo = Util.dbInfoMap.get(JsonKey.USER_SKILL_DB);
-    static UserSkillDao userSkillDao;
+  private CassandraOperation cassandraOperation = ServiceFactory.getInstance();
+  private Util.DbInfo userSkillDbInfo = Util.dbInfoMap.get(JsonKey.USER_SKILL_DB);
+  static UserSkillDao userSkillDao;
 
-    public static UserSkillDao getInstance(){
-        if(userSkillDao == null){
-            userSkillDao = new UserSkillDaoImpl();
-        }
-
-        return userSkillDao;
+  public static UserSkillDao getInstance() {
+    if (userSkillDao == null) {
+      userSkillDao = new UserSkillDaoImpl();
     }
+    return userSkillDao;
+  }
 
-    @Override
-    public void addUserSkill(Map<String, Object> userSkill) {
-        cassandraOperation.insertRecord(
-                userSkillDbInfo.getKeySpace(), userSkillDbInfo.getTableName(), userSkill);
-    }
+  @Override
+  public void add(Map<String, Object> userSkill) {
+    cassandraOperation.insertRecord(
+        userSkillDbInfo.getKeySpace(), userSkillDbInfo.getTableName(), userSkill);
+  }
 
-    @Override
-    public boolean deleteUserSkill(List<String> idList) {
-        return cassandraOperation.deleteRecord(userSkillDbInfo.getKeySpace(), userSkillDbInfo.getTableName(), idList);
-    }
+  @Override
+  public boolean delete(List<String> idList) {
+    return cassandraOperation.deleteRecord(
+        userSkillDbInfo.getKeySpace(), userSkillDbInfo.getTableName(), idList);
+  }
 }

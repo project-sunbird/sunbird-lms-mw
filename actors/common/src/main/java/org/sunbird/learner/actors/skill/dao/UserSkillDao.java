cefe8353de3752c8ed8026bd6ad007a214c42629
Rajat <rajat@mantralabsglobal.com>
2018-08-14 11:27:09 +0530

    add actor for skill endorsement

--- actors/common/src/main/java/org/sunbird/learner/actors/skill/dao/UserSkillDao.java
+++ actors/common/src/main/java/org/sunbird/learner/actors/skill/dao/UserSkillDao.java
@@ -1,25 +1,19 @@
 package org.sunbird.learner.actors.skill.dao;
 
-
 import java.util.List;
 import java.util.Map;
+import org.sunbird.models.user.skill.Skill;
 
 public interface UserSkillDao {
 
-    /**
-     * This method will add skills for user
-     * ProjectCommonException.
-     *
-     *
-     */
-    void addUserSkill(Map<String, Object> userSkill);
+  /** This method will add skills for user ProjectCommonException. */
+  void addUserSkill(Map<String, Object> userSkill);
 
-    /**
-     * This method will delete skill for user
-     * ProjectCommonException.
-     *
-     *
-     */
-    boolean deleteUserSkill(List<String> identifierList);
+  /** This method will delete skill for user ProjectCommonException. */
+  boolean deleteUserSkill(List<String> identifierList);
 
+  /** This method will read skill for user ProjectCommonException. */
+  Skill read(String id);
+  /** This method will update skill for user ProjectCommonException. */
+  void update(Skill skill);
 }
--- actors/common/src/main/java/org/sunbird/learner/actors/skill/dao/impl/UserSkillDaoImpl.java
+++ actors/common/src/main/java/org/sunbird/learner/actors/skill/dao/impl/UserSkillDaoImpl.java
@@ -1,36 +1,59 @@
 package org.sunbird.learner.actors.skill.dao.impl;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.sunbird.cassandra.CassandraOperation;
 import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.helper.ServiceFactory;
 import org.sunbird.learner.actors.skill.dao.UserSkillDao;
 import org.sunbird.learner.util.Util;
-
-import java.util.List;
-import java.util.Map;
+import org.sunbird.models.user.skill.Skill;
 
 public class UserSkillDaoImpl implements UserSkillDao {
 
-    private CassandraOperation cassandraOperation = ServiceFactory.getInstance();
-    private Util.DbInfo userSkillDbInfo = Util.dbInfoMap.get(JsonKey.USER_SKILL_DB);
-    static UserSkillDao userSkillDao;
+  private CassandraOperation cassandraOperation = ServiceFactory.getInstance();
+  private Util.DbInfo userSkillDbInfo = Util.dbInfoMap.get(JsonKey.USER_SKILL_DB);
+  static UserSkillDao userSkillDao;
 
-    public static UserSkillDao getInstance(){
-        if(userSkillDao == null){
-            userSkillDao = new UserSkillDaoImpl();
-        }
-
-        return userSkillDao;
+  public static UserSkillDao getInstance() {
+    if (userSkillDao == null) {
+      userSkillDao = new UserSkillDaoImpl();
     }
 
-    @Override
-    public void addUserSkill(Map<String, Object> userSkill) {
-        cassandraOperation.insertRecord(
-                userSkillDbInfo.getKeySpace(), userSkillDbInfo.getTableName(), userSkill);
-    }
+    return userSkillDao;
+  }
 
-    @Override
-    public boolean deleteUserSkill(List<String> idList) {
-        return cassandraOperation.deleteRecord(userSkillDbInfo.getKeySpace(), userSkillDbInfo.getTableName(), idList);
-    }
+  @Override
+  public void addUserSkill(Map<String, Object> userSkill) {
+    cassandraOperation.insertRecord(
+        userSkillDbInfo.getKeySpace(), userSkillDbInfo.getTableName(), userSkill);
+  }
+
+  @Override
+  public boolean deleteUserSkill(List<String> idList) {
+    return cassandraOperation.deleteRecord(
+        userSkillDbInfo.getKeySpace(), userSkillDbInfo.getTableName(), idList);
+  }
+
+  @Override
+  public Skill read(String id) {
+    ObjectMapper objectMapper = new ObjectMapper();
+    return objectMapper.convertValue(
+        cassandraOperation.getRecordById(
+            userSkillDbInfo.getKeySpace(), userSkillDbInfo.getTableName(), id),
+        Skill.class);
+  }
+
+  @Override
+  public void update(Skill skill) {
+    ObjectMapper objectMapper = new ObjectMapper();
+    TypeReference<HashMap<String, String>> typeRef =
+        new TypeReference<HashMap<String, String>>() {};
+    HashMap<String, Object> map = objectMapper.convertValue(skill, typeRef);
+    cassandraOperation.updateRecord(
+        userSkillDbInfo.getKeySpace(), userSkillDbInfo.getTableName(), map);
+  }
 }

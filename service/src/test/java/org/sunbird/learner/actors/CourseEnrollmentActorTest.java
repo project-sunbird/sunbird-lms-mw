9b11de6ba4aa5d6cf466c8dec61e10e6eb4a37eb
sudhirgiri2911 <sudhirgiri2911@gmail.com>
2018-10-04 11:03:50 +0530

    Issue SB:7024 feat: Enroll Actor unit Test

--- service/src/test/java/org/sunbird/learner/actors/CourseEnrollmentActorTest.java
+++ service/src/test/java/org/sunbird/learner/actors/CourseEnrollmentActorTest.java
@@ -1,5 +1,6 @@
 package org.sunbird.learner.actors;
 
+import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.when;
 
 import akka.actor.ActorRef;
@@ -7,249 +8,248 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.testkit.javadsl.TestKit;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.sunbird.cassandra.CassandraOperation;
+import org.sunbird.cassandraimpl.CassandraOperationImpl;
 import org.sunbird.common.exception.ProjectCommonException;
 import org.sunbird.common.models.response.Response;
 import org.sunbird.common.models.util.ActorOperations;
 import org.sunbird.common.models.util.JsonKey;
-import org.sunbird.common.models.util.datasecurity.OneWayHashing;
+import org.sunbird.common.models.util.ProjectUtil.ProgressStatus;
 import org.sunbird.common.request.Request;
 import org.sunbird.common.responsecode.ResponseCode;
 import org.sunbird.helper.ServiceFactory;
 import org.sunbird.learner.actors.coursebatch.CourseEnrollmentActor;
 import org.sunbird.learner.util.EkStepRequestUtil;
-import org.sunbird.learner.util.Util;
 
 /** @author arvind */
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+/** @author sudhirgiri */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(EkStepRequestUtil.class)
+@PrepareForTest({EkStepRequestUtil.class, ServiceFactory.class})
 @PowerMockIgnore("javax.management.*")
 public class CourseEnrollmentActorTest {
 
   private static ActorSystem system;
-  private ActorRef subject = system.actorOf(props);
-  private TestKit probe = new TestKit(system);
   private static final Props props = Props.create(CourseEnrollmentActor.class);
-  private static Util.DbInfo userCoursesdbInfo = null;
-  private static CassandraOperation cassandraOperation = ServiceFactory.getInstance();
-  private static String batchId = "115zguf934fy80fui";
-  private static final String courseId = "do_212282810555342848180";
-  private static Util.DbInfo batchdbInfo = Util.dbInfoMap.get(JsonKey.COURSE_BATCH_DB);
+  private static CassandraOperation cassandraOperation;
+  private static String userId = "someUserId";
+  private static String batchId = "someBatchId";
+  private static String courseId = "someCourseId";
+  private static String id = "someid";
+  private static String courseName = "someCourseName";
+  private static String courseDescription = "someCourseDescription";
+  private static String courseAppIcon = "somecourseAppIcon";
+  SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
   @BeforeClass
   public static void setUp() {
     system = ActorSystem.create("system");
-    Util.checkCassandraDbConnections(JsonKey.SUNBIRD);
-    userCoursesdbInfo = Util.dbInfoMap.get(JsonKey.LEARNER_COURSE_DB);
-    // PowerMockito.mockStatic(EkStepRequestUtil.class);
-    insertBatch();
+    PowerMockito.mockStatic(EkStepRequestUtil.class);
   }
 
-  private static void insertBatch() {
-
-    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-    Map<String, Object> batchMap = new HashMap<String, Object>();
-    batchMap.put(JsonKey.ID, batchId);
-    batchMap.put(JsonKey.STATUS, 1);
-    batchMap.put(JsonKey.COURSE_ID, courseId);
-    batchMap.put(JsonKey.CREATED_DATE, (String) format.format(new Date()));
-    batchMap.put(JsonKey.START_DATE, (String) format.format(new Date()));
-    Calendar now = Calendar.getInstance();
-    now.add(Calendar.DAY_OF_MONTH, 5);
-    Date after5Days = now.getTime();
-    batchMap.put(JsonKey.END_DATE, (String) format.format(after5Days));
-
-    cassandraOperation.insertRecord(
-        batchdbInfo.getKeySpace(), batchdbInfo.getTableName(), batchMap);
+  @Before
+  public void beforeEachTest() {
+    PowerMockito.mockStatic(ServiceFactory.class);
+    cassandraOperation = mock(CassandraOperationImpl.class);
+    when(ServiceFactory.getInstance()).thenReturn(cassandraOperation);
+    Mockito.reset(cassandraOperation);
   }
 
   @Test
-  public void testAll() throws Throwable {
-    test1OnReceiveTestWithEnrollOperation();
-    test2EnrollWithAlreadyEnrolled();
-    test3OnReceiveTestWithUnenrollOperation();
-    test4OnReceiveTestWithInvalidOperation();
-    test5onReceiveTestWithInvalidEkStepContent();
-    test6UnenrollWithoutEnroll();
-    test7onReceiveTestWithInvalidRequestType();
-    test8WithInvalidCourseBatchId();
-  }
+  public void testEnrollCourseSuccess() {
+    TestKit probe = new TestKit(system);
+    ActorRef subject = system.actorOf(props);
 
-  // @Test
-  public void test1OnReceiveTestWithEnrollOperation() {
-    Request reqObj = testOnReceive(ActorOperations.ENROLL_COURSE.getValue());
-    Response response = courseEnrollSuccess(reqObj);
+    Response insertResponse = createCassandraInsertSuccessResponse();
+    when(cassandraOperation.insertRecord(
+            Mockito.anyString(), Mockito.anyString(), Mockito.anyMap()))
+        .thenReturn(insertResponse);
+    mockCassandraRequestForReadRecordById(true, false, ProgressStatus.STARTED.getValue());
+    subject.tell(
+        createRequest(userId, batchId, courseId, ActorOperations.ENROLL_COURSE.getValue()),
+        probe.getRef());
+    Response response = probe.expectMsgClass(Response.class);
     Assert.assertTrue(null != response && response.getResponseCode() == ResponseCode.OK);
   }
 
-  // @Test
-  public void test2EnrollWithAlreadyEnrolled() {
-    Request reqObj = new Request();
-    reqObj.setRequestId("1");
-    reqObj.setOperation(ActorOperations.ENROLL_COURSE.getValue());
-    HashMap<String, Object> innerMap = new HashMap<>();
-    innerMap.put(JsonKey.COURSE_ID, "do_212282810555342848180");
-    innerMap.put(JsonKey.USER_ID, "USR");
-    innerMap.put(JsonKey.BATCH_ID, batchId);
-    reqObj.setRequest(innerMap);
-    ProjectCommonException exception = performRequest(reqObj);
+  @Test
+  public void testEnrollCourseFailureWithAlreadyEnrolled() {
+    TestKit probe = new TestKit(system);
+    ActorRef subject = system.actorOf(props);
+
+    mockCassandraRequestForReadRecordById(false, true, ProgressStatus.STARTED.getValue());
+    subject.tell(
+        createRequest(userId, batchId, courseId, ActorOperations.ENROLL_COURSE.getValue()),
+        probe.getRef());
+    ProjectCommonException exception = probe.expectMsgClass(ProjectCommonException.class);
     Assert.assertTrue(
-        ((ProjectCommonException) exception)
-            .getCode()
-            .equals(ResponseCode.userAlreadyEnrolledCourse.getErrorCode()));
+        null != exception
+            && exception.getResponseCode() == ResponseCode.CLIENT_ERROR.getResponseCode());
   }
 
-  // @Test
-  public void test3OnReceiveTestWithUnenrollOperation() {
-    Request reqObj = testOnReceive(ActorOperations.UNENROLL_COURSE.getValue());
-    Response response = courseEnrollSuccess(reqObj);
+  @Test
+  public void testEnrollCourseSuccessAfterUnenroll() {
+    TestKit probe = new TestKit(system);
+    ActorRef subject = system.actorOf(props);
+
+    Response insertResponse = createCassandraInsertSuccessResponse();
+    when(cassandraOperation.insertRecord(
+            Mockito.anyString(), Mockito.anyString(), Mockito.anyMap()))
+        .thenReturn(insertResponse);
+    mockCassandraRequestForReadRecordById(false, false, ProgressStatus.STARTED.getValue());
+    subject.tell(
+        createRequest(userId, batchId, courseId, ActorOperations.ENROLL_COURSE.getValue()),
+        probe.getRef());
+    Response response = probe.expectMsgClass(Response.class);
     Assert.assertTrue(null != response && response.getResponseCode() == ResponseCode.OK);
   }
 
-  // @Test
+  @Test
+  public void testUnenrollCourseSuccess() {
+    TestKit probe = new TestKit(system);
+    ActorRef subject = system.actorOf(props);
+
+    mockCassandraRequestForReadRecordById(false, true, ProgressStatus.STARTED.getValue());
+    subject.tell(
+        createRequest(userId, batchId, courseId, ActorOperations.UNENROLL_COURSE.getValue()),
+        probe.getRef());
+    Response response = probe.expectMsgClass(Response.class);
+    Assert.assertTrue(null != response && response.getResponseCode() == ResponseCode.OK);
+  }
+
+  @Test
   public void test4OnReceiveTestWithInvalidOperation() throws Throwable {
-    Request reqObj = testOnReceive("invalid-operation");
-    ProjectCommonException exc = performRequest(reqObj);
-    Assert.assertTrue(null != exc);
+    TestKit probe = new TestKit(system);
+    ActorRef subject = system.actorOf(props);
+    // Request reqObj = testOnReceive("invalid-operation");
+    // ProjectCommonException exc = performRequest(reqObj);
+    // Assert.assertTrue(null != exc);
   }
 
-  // @Test
-  public void test5onReceiveTestWithInvalidEkStepContent() {
+  @Test
+  public void testUnenrollCourseFailureWithoutEnroll() {
+    TestKit probe = new TestKit(system);
+    ActorRef subject = system.actorOf(props);
 
-    PowerMockito.mockStatic(EkStepRequestUtil.class);
-
-    Object[] arr = {};
-    Map<String, Object> ekstepMockResult = new HashMap<>();
-    ekstepMockResult.put(JsonKey.CONTENTS, arr);
-    when(EkStepRequestUtil.searchContent(Mockito.anyString(), Mockito.anyMap()))
-        .thenReturn(ekstepMockResult);
-
-    Request reqObj = new Request();
-    reqObj.setRequestId("1");
-    reqObj.setOperation(ActorOperations.ENROLL_COURSE.getValue());
-    reqObj.put(JsonKey.COURSE_ID, "do_212282810555342848180");
-    reqObj.put(JsonKey.USER_ID, "USR");
-    reqObj.put(JsonKey.BATCH_ID, batchId);
-    HashMap<String, Object> innerMap = new HashMap<>();
-    innerMap.put(JsonKey.COURSE, reqObj.getRequest());
-    innerMap.put(JsonKey.USER_ID, "USR");
-    innerMap.put(JsonKey.COURSE_ID, "do_212282810555342848180");
-    reqObj.setRequest(innerMap);
-
-    subject.tell(reqObj, probe.getRef());
-    probe.expectMsgClass(ProjectCommonException.class);
-  }
-
-  // @Test
-  public void test6UnenrollWithoutEnroll() {
-    Request reqObj = testOnReceive(ActorOperations.UNENROLL_COURSE.getValue());
-    ProjectCommonException exception = performRequest(reqObj);
-    Assert.assertTrue(
-        ((ProjectCommonException) exception)
-            .getCode()
-            .equals(ResponseCode.userNotEnrolledCourse.getErrorCode()));
-  }
-
-  // @Test()
-  public void test7onReceiveTestWithInvalidRequestType() throws Throwable {
-    Response resObj = new Response();
-    subject.tell(resObj, probe.getRef());
-    ProjectCommonException exception = probe.expectMsgClass(ProjectCommonException.class);
-    Assert.assertTrue(null != exception);
-    /*Assert.assertTrue(
-    ((ProjectCommonException) exception)
-    .getCode()
-    .equals(ResponseCode.invalidRequestData.getErrorCode()));*/
-  }
-
-  // @Test
-  public void test8WithInvalidCourseBatchId() {
-    Request reqObj = new Request();
-    reqObj.setRequestId("1");
-    reqObj.setOperation(ActorOperations.ENROLL_COURSE.getValue());
-    HashMap<String, Object> innerMap = new HashMap<>();
-    innerMap.put(JsonKey.COURSE_ID, "do_212282810555342848180");
-    innerMap.put(JsonKey.USER_ID, "USR");
-    innerMap.put(JsonKey.BATCH_ID, batchId + "0123");
-    reqObj.setRequest(innerMap);
-
-    subject.tell(reqObj, probe.getRef());
+    mockCassandraRequestForReadRecordById(false, false, ProgressStatus.STARTED.getValue());
+    subject.tell(
+        createRequest(userId, batchId, courseId, ActorOperations.UNENROLL_COURSE.getValue()),
+        probe.getRef());
     ProjectCommonException exception = probe.expectMsgClass(ProjectCommonException.class);
     Assert.assertTrue(
-        ((ProjectCommonException) exception)
-            .getCode()
-            .equals(ResponseCode.invalidCourseBatchId.getErrorCode()));
+        null != exception
+            && exception.getResponseCode() == ResponseCode.CLIENT_ERROR.getResponseCode());
   }
 
-  public static Request testOnReceive(String operation) {
-    Request reqObj = new Request();
-    reqObj.setRequestId("1");
-    reqObj.setOperation(operation);
-    if (operation.equals(ActorOperations.ENROLL_COURSE.getValue())
-        || operation.equals(ActorOperations.UNENROLL_COURSE.getValue())) {
-      HashMap<String, Object> innerMap = new HashMap<String, Object>();
-      // innerMap.put(JsonKey.COURSE, reqObj.getRequest());
-      innerMap.put(JsonKey.USER_ID, "USR");
-      innerMap.put(JsonKey.COURSE_ID, "do_212282810555342848180");
-      innerMap.put(JsonKey.BATCH_ID, batchId);
-      reqObj.setRequest(innerMap);
+  @Test
+  public void testUnenrollCourseFailureWithCourseAlreadyCompleted() {
+    TestKit probe = new TestKit(system);
+    ActorRef subject = system.actorOf(props);
 
-      PowerMockito.mockStatic(EkStepRequestUtil.class);
-      Map<String, Object> ekstepResponse = new HashMap<String, Object>();
-      ekstepResponse.put("count", 10);
-      Object[] arr = {ekstepResponse};
-      Map<String, Object> ekstepMockResult = new HashMap<>();
-      ekstepMockResult.put(JsonKey.CONTENTS, arr);
-      when(EkStepRequestUtil.searchContent(Mockito.anyString(), Mockito.anyMap()))
-          .thenReturn(ekstepMockResult);
-    }
-
-    // subject.tell(reqObj, probe.getRef());
-    // probe.expectMsgClass(duration("100 second"), Response.class);
-    return reqObj;
-  }
-
-  private ProjectCommonException performRequest(Request reqObj) {
-    subject.tell(reqObj, probe.getRef());
+    mockCassandraRequestForReadRecordById(false, false, ProgressStatus.COMPLETED.getValue());
+    subject.tell(
+        createRequest(userId, batchId, courseId, ActorOperations.UNENROLL_COURSE.getValue()),
+        probe.getRef());
     ProjectCommonException exception = probe.expectMsgClass(ProjectCommonException.class);
-    return exception;
+    Assert.assertTrue(
+        null != exception
+            && exception.getResponseCode() == ResponseCode.CLIENT_ERROR.getResponseCode());
   }
 
-  private Response courseEnrollSuccess(Request reqObj) {
-
-    subject.tell(reqObj, probe.getRef());
-    return probe.expectMsgClass(Response.class);
+  private Response createCassandraInsertSuccessResponse() {
+    Response response = new Response();
+    response.put(JsonKey.RESPONSE, JsonKey.SUCCESS);
+    return response;
   }
 
-  @AfterClass
-  public static void destroy() {
+  private void mockCassandraRequestForReadRecordById(
+      boolean firstTime, boolean active, int status) {
+    if (firstTime)
+      when(cassandraOperation.getRecordById(
+              Mockito.anyString(), Mockito.anyString(), Mockito.anyString()))
+          .thenReturn(createGetCourseBatchSuccessResponse(status))
+          .thenReturn(createGetUserCourseFailureResponse());
+    else
+      when(cassandraOperation.getRecordById(
+              Mockito.anyString(), Mockito.anyString(), Mockito.anyString()))
+          .thenReturn(createGetCourseBatchSuccessResponse(status))
+          .thenReturn(createGetUserCourseSuccessResponse(active));
+  }
 
-    cassandraOperation.deleteRecord(
-        userCoursesdbInfo.getKeySpace(),
-        userCoursesdbInfo.getTableName(),
-        OneWayHashing.encryptVal(
-            "USR"
-                + JsonKey.PRIMARY_KEY_DELIMETER
-                + "do_212282810555342848180"
-                + JsonKey.PRIMARY_KEY_DELIMETER
-                + batchId));
-    cassandraOperation.deleteRecord(batchdbInfo.getKeySpace(), batchdbInfo.getTableName(), batchId);
+  private Response createGetUserCourseFailureResponse() {
+    Response response = new Response();
+    List<Map<String, Object>> result = new ArrayList<>();
+    response.put(JsonKey.RESPONSE, result);
+    return response;
+  }
+
+  private Response createGetUserCourseSuccessResponse(boolean active) {
+    Response response = new Response();
+    Map<String, Object> userMap = new HashMap<>();
+    userMap.put(JsonKey.USER_ID, userId);
+    userMap.put(JsonKey.BATCH_ID, batchId);
+    userMap.put(JsonKey.COURSE_ID, courseId);
+    userMap.put(JsonKey.ACTIVE, active);
+    userMap.put(JsonKey.PROGRESS, 0);
+    userMap.put(JsonKey.ID, id);
+    List<Map<String, Object>> result = new ArrayList<>();
+    result.add(userMap);
+    response.put(JsonKey.RESPONSE, result);
+    return response;
+  }
+
+  private Response createGetCourseBatchSuccessResponse(int status) {
+    Response response = new Response();
+    Map<String, Object> userMap = new HashMap<>();
+    userMap.put(JsonKey.COURSE_ID, courseId);
+    userMap.put(JsonKey.ID, batchId);
+    userMap.put(JsonKey.STATUS, status);
+    userMap.put(JsonKey.START_DATE, calculateDate(0));
+    userMap.put(JsonKey.END_DATE, calculateDate(4));
+    Map<String, Object> courseInfo = new HashMap<>();
+    courseInfo.put(JsonKey.NAME, courseName);
+    courseInfo.put(JsonKey.DESCRIPTION, courseDescription);
+    courseInfo.put(JsonKey.APP_ICON, courseAppIcon);
+    userMap.put(JsonKey.COURSE_ADDITIONAL_INFO, courseInfo);
+    List<Map<String, Object>> result = new ArrayList<>();
+    result.add(userMap);
+    response.put(JsonKey.RESPONSE, result);
+    return response;
+  }
+
+  public Request createRequest(String userId, String batchId, String courseId, String operation) {
+    Request actorMessage = new Request();
+    HashMap<String, Object> innerMap = new HashMap<>();
+    innerMap.put(JsonKey.REQUESTED_BY, userId);
+    actorMessage.setContext(innerMap);
+    actorMessage.setOperation(operation);
+    HashMap<String, Object> requestBody = new HashMap<>();
+    requestBody.put(JsonKey.BATCH_ID, batchId);
+    requestBody.put(JsonKey.USER_ID, userId);
+    requestBody.put(JsonKey.COURSE_ID, courseId);
+    actorMessage.setRequest(requestBody);
+
+    return actorMessage;
+  }
+
+  private String calculateDate(int dayOffset) {
+
+    Calendar calender = Calendar.getInstance();
+    calender.add(Calendar.DAY_OF_MONTH, dayOffset);
+    return format.format(calender.getTime());
   }
 }

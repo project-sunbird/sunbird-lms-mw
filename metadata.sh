8b16428f09ef946443e09a9f98b3848531b1b5c2
Manzarul <manzarul07@gmail.com>
2018-10-16 11:23:42 +0530

    Issues #SB-00 fix: changes for build number from 1.11 to 1.12

--- metadata.sh
+++ metadata.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # return version
-echo '{"name":"actor-service","version":"1.11.0","org":"sunbird","hubuser":"purplesunbird"}'
+echo '{"name":"actor-service","version":"1.12.0","org":"sunbird","hubuser":"purplesunbird"}'

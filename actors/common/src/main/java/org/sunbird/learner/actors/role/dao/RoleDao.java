98d3dedb2c0eb01db3683dd6d1a5139009ebad06
sudhirgiri2911 <sudhirgiri2911@gmail.com>
2018-10-17 14:59:31 +0530

    Issue #SB-0000 feat: User Refactoring Code

---
+++ actors/common/src/main/java/org/sunbird/learner/actors/role/dao/RoleDao.java
@@ -0,0 +1,14 @@
+package org.sunbird.learner.actors.role.dao;
+
+import java.util.List;
+import org.sunbird.models.role.Role;
+
+public interface RoleDao {
+
+  /**
+   * Get All Records
+   *
+   * @return List of all Roles from Role Table
+   */
+  List<Role> getAllRecords();
+}

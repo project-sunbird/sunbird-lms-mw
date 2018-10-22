97c3cc7b5791d14892513dfc57994361945af72a
rajat <rajatgupta0889@gmail.com>
2018-10-22 16:43:36 +0530

    Issue #SB-4221 fix add orgBulk upload in conf and add logic to add first rootorg and then nonRootOrg

--- actors/common/src/main/java/org/sunbird/learner/actors/bulkupload/OrgBulkUploadBackGroundJobActor.java
+++ actors/common/src/main/java/org/sunbird/learner/actors/bulkupload/OrgBulkUploadBackGroundJobActor.java
@@ -2,11 +2,14 @@ package org.sunbird.learner.actors.bulkupload;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import java.io.IOException;
-import java.util.Map;
+import java.sql.Timestamp;
+import java.util.*;
+import java.util.function.Function;
 import org.apache.commons.lang3.StringUtils;
 import org.sunbird.actor.router.ActorConfig;
 import org.sunbird.actorutil.org.OrganisationClient;
 import org.sunbird.actorutil.org.impl.OrganisationClientImpl;
+import org.sunbird.common.Constants;
 import org.sunbird.common.exception.ProjectCommonException;
 import org.sunbird.common.models.util.*;
 import org.sunbird.common.request.ExecutionContext;
@@ -44,6 +47,59 @@ public class OrgBulkUploadBackGroundJobActor extends BaseBulkUploadBackGroundJob
     } else {
       onReceiveUnsupportedOperation("OrgBulkUploadBackGroundJobActor");
     }
+  }
+
+  @Override
+  public void processBulkUpload(BulkUploadProcess bulkUploadProcess, Function function) {
+    Integer sequence = 0;
+    Integer taskCount = bulkUploadProcess.getTaskCount();
+    List<Map<String, Object>> successList = new LinkedList<>();
+    List<Map<String, Object>> failureList = new LinkedList<>();
+    List<BulkUploadProcessTask> nonRootOrg = new ArrayList<>();
+    while (sequence <= taskCount) {
+      Integer nextSequence = sequence + CASSANDRA_BATCH_SIZE;
+      Map<String, Object> queryMap = new HashMap<>();
+      queryMap.put(JsonKey.PROCESS_ID, bulkUploadProcess.getId());
+      Map<String, Object> sequenceRange = new HashMap<>();
+      sequenceRange.put(Constants.GT, sequence);
+      sequenceRange.put(Constants.LTE, nextSequence);
+      queryMap.put(BulkUploadJsonKey.SEQUENCE_ID, sequenceRange);
+      List<BulkUploadProcessTask> tasks = bulkUploadProcessTaskDao.readByPrimaryKeys(queryMap);
+      for (BulkUploadProcessTask task : tasks) {
+        try {
+          if (task.getStatus() != null
+              && task.getStatus() != ProjectUtil.BulkProcessStatus.COMPLETED.getValue()) {
+            Map<String, Object> row = mapper.readValue(task.getData(), Map.class);
+            if (row.containsKey(JsonKey.IS_ROOT_ORG)
+                && row.get(JsonKey.IS_ROOT_ORG) != null
+                && Boolean.valueOf((String) row.get(JsonKey.IS_ROOT_ORG))) {
+              processOrg(task);
+              task.setLastUpdatedOn(new Timestamp(System.currentTimeMillis()));
+              task.setIterationId(task.getIterationId() + 1);
+            } else {
+              nonRootOrg.add(task);
+            }
+          }
+        } catch (Exception ex) {
+          task.setFailureResult(ex.getMessage());
+        }
+      }
+      if (!nonRootOrg.isEmpty()) {
+        for (BulkUploadProcessTask task : nonRootOrg) {
+          processOrg(task);
+          task.setLastUpdatedOn(new Timestamp(System.currentTimeMillis()));
+          task.setIterationId(task.getIterationId() + 1);
+        }
+      }
+      performBatchUpdate(tasks);
+      sequence = nextSequence;
+    }
+    ProjectLogger.log(
+        "OrgBulkUploadBackGroundJobActor : processBulkUpload process finished", LoggerEnum.INFO);
+    bulkUploadProcess.setSuccessResult(ProjectUtil.convertMapToJsonString(successList));
+    bulkUploadProcess.setFailureResult(ProjectUtil.convertMapToJsonString(failureList));
+    bulkUploadProcess.setStatus(ProjectUtil.BulkProcessStatus.COMPLETED.getValue());
+    bulkUploadDao.update(bulkUploadProcess);
   }
 
   private void processOrg(BulkUploadProcessTask task) {

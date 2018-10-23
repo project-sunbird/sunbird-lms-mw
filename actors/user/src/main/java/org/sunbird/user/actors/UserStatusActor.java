d23efc29f5c036b71715e235d356e29edf5e56b8
bvinayakumar <36028332+bvinayakumar@users.noreply.github.com>
2018-10-23 11:45:21 +0530

    Update UserStatusActor.java

--- actors/user/src/main/java/org/sunbird/user/actors/UserStatusActor.java
+++ actors/user/src/main/java/org/sunbird/user/actors/UserStatusActor.java
@@ -2,9 +2,7 @@ package org.sunbird.user.actors;
 
 import akka.actor.ActorRef;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import org.sunbird.actor.core.BaseActor;
 import org.sunbird.actor.router.ActorConfig;
@@ -29,13 +27,6 @@ import org.sunbird.user.dao.impl.UserDaoImpl;
 import org.sunbird.user.service.UserService;
 import org.sunbird.user.service.impl.UserServiceImpl;
 
-/**
- * This actor will handle user status operation .
- *
- * @author Manzarul
- * @author Amit Kumar
- * @author sudhirgiri
- */
 @ActorConfig(
   tasks = {"unblockUser", "blockUser"},
   asyncTasks = {}
@@ -50,7 +41,6 @@ public class UserStatusActor extends BaseActor {
   private ActorRef systemSettingActorRef =
       getActorRef(ActorOperations.GET_SYSTEM_SETTING.getValue());
 
-  /** Receives the actor message and perform the user status operation . */
   @Override
   public void onReceive(Request request) throws Throwable {
     Util.initializeContext(request, JsonKey.USER);
@@ -71,89 +61,84 @@ public class UserStatusActor extends BaseActor {
     }
   }
 
-  /**
-   * Method to block the user , it performs only soft delete from Cassandra , ES , Keycloak
-   *
-   * @param actorMessage
-   */
   private void blockUser(Request actorMessage) {
-    commonRequestHandler(actorMessage, true);
-  }
-  /**
-   * Method to un block the user
-   *
-   * @param actorMessage
-   */
-  private void unblockUser(Request actorMessage) {
-    commonRequestHandler(actorMessage, false);
+    updateUserStatus(actorMessage, true);
   }
 
-  private void commonRequestHandler(Request request, boolean canBlocked) {
+  private void unblockUser(Request actorMessage) {
+    updateUserStatus(actorMessage, false);
+  }
+
+  private void updateUserStatus(Request request, boolean isBlocked) {
     String operation = request.getOperation();
-    ProjectLogger.log("Method call  " + operation);
+    
     Util.getUserProfileConfig(systemSettingActorRef);
     String userId = (String) request.getRequest().get(JsonKey.USER_ID);
-    User user = userService.validateUserIdAndGetUserIfPresent(userId);
+    String logMsgPrefix = MessageFormat.format("UserStatusActor:updateUserStatus:{0}:{1}: ", operation, userId);
+    User user = userService.validateUserId(userId);
+
     if (operation.equals(ActorOperations.BLOCK_USER.getValue()) && user.getIsDeleted()) {
       throw new ProjectCommonException(
           ResponseCode.userAlreadyInactive.getErrorCode(),
           ResponseCode.userAlreadyInactive.getErrorMessage(),
           ResponseCode.CLIENT_ERROR.getResponseCode());
     }
+    
     if (operation.equals(ActorOperations.UNBLOCK_USER.getValue()) && !user.getIsDeleted()) {
       throw new ProjectCommonException(
           ResponseCode.userAlreadyActive.getErrorCode(),
           ResponseCode.userAlreadyActive.getErrorMessage(),
           ResponseCode.CLIENT_ERROR.getResponseCode());
     }
-    Map<String, Object> userMapES =
-        createUserMapES(
-            userId, (String) request.getContext().get(JsonKey.REQUESTED_BY), canBlocked);
+
+    Map<String, Object> userMapES = getUserMapES(userId, (String) request.getContext().get(JsonKey.REQUESTED_BY), isBlocked);
     User updatedUser = mapper.convertValue(userMapES, User.class);
+    
     if (isSSOEnabled) {
-      if (canBlocked) {
+      if (isBlocked) {
         ssoManager.deactivateUser(userMapES);
       } else {
         ssoManager.activateUser(userMapES);
       }
     }
+
     Response response = userDao.updateUser(updatedUser);
-    ProjectLogger.log(operation + " completed for " + userId);
     sender().tell(response, self());
-    // update record in elasticsearch ......
+
+    // Update status in ES
     if (((String) response.get(JsonKey.RESPONSE)).equalsIgnoreCase(JsonKey.SUCCESS)) {
-      ProjectLogger.log("UserManagementActor:" + operation + " : updating user data to ES.");
+      ProjectLogger.log(logMsgPrefix + "Update user data to ES.");
+      
       Request userRequest = new Request();
       userRequest.setOperation(ActorOperations.UPDATE_USER_INFO_ELASTIC.getValue());
       userRequest.getRequest().put(JsonKey.ID, userId);
+
       try {
         tellToAnother(userRequest);
-      } catch (Exception ex) {
-        ProjectLogger.log("UserStatusActor:" + operation + " : Exception occurred : ", ex);
+      } catch (Exception e) {
+        ProjectLogger.log(logMsgPrefix + "Exception occurred with error message = " + e.getMessage(), e);
       }
     } else {
-      ProjectLogger.log("UserStatusActor:" + operation + " : no call for ES to save user");
+      ProjectLogger.log(logMsgPrefix + "Update user data to ES is skipped.");
     }
-    generateTeleEventForUser(userId, operation);
+
+    generateTelemetry(null, userId, operation);
   }
 
-  private Map<String, Object> createUserMapES(String userId, String updatedBy, boolean isDeleted) {
-    Map<String, Object> dbMap = new HashMap<>();
-    dbMap.put(JsonKey.IS_DELETED, isDeleted);
-    if (isDeleted) {
-      dbMap.put(JsonKey.STATUS, Status.INACTIVE.getValue());
-    } else {
-      dbMap.put(JsonKey.STATUS, Status.ACTIVE.getValue());
-    }
-    dbMap.put(JsonKey.ID, userId);
-    dbMap.put(JsonKey.USER_ID, userId);
-    dbMap.put(JsonKey.UPDATED_DATE, ProjectUtil.getFormattedDate());
-    dbMap.put(JsonKey.UPDATED_BY, updatedBy);
+  private Map<String, Object> getUserMapES(String userId, String updatedBy, boolean isDeleted) {
+    Map<String, Object> esUserMap = new HashMap<>();
+    
+    esUserMap.put(JsonKey.IS_DELETED, isDeleted);
+    esUserMap.put(JsonKey.STATUS, isDeleted ? Status.INACTIVE.getValue() : Status.ACTIVE.getValue());
+    esUserMap.put(JsonKey.ID, userId);
+    esUserMap.put(JsonKey.USER_ID, userId);
+    esUserMap.put(JsonKey.UPDATED_DATE, ProjectUtil.getFormattedDate());
+    esUserMap.put(JsonKey.UPDATED_BY, updatedBy);
 
-    return dbMap;
+    return esUserMap;
   }
 
-  private void generateTeleEventForUser(String userId, String objectType) {
+  private void generateTelemetry(String userId, String objectType) {
     List<Map<String, Object>> correlatedObject = new ArrayList<>();
     Map<String, Object> targetObject =
         TelemetryUtil.generateTargetObject(userId, JsonKey.USER, JsonKey.UPDATE, null);
@@ -161,8 +146,10 @@ public class UserStatusActor extends BaseActor {
     if (objectType.equalsIgnoreCase("blockUser")) {
       telemetryAction.put("BlockUser", "user blocked");
     } else {
-      telemetryAction.put("UnBlockUser", "user unblocked");
+      telemetryAction.put("UnblockUser", "user unblocked");
     }
     TelemetryUtil.telemetryProcessingCall(telemetryAction, targetObject, correlatedObject);
   }
+
 }
+

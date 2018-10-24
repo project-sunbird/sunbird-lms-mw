4e624f487e7cf87ca01704a9fecae53a61ee668a
sudhirgiri2911 <sudhirgiri2911@gmail.com>
2018-10-24 13:13:37 +0530

    Issue #SB-8217 feat: Adding/Removing Participant, updating mentors from open batch

---
+++ actors/common/src/main/java/org/sunbird/learner/actors/coursebatch/service/CourseBatchService.java
@@ -0,0 +1,29 @@
+package org.sunbird.learner.actors.coursebatch.service;
+
+import java.util.Map;
+import org.sunbird.common.ElasticSearchUtil;
+import org.sunbird.common.models.util.LoggerEnum;
+import org.sunbird.common.models.util.ProjectLogger;
+import org.sunbird.common.models.util.ProjectUtil;
+
+public class CourseBatchService {
+
+  public static void syncCourseBatchForeground(String uniqueId, Map<String, Object> req) {
+    ProjectLogger.log(
+        "CourseBatchManagementActor: syncCourseBatchForeground called for course batch ID = "
+            + uniqueId,
+        LoggerEnum.INFO.name());
+    String esResponse =
+        ElasticSearchUtil.createData(
+            ProjectUtil.EsIndex.sunbird.getIndexName(),
+            ProjectUtil.EsType.course.getTypeName(),
+            uniqueId,
+            req);
+    ProjectLogger.log(
+        "CourseBatchManagementActor::syncCourseBatchForeground: Sync response for course batch ID = "
+            + uniqueId
+            + " received response = "
+            + esResponse,
+        LoggerEnum.INFO.name());
+  }
+}

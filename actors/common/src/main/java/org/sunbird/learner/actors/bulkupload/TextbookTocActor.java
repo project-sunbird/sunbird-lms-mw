ace7f7814efcda2a41994cb8a18c249717011b24
Shubham Varshney <>
2018-11-30 12:13:10 +0530

    Issue #SB-8924 feat: Added cloud upload for textbooc toc

--- actors/common/src/main/java/org/sunbird/learner/actors/bulkupload/TextbookTocActor.java
+++ actors/common/src/main/java/org/sunbird/learner/actors/bulkupload/TextbookTocActor.java
@@ -1,13 +1,20 @@
 package org.sunbird.learner.actors.bulkupload;
 
+import org.apache.commons.lang3.StringUtils;
 import org.sunbird.actor.router.ActorConfig;
+import org.sunbird.common.exception.ProjectCommonException;
 import org.sunbird.common.models.response.Response;
 import org.sunbird.common.models.response.ResponseParams;
 import org.sunbird.common.models.util.JsonKey;
+import org.sunbird.common.models.util.ProjectUtil;
 import org.sunbird.common.models.util.TextbookActorOperation;
 import org.sunbird.common.request.Request;
 import org.sunbird.common.responsecode.ResponseCode;
+import org.sunbird.content.ContentCloudStore;
+import org.sunbird.content.textbook.TextBookTocUpload;
+import org.sunbird.content.util.ContentStoreUtil;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -39,11 +46,34 @@ public class TextbookTocActor extends BaseBulkUploadActor {
     }
 
     private void getTocUrl(Request request) {
+        String textbookId = (String) request.get(JsonKey.TEXTBOOK_ID);
+        Map<String, Object> readHierarchyResponse = ContentStoreUtil.readHierarchy(textbookId);
         Response response = new Response();
-        Map<String, Object> textbook = new HashMap<>();
-        textbook.put("tocUrl", "https://sunbirddev.blob.core.windows.net/sunbird-content-dev/content/do_1126441512460369921103/artifact/1_1543475510769.pdf");
-        textbook.put("ttl", 86400);
-        response.getResult().put(JsonKey.TEXTBOOK, textbook);
+        String responseCode = (String) readHierarchyResponse.get(JsonKey.RESPONSE_CODE);
+        if (StringUtils.equals(ResponseCode.OK.name(), responseCode)) {
+            Map<String, Object> result  = (Map<String, Object>) readHierarchyResponse.get(JsonKey.RESULT);
+            Map<String, Object> content = (Map<String, Object>) result.get(JsonKey.CONTENT);
+            File csv = null;
+            if (null != content) {
+                String versionKey = (String) content.get(JsonKey.VERSION_KEY);
+                String prefix =
+                        File.separator + TextBookTocUpload.textBookTocCloudFolder + File.separator +
+                                textbookId + "_" + versionKey + "." + JsonKey.FILE_TYPE_CSV;
+                String cloudPath = ContentCloudStore.getUri(prefix, false);
+                if (StringUtils.isBlank(cloudPath)) {
+                    cloudPath = TextBookTocUpload.csvToCloud(csv, content, textbookId, versionKey);
+                }
+                Map<String, Object> textbook = new HashMap<>();
+                textbook.put(JsonKey.TOC_URL, cloudPath);
+                textbook.put(JsonKey.TTL, ProjectUtil.getConfigValue(JsonKey.TEXTBOOK_TOC_CSV_TTL));
+                response.put(JsonKey.TEXTBOOK, textbook);
+            }
+        } else {
+            throw new ProjectCommonException(
+                ResponseCode.errorProcessingRequest.getErrorCode(),
+                ResponseCode.errorProcessingRequest.getErrorMessage(),
+                ResponseCode.SERVER_ERROR.getResponseCode());
+        }
         sender().tell(response, sender());
     }
 

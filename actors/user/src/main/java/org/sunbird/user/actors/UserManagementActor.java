cb9fa5a24230bcc381f7ab9c04110605edf367ac
rajat <rajatgupta0889@gmail.com>
2018-12-12 18:40:48 +0530

    Issue #SC-545 fix: formatting

--- actors/user/src/main/java/org/sunbird/user/actors/UserManagementActor.java
+++ actors/user/src/main/java/org/sunbird/user/actors/UserManagementActor.java
@@ -5,14 +5,11 @@ import static org.sunbird.learner.util.Util.isNotNull;
 import akka.actor.ActorRef;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.sql.Timestamp;
 import java.text.MessageFormat;
 import java.util.*;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
-
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -711,6 +708,7 @@ public class UserManagementActor extends BaseActor {
     removeFieldsFrmReq(userMap);
     // if we are updating email then need to update isEmailVerified flag inside keycloak
     UserUtil.checkEmailSameOrDiff(userMap, userDbRecord);
+
     if (IS_REGISTRY_ENABLED) {
       UserUtil.updateUserToRegistry(userMap, (String) userDbRecord.get(JsonKey.REGISTRY_ID));
     }
@@ -721,8 +719,9 @@ public class UserManagementActor extends BaseActor {
     }
     Map<String, Object> requestMap = UserUtil.encryptUserData(userMap);
     removeUnwanted(requestMap);
-    if(requestMap.containsKey(JsonKey.TNC_ACCEPTED_ON)){
-      requestMap.put(JsonKey.TNC_ACCEPTED_ON,new Timestamp((Long) requestMap.get(JsonKey.TNC_ACCEPTED_ON)));
+    if (requestMap.containsKey(JsonKey.TNC_ACCEPTED_ON)) {
+      requestMap.put(
+          JsonKey.TNC_ACCEPTED_ON, new Timestamp((Long) requestMap.get(JsonKey.TNC_ACCEPTED_ON)));
     }
     Response response =
         cassandraOperation.updateRecord(

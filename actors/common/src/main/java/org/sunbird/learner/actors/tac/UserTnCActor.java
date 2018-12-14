433969512c33b41795b7b9b63505e2b8f78baf75
rajat <rajatgupta0889@gmail.com>
2018-12-14 16:06:43 +0530

    Issue #SC-545 fix

--- actors/common/src/main/java/org/sunbird/learner/actors/tac/UserTnCActor.java
+++ actors/common/src/main/java/org/sunbird/learner/actors/tac/UserTnCActor.java
@@ -1,7 +1,8 @@
 package org.sunbird.learner.actors.tac;
 
+import java.sql.Timestamp;
+import java.util.*;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.util.StringUtil;
 import org.sunbird.actor.core.BaseActor;
 import org.sunbird.actor.router.ActorConfig;
 import org.sunbird.cassandra.CassandraOperation;
@@ -18,9 +19,6 @@ import org.sunbird.common.responsecode.ResponseCode;
 import org.sunbird.helper.ServiceFactory;
 import org.sunbird.learner.util.Util;
 import org.sunbird.telemetry.util.TelemetryUtil;
-
-import java.sql.Timestamp;
-import java.util.*;
 
 @ActorConfig(
   tasks = {"userTnCAccept"},
@@ -43,57 +41,59 @@ public class UserTnCActor extends BaseActor {
   }
 
   private void acceptTNC(Request request) {
-    String acceptedTnC = (String) request.getRequest().get(JsonKey.TNC_ACCEPTED_VERSION);
+    String acceptedTnC = (String) request.getRequest().get(JsonKey.VERSION);
     Map<String, Object> userMap = new HashMap();
     String userId = (String) request.getContext().get(JsonKey.REQUESTED_BY);
     Map<String, Object> result =
-            ElasticSearchUtil.getDataByIdentifier(
-                    ProjectUtil.EsIndex.sunbird.getIndexName(),
-                    ProjectUtil.EsType.user.getTypeName(),
-                    userId);
+        ElasticSearchUtil.getDataByIdentifier(
+            ProjectUtil.EsIndex.sunbird.getIndexName(),
+            ProjectUtil.EsType.user.getTypeName(),
+            userId);
     if (result == null || result.size() == 0) {
       throw new ProjectCommonException(
-              ResponseCode.userNotFound.getErrorCode(),
-              ResponseCode.userNotFound.getErrorMessage(),
-              ResponseCode.RESOURCE_NOT_FOUND.getResponseCode());
+          ResponseCode.userNotFound.getErrorCode(),
+          ResponseCode.userNotFound.getErrorMessage(),
+          ResponseCode.RESOURCE_NOT_FOUND.getResponseCode());
     }
     // check whether is_deletd true or false
     if (ProjectUtil.isNotNull(result)
-            && result.containsKey(JsonKey.IS_DELETED)
-            && ProjectUtil.isNotNull(result.get(JsonKey.IS_DELETED))
-            && (Boolean) result.get(JsonKey.IS_DELETED)) {
+        && result.containsKey(JsonKey.IS_DELETED)
+        && ProjectUtil.isNotNull(result.get(JsonKey.IS_DELETED))
+        && (Boolean) result.get(JsonKey.IS_DELETED)) {
       ProjectCommonException.throwClientErrorException(ResponseCode.userAccountlocked);
     }
 
-    String lastAcceptedVersion =(String) result.get(JsonKey.TNC_ACCEPTED_VERSION);
+    String lastAcceptedVersion = (String) result.get(JsonKey.TNC_ACCEPTED_VERSION);
     Response response = new Response();
-    if(!lastAcceptedVersion.equalsIgnoreCase(acceptedTnC) || StringUtils.isEmpty((String)result.get(JsonKey.TNC_ACCEPTED_ON))) {
+    if (StringUtils.isEmpty(lastAcceptedVersion)
+        || !lastAcceptedVersion.equalsIgnoreCase(acceptedTnC)
+        || StringUtils.isEmpty((String) result.get(JsonKey.TNC_ACCEPTED_ON))) {
       userMap.put(JsonKey.ID, userId);
       userMap.put(JsonKey.TNC_ACCEPTED_VERSION, acceptedTnC);
-      userMap.put(JsonKey.TNC_ACCEPTED_ON, new Timestamp(Calendar.getInstance().getTime().getTime()));
+      userMap.put(
+          JsonKey.TNC_ACCEPTED_ON, new Timestamp(Calendar.getInstance().getTime().getTime()));
       response =
-              cassandraOperation.updateRecord(
-                      usrDbInfo.getKeySpace(), usrDbInfo.getTableName(), userMap);
+          cassandraOperation.updateRecord(
+              usrDbInfo.getKeySpace(), usrDbInfo.getTableName(), userMap);
       if (((String) response.get(JsonKey.RESPONSE)).equalsIgnoreCase(JsonKey.SUCCESS)) {
         saveUserDetailsToEs(userMap);
       }
-
     }
     response.getResult().put(JsonKey.RESULT, "SUCCESS");
     sender().tell(response, self());
     Map<String, Object> targetObject = null;
     List<Map<String, Object>> correlatedObject = new ArrayList<>();
     targetObject =
-            TelemetryUtil.generateTargetObject(
-                    (String) userMap.get(JsonKey.USER_ID), JsonKey.TNC, JsonKey.ACCEPT, null);
+        TelemetryUtil.generateTargetObject(
+            (String) userMap.get(JsonKey.USER_ID), JsonKey.TNC, JsonKey.ACCEPT, null);
     TelemetryUtil.telemetryProcessingCall(userMap, targetObject, correlatedObject);
   }
+
   private void saveUserDetailsToEs(Map<String, Object> completeUserMap) {
     Request userRequest = new Request();
     userRequest.setOperation(ActorOperations.UPDATE_USER_INFO_ELASTIC.getValue());
     userRequest.getRequest().put(JsonKey.ID, completeUserMap.get(JsonKey.ID));
-    ProjectLogger.log(
-            "UserTnCActor:saveUserDetailsToEs: Trigger sync of user details to ES");
+    ProjectLogger.log("UserTnCActor:saveUserDetailsToEs: Trigger sync of user details to ES");
     tellToAnother(userRequest);
   }
 }

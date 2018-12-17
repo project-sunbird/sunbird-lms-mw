c8489e5732e78ad9a7fb8172878e2874cfaba7a5
bvinayakumar <36028332+bvinayakumar@users.noreply.github.com>
2018-12-17 10:46:55 +0530

    Update OTPService.java

--- actors/common/src/main/java/org/sunbird/learner/actors/otp/service/OTPService.java
+++ actors/common/src/main/java/org/sunbird/learner/actors/otp/service/OTPService.java
@@ -24,13 +24,15 @@ public class OTPService {
     Map<String, Object> request = new HashMap<>();
     request.put(JsonKey.TYPE, type);
     request.put(JsonKey.KEY, key);
+
     Response result =
         cassandraOperation.getRecordById(otpDb.getKeySpace(), otpDb.getTableName(), request);
+
     List<Map<String, Object>> otpMapList = (List<Map<String, Object>>) result.get(JsonKey.RESPONSE);
-    ProjectLogger.log("response otp details = " + otpMapList, LoggerEnum.INFO.name());
     if (CollectionUtils.isEmpty(otpMapList)) {
       return null;
     }
+
     return otpMapList.get(0);
   }
 
@@ -40,13 +42,14 @@ public class OTPService {
     request.put(JsonKey.KEY, key);
     request.put(JsonKey.OTP, otp);
     request.put(JsonKey.CREATED_ON, new Timestamp(Calendar.getInstance().getTimeInMillis()));
+
     String expirationInSeconds =
         PropertiesCache.getInstance().getProperty(JsonKey.SUNBIRD_OTP_EXPIRATION);
     int ttl = Integer.valueOf(expirationInSeconds);
+
     Response response =
         cassandraOperation.insertRecordWithTTL(
             otpDb.getKeySpace(), otpDb.getTableName(), request, ttl);
-    ProjectLogger.log("response code = " + response.getResponseCode(), LoggerEnum.INFO.name());
   }
 
 }

262c5711feaa69b85e48cd306a64ed7c83c6ca33
amolnin <42316446+amolnin@users.noreply.github.com>
2019-01-09 14:39:28 +0530

    Update LearnerStateActorTest.java

--- service/src/test/java/org/sunbird/learner/actors/LearnerStateActorTest.java
+++ service/src/test/java/org/sunbird/learner/actors/LearnerStateActorTest.java
@@ -206,18 +206,18 @@ public class LearnerStateActorTest {
   @Test
   public void testGetCourseByUserIdAndCourseBatchesSuccess() {
     String s[] = {JsonKey.STATUS};
-    Map<String, Object> batch = getTestDone(s);
+    Map<String, Object> batch = testGetEnrolledCoursesWithBatchInfo(s);
     Assert.assertEquals(1, batch.get("status"));
   }
 
   @Test
   public void testGetCourseByUserIdFailureWithInvalidFieldNames() {
     String s[] = {"invalid", "status", "erre"};
-    Map<String, Object> batch = getTestDone(s);
+    Map<String, Object> batch = testGetEnrolledCoursesWithBatchInfo(s);
     Assert.assertEquals(null, batch.get("invalid"));
   }
 
-  private Map<String, Object> getTestDone(String[] s) {
+  private Map<String, Object> testGetEnrolledCoursesWithBatchInfo(String[] s) {
     TestKit probe = new TestKit(system);
     ActorRef subject = system.actorOf(props);
 

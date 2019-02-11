4684a0b185aeca59a1f2ed4bc447885484240a47
Mayank Morya <moryamayank9@gmail.com>
2019-02-11 15:35:52 +0530

    user-management-actor base impl

--- service/src/test/java/org/sunbird/learner/actors/UserManagementActorTest.java
+++ service/src/test/java/org/sunbird/learner/actors/UserManagementActorTest.java
@@ -2,32 +2,22 @@ package org.sunbird.learner.actors;
 
 import static akka.testkit.JavaTestKit.duration;
 import static org.junit.Assert.assertTrue;
-import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.when;
 
 import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
-import akka.actor.Props;
 import akka.testkit.javadsl.TestKit;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.sunbird.actor.router.RequestRouter;
-import org.sunbird.actorutil.InterServiceCommunication;
 import org.sunbird.actorutil.InterServiceCommunicationFactory;
-import org.sunbird.actorutil.impl.InterServiceCommunicationImpl;
 import org.sunbird.actorutil.location.impl.LocationClientImpl;
 import org.sunbird.actorutil.systemsettings.impl.SystemSettingClientImpl;
-import org.sunbird.cassandraimpl.CassandraOperationImpl;
 import org.sunbird.common.ElasticSearchUtil;
 import org.sunbird.common.exception.ProjectCommonException;
 import org.sunbird.common.models.response.Response;
@@ -37,8 +27,6 @@ import org.sunbird.common.request.Request;
 import org.sunbird.common.responsecode.ResponseCode;
 import org.sunbird.helper.ServiceFactory;
 import org.sunbird.learner.util.Util;
-import org.sunbird.models.user.User;
-import org.sunbird.user.actors.UserManagementActor;
 import org.sunbird.user.service.impl.UserServiceImpl;
 import org.sunbird.user.util.UserUtil;
 
@@ -55,72 +43,7 @@ import org.sunbird.user.util.UserUtil;
   LocationClientImpl.class
 })
 @PowerMockIgnore({"javax.management.*"})
-public class UserManagementActorTest {
-
-  private ActorSystem system = ActorSystem.create("system");
-  private static final Props props = Props.create(UserManagementActor.class);
-  private static Map<String, Object> reqMap;
-  static InterServiceCommunication interServiceCommunication =
-      mock(InterServiceCommunicationImpl.class);;
-  private static UserServiceImpl userService;
-
-  @Before
-  public void beforeEachTest() {
-
-    ActorRef actorRef = mock(ActorRef.class);
-    PowerMockito.mockStatic(RequestRouter.class);
-    when(RequestRouter.getActor(Mockito.anyString())).thenReturn(actorRef);
-
-    PowerMockito.mockStatic(ServiceFactory.class);
-    CassandraOperationImpl cassandraOperation = mock(CassandraOperationImpl.class);
-    when(ServiceFactory.getInstance()).thenReturn(cassandraOperation);
-    when(cassandraOperation.insertRecord(
-            Mockito.anyString(), Mockito.anyString(), Mockito.anyMap()))
-        .thenReturn(getSuccessResponse());
-    when(cassandraOperation.updateRecord(
-            Mockito.anyString(), Mockito.anyString(), Mockito.anyMap()))
-        .thenReturn(getSuccessResponse());
-
-    PowerMockito.mockStatic(InterServiceCommunicationFactory.class);
-    when(InterServiceCommunicationFactory.getInstance()).thenReturn(interServiceCommunication);
-    when(interServiceCommunication.getResponse(
-            Mockito.any(ActorRef.class), Mockito.any(Request.class)))
-        .thenReturn(getEsResponse());
-
-    PowerMockito.mockStatic(SystemSettingClientImpl.class);
-    SystemSettingClientImpl systemSettingClient = mock(SystemSettingClientImpl.class);
-    when(SystemSettingClientImpl.getInstance()).thenReturn(systemSettingClient);
-    when(systemSettingClient.getSystemSettingByFieldAndKey(
-            Mockito.any(ActorRef.class),
-            Mockito.anyString(),
-            Mockito.anyString(),
-            Mockito.anyObject()))
-        .thenReturn(new HashMap<>());
-
-    PowerMockito.mockStatic(UserServiceImpl.class);
-    userService = mock(UserServiceImpl.class);
-    when(UserServiceImpl.getInstance()).thenReturn(userService);
-    when(userService.getRootOrgIdFromChannel(Mockito.anyString())).thenReturn("anyId");
-    when(userService.getCustodianChannel(Mockito.anyMap(), Mockito.any(ActorRef.class)))
-        .thenReturn("anyChannel");
-    when(userService.getRootOrgIdFromChannel(Mockito.anyString())).thenReturn("rootOrgId");
-
-    PowerMockito.mockStatic(ElasticSearchUtil.class);
-    when(ElasticSearchUtil.getDataByIdentifier(
-            Mockito.anyString(), Mockito.anyString(), Mockito.anyString()))
-        .thenReturn(getEsResponseMap());
-
-    PowerMockito.mockStatic(Util.class);
-    Util.getUserProfileConfig(Mockito.any(ActorRef.class));
-
-    PowerMockito.mockStatic(UserUtil.class);
-    UserUtil.setUserDefaultValue(Mockito.anyMap(), Mockito.anyString());
-
-    Map<String, Object> requestMap = new HashMap<>();
-    requestMap.put(JsonKey.TNC_ACCEPTED_ON, 12345678L);
-    when(UserUtil.encryptUserData(Mockito.anyMap())).thenReturn(requestMap);
-    reqMap = getMapObject();
-  }
+public class UserManagementActorTest extends UserManagementActorTestBase {
 
   @Test
   public void testCreateUserSuccessWithUserCallerId() {
@@ -379,92 +302,5 @@ public class UserManagementActorTest {
       return res.getCode().equals(errorCode.getErrorCode())
           || res.getResponseCode() == errorCode.getResponseCode();
     }
-  }
-
-  private Map<String, Object> getExternalIdMap() {
-
-    Map<String, Object> reqMap = new HashMap<>();
-    reqMap.put(JsonKey.EXTERNAL_ID, "externalId");
-    reqMap.put(JsonKey.EXTERNAL_ID_PROVIDER, "externalIdProvider");
-    reqMap.put(JsonKey.EXTERNAL_ID_TYPE, "externalIdType");
-    return reqMap;
-  }
-
-  private HashMap<String, Object> getMapObject() {
-
-    HashMap<String, Object> reqMap = new HashMap<>();
-    reqMap.put(JsonKey.FIRST_NAME, "firstname");
-    reqMap.put(JsonKey.USERNAME, "userName");
-    reqMap.put(JsonKey.EMAIL, "email@email.com");
-    reqMap.put(JsonKey.LANGUAGE, new ArrayList<>());
-    reqMap.put(JsonKey.DOB, "1992-12-31");
-    reqMap.put(JsonKey.EMAIL_VERIFIED, true);
-    reqMap.put(JsonKey.PHONE_VERIFIED, true);
-    reqMap.put(JsonKey.ADDRESS, new ArrayList<>());
-    return reqMap;
-  }
-
-  private Request getRequest(
-      boolean isCallerIdReq,
-      boolean isRootOrgIdReq,
-      boolean isVersionReq,
-      Map<String, Object> reqMap,
-      ActorOperations actorOperation) {
-
-    Request reqObj = new Request();
-    HashMap<String, Object> innerMap = new HashMap<>();
-    if (isCallerIdReq) innerMap.put(JsonKey.CALLER_ID, "anyCallerId");
-    if (isVersionReq) innerMap.put(JsonKey.VERSION, "v2");
-    if (isRootOrgIdReq) innerMap.put(JsonKey.ROOT_ORG_ID, "MY_ROOT_ORG_ID");
-    innerMap.put(JsonKey.REQUESTED_BY, "requestedBy");
-    reqObj.setRequest(reqMap);
-    reqObj.setContext(innerMap);
-    reqObj.setOperation(actorOperation.getValue());
-    return reqObj;
-  }
-
-  private Map<String, Object> getUpdateRequestWithLocationCodes() {
-    Map<String, Object> reqObj = new HashMap();
-    reqObj.put(JsonKey.LOCATION_CODES, Arrays.asList("locationCode"));
-    reqObj.put(JsonKey.USER_ID, "userId");
-    return reqObj;
-  }
-
-  private static Response getEsResponse() {
-
-    Response response = new Response();
-    Map<String, Object> map = new HashMap<>();
-    map.put("anyString", new Object());
-    response.put(JsonKey.RESPONSE, map);
-    return response;
-  }
-
-  private static Response getSuccessResponse() {
-    Response response = new Response();
-    response.put(JsonKey.RESPONSE, JsonKey.SUCCESS);
-    return response;
-  }
-
-  private static Map<String, Object> getEsResponseMap() {
-    Map<String, Object> map = new HashMap<>();
-    map.put(JsonKey.IS_ROOT_ORG, true);
-    map.put(JsonKey.ID, "rootOrgId");
-    map.put(JsonKey.CHANNEL, "anyChannel");
-    return map;
-  }
-
-  public Object getEsResponseForLocation() {
-    Response response = new Response();
-    response.put(JsonKey.RESPONSE, Arrays.asList("id"));
-    return response;
-  }
-
-  private User getUser(boolean isCustodian) {
-    User user = new User();
-    user.setRootOrgId("rootOrgId");
-    if (isCustodian) {
-      user.setRootOrgId("custodianOrgId");
-    }
-    return user;
   }
 }

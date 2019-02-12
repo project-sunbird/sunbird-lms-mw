c0e30c67e6316704b364161a54475804e36ecf20
kirti sagar jenamoni <kirti.sagar@mantralabsglobal.com>
2019-02-12 19:27:50 +0530

    mw

---
+++ actors/user/src/main/java/org/sunbird/user/actors/UserTypeActor.java
@@ -0,0 +1,51 @@
+package org.sunbird.user.actors;
+
+import java.util.List;
+import org.sunbird.actor.router.ActorConfig;
+import org.sunbird.common.models.response.Response;
+import org.sunbird.common.models.util.JsonKey;
+import org.sunbird.common.models.util.ProjectLogger;
+import org.sunbird.common.request.Request;
+import org.sunbird.user.dao.UserTypeDao;
+import org.sunbird.user.dao.impl.userTypeDaoImpl;
+
+@ActorConfig(
+  tasks = {"getUserTypes"},
+  asyncTasks = {}
+)
+public class UserTypeActor extends UserBaseActor {
+
+  private UserTypeDao userTypeDao = userTypeDaoImpl.getInstance();
+
+  @Override
+  public void onReceive(Request request) throws Throwable {
+    String operation = request.getOperation();
+    switch (operation) {
+      case "getUserTypes":
+        getUserTypes();
+        break;
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  private void getUserTypes() {
+
+    Response response = new Response();
+    List<String> userTypeList = getUserTypeList();
+    response.getResult().put(JsonKey.USER_TYPES, userTypeList);
+    sender().tell(response, self());
+  }
+
+  private List<String> getUserTypeList() {
+    List<String> userTypeList = null;
+    try {
+      Response res = userTypeDao.getUserTypes();
+      if (!((List<String>) res.get(JsonKey.RESPONSE)).isEmpty()) {
+        userTypeList = (List<String>) res.get(JsonKey.RESPONSE);
+      }
+    } catch (Exception e) {
+      ProjectLogger.log(e.getMessage(), e);
+    }
+    return userTypeList;
+  }
+}
---
+++ actors/user/src/main/java/org/sunbird/user/dao/UserTypeDao.java
@@ -0,0 +1,8 @@
+package org.sunbird.user.dao;
+
+import org.sunbird.common.models.response.Response;
+
+public interface UserTypeDao {
+
+  Response getUserTypes();
+}

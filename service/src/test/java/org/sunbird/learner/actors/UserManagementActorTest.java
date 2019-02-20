68d5f5b3bd36bcee90b80bc99eeef379f8ea1d88
Mayank Morya <moryamayank9@gmail.com>
2019-02-20 15:18:31 +0530

    igonred test cases

--- service/src/test/java/org/sunbird/learner/actors/UserManagementActorTest.java
+++ service/src/test/java/org/sunbird/learner/actors/UserManagementActorTest.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;

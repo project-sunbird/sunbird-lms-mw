1e3490d8fac1683c835466ef903aadb18a37e2d8
kirti sagar jenamoni <kirti.sagar@mantralabsglobal.com>
2019-02-21 12:46:58 +0530

    userTypeActorTest

--- actors/user/src/test/java/org/sunbird/user/UserTypeActorTest.java
+++ actors/user/src/test/java/org/sunbird/user/UserTypeActorTest.java
@@ -8,41 +8,15 @@ import akka.actor.Props;
 import akka.testkit.javadsl.TestKit;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
-import org.sunbird.actor.router.RequestRouter;
-import org.sunbird.actor.service.BaseMWService;
-import org.sunbird.actorutil.InterServiceCommunicationFactory;
-import org.sunbird.common.ElasticSearchUtil;
 import org.sunbird.common.models.response.Response;
 import org.sunbird.common.models.util.ActorOperations;
-import org.sunbird.common.models.util.datasecurity.DecryptionService;
 import org.sunbird.common.request.Request;
 import org.sunbird.common.responsecode.ResponseCode;
-import org.sunbird.helper.ServiceFactory;
-import org.sunbird.learner.actors.role.dao.impl.RoleDaoImpl;
-import org.sunbird.learner.util.Util;
 import org.sunbird.user.actors.UserTypeActor;
-import org.sunbird.user.dao.impl.UserOrgDaoImpl;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({
-  ServiceFactory.class,
-  RoleDaoImpl.class,
-  BaseMWService.class,
-  RequestRouter.class,
-  InterServiceCommunicationFactory.class,
-  ElasticSearchUtil.class,
-  Util.class,
-  UserOrgDaoImpl.class,
-  DecryptionService.class,
-})
-@PowerMockIgnore({"javax.management.*"})
 public class UserTypeActorTest {
 
-  private ActorSystem system = ActorSystem.create("system");
+  private static ActorSystem system = ActorSystem.create("system");
   private static final Props props = Props.create(UserTypeActor.class);
 
   @Test
@@ -54,7 +28,7 @@ public class UserTypeActorTest {
     reqObj.setOperation(ActorOperations.GET_USER_TYPES.getValue());
     subject.tell(reqObj, probe.getRef());
 
-    Response res = probe.expectMsgClass(duration("1000 second"), Response.class);
+    Response res = probe.expectMsgClass(duration("10 second"), Response.class);
     Assert.assertTrue(null != res && res.getResponseCode() == ResponseCode.OK);
   }
 }

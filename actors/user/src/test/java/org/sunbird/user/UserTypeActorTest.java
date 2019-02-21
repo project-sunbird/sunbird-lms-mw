755acbfdc4e5a56c1e089345682832a1fa7b0391
kirti sagar jenamoni <kirti.sagar@mantralabsglobal.com>
2019-02-21 16:17:50 +0530

    usertype

--- actors/user/src/test/java/org/sunbird/user/UserTypeActorTest.java
+++ actors/user/src/test/java/org/sunbird/user/UserTypeActorTest.java
@@ -6,10 +6,13 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.testkit.javadsl.TestKit;
+import java.util.List;
+import java.util.Map;
 import org.junit.Assert;
 import org.junit.Test;
 import org.sunbird.common.models.response.Response;
 import org.sunbird.common.models.util.ActorOperations;
+import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.request.Request;
 import org.sunbird.common.responsecode.ResponseCode;
 import org.sunbird.user.actors.UserTypeActor;
@@ -27,8 +30,17 @@ public class UserTypeActorTest {
     Request reqObj = new Request();
     reqObj.setOperation(ActorOperations.GET_USER_TYPES.getValue());
     subject.tell(reqObj, probe.getRef());
-
     Response res = probe.expectMsgClass(duration("10 second"), Response.class);
-    Assert.assertTrue(null != res && res.getResponseCode() == ResponseCode.OK);
+    Assert.assertTrue(res.getResponseCode() == ResponseCode.OK && getResponse(res));
+  }
+
+  private boolean getResponse(Response res) {
+
+    List<Map<String, Object>> lst =
+        (List<Map<String, Object>>) res.getResult().get(JsonKey.RESPONSE);
+    if (lst.get(0).get(JsonKey.ID) == "TEACHER"
+        && lst.get(1).get(JsonKey.ID) == "OTHER"
+        && lst.size() == 2) return true;
+    return false;
   }
 }

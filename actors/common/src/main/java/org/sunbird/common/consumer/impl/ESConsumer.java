2de32404ac6ba5d4997553e4ab4cade2f5ef0355
rajat <rajatgupta0889@gmail.com>
2019-03-11 10:50:42 +0530

    add

--- actors/common/src/main/java/org/sunbird/common/consumer/impl/ESConsumer.java
+++ actors/common/src/main/java/org/sunbird/common/consumer/impl/ESConsumer.java
@@ -1,5 +1,7 @@
 package org.sunbird.common.consumer.impl;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.sunbird.common.ElasticSearchUtil;
 import org.sunbird.common.message.broker.factory.MessageBrokerFactory;
 import org.sunbird.common.message.broker.inf.MessageBroker;
@@ -10,54 +12,53 @@ import org.sunbird.common.models.util.LoggerEnum;
 import org.sunbird.common.models.util.ProjectLogger;
 import org.sunbird.common.models.util.ProjectUtil;
 
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * Created by rajatgupta on 09/03/19.
- */
+/** Created by rajatgupta on 09/03/19. */
 public class ESConsumer implements Consumer {
 
+  static Map<String, String> identiFierMap = new HashMap<>();
+  static Map<String, Boolean> updateMap = new HashMap<>();
 
-   static Map<String,String> identiFierMap = new HashMap<>();
-    static Map<String,Boolean> updateMap = new HashMap<>();
+  static {
+    identiFierMap.put(JsonKey.USER, JsonKey.ID);
+    identiFierMap.put(JsonKey.USER_ORG, JsonKey.USER_ID);
+    identiFierMap.put("user_skills", JsonKey.USER_ID);
 
-    static {
-        identiFierMap.put(JsonKey.USER,JsonKey.ID);
-        identiFierMap.put(JsonKey.USER_ORG,JsonKey.USER_ID);
-        identiFierMap.put("user_skills",JsonKey.USER_ID);
+    updateMap.put(JsonKey.USER_ORG, true);
+    updateMap.put(JsonKey.SKILLS, true);
+  }
 
-        updateMap.put(JsonKey.USER_ORG, true);
-        updateMap.put(JsonKey.SKILLS, true);
+  @Override
+  public void consume() {
+    ProjectLogger.log("ESConsumer:execute: ESConsumer job trigerred.", LoggerEnum.INFO);
+
+    performJob();
+  }
+
+  private void performJob() {
+    ProjectLogger.log("ESConsumer:performJob: ESConsumer  performJob.");
+    MessageBroker messageBroker = MessageBrokerFactory.getInstance();
+    EventMessage eventMessage = messageBroker.recieve(JsonKey.DATABASE_OPERATION);
+
+    if (JsonKey.INSERT.equalsIgnoreCase(eventMessage.getOperationType())) {
+      ElasticSearchUtil.upsertData(
+          ProjectUtil.EsIndex.sunbird.getIndexName(),
+          eventMessage.getOpertaionType(),
+          (String) eventMessage.getMessage().get(identiFierMap.get(eventMessage.getOperationOn())),
+          eventMessage.getMessage());
+    } else {
+      if (updateMap.get(eventMessage.getOperationOn())) {
+        Map<String, Object> esMap =
+            ElasticSearchUtil.getDataByIdentifier(
+                ProjectUtil.EsIndex.sunbird.getIndexName(),
+                eventMessage.getOpertaionType(),
+                (String)
+                    eventMessage
+                        .getMessage()
+                        .get(identiFierMap.get(eventMessage.getOperationOn())));
+
+      } else {
+
+      }
     }
-
-
-
-
-    @Override
-    public void consume() {
-        ProjectLogger.log("ESConsumer:execute: ESConsumer job trigerred.", LoggerEnum.INFO);
-
-        performJob();
-    }
-
-        private void performJob() {
-            ProjectLogger.log("ESConsumer:performJob: ESConsumer  performJob.");
-            MessageBroker messageBroker = MessageBrokerFactory.getInstance();
-            EventMessage eventMessage = messageBroker.recieve(JsonKey.DATABASE_OPERATION);
-
-            if(JsonKey.INSERT.equalsIgnoreCase(eventMessage.getOperationType())) {
-                ElasticSearchUtil.upsertData(ProjectUtil.EsIndex.sunbird.getIndexName(), eventMessage.getOpertaionType(), (String) eventMessage.getMessage().get(identiFierMap.get(eventMessage.getOperationOn())), eventMessage.getMessage());
-            }else{
-                if(updateMap.get(eventMessage.getOperationOn())) {
-                    Map<String, Object> esMap = ElasticSearchUtil.getDataByIdentifier(ProjectUtil.EsIndex.sunbird.getIndexName(), eventMessage.getOpertaionType(), (String) eventMessage.getMessage().get(identiFierMap.get(eventMessage.getOperationOn())));
-
-                }else{
-
-                }
-            }
-        }
+  }
 }
-
-
-

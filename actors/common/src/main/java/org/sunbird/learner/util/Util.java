1dfa9e675dc9049cbe65693230de981ab8aeab6b
sudhirgiri2911 <sudhirgiri2911@gmail.com>
2019-04-03 17:20:36 +0530

    Issue #SB-0000 feat: Addded more logs for username troubleshooting

--- actors/common/src/main/java/org/sunbird/learner/util/Util.java
+++ actors/common/src/main/java/org/sunbird/learner/util/Util.java
@@ -1543,7 +1543,14 @@ public final class Util {
         (Map<String, String>) userMap.get(JsonKey.PROFILE_VISIBILITY);
     Map<String, String> completeProfileVisibilityMap =
         getCompleteProfileVisibilityMap(userProfileVisibilityMap, actorRef);
-
+    ProjectLogger.log(
+        "Util:checkUserProfileVisibility: completeProfileVisibilityMap is "
+            + completeProfileVisibilityMap,
+        LoggerEnum.INFO.name());
+    ProjectLogger.log(
+        "Util:checkUserProfileVisibility: userMap contains username and the encrypted value before removing"
+            + userMap.get(JsonKey.USER_NAME),
+        LoggerEnum.INFO.name());
     if (MapUtils.isNotEmpty(completeProfileVisibilityMap)) {
       Map<String, Object> privateFieldsMap = new HashMap<>();
       for (String field : completeProfileVisibilityMap.keySet()) {
@@ -1552,10 +1559,11 @@ public final class Util {
         }
       }
       ProjectLogger.log(
-          "Util:checkUserProfileVisibility: UserMap before removing private fields for userId "
-              + userMap.get(JsonKey.USER_ID)
-              + " is "
-              + userMap,
+          "Util:checkUserProfileVisibility: private fields key are " + privateFieldsMap.keySet(),
+          LoggerEnum.INFO.name());
+      ProjectLogger.log(
+          "Util:checkUserProfileVisibility: userMap contains username and the encrypted value after removing"
+              + userMap.get(JsonKey.USER_NAME),
           LoggerEnum.INFO.name());
       ElasticSearchUtil.upsertData(
           ProjectUtil.EsIndex.sunbird.getIndexName(),

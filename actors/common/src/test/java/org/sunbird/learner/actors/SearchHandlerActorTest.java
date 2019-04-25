93f1b20bf65f1b0b2f68d279ee96f0e35cecb289
kirti sagar jenamoni <kirti.sagar@mantralabsglobal.com>
2019-04-25 16:23:52 +0530

    searchHandlerTest

--- actors/common/src/test/java/org/sunbird/learner/actors/SearchHandlerActorTest.java
+++ actors/common/src/test/java/org/sunbird/learner/actors/SearchHandlerActorTest.java
@@ -1,36 +1,102 @@
 package org.sunbird.learner.actors;
 
 import static akka.testkit.JavaTestKit.duration;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.testkit.javadsl.TestKit;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.sunbird.cassandraimpl.CassandraOperationImpl;
+import org.sunbird.common.ElasticSearchUtil;
 import org.sunbird.common.exception.ProjectCommonException;
 import org.sunbird.common.models.response.Response;
 import org.sunbird.common.models.util.ActorOperations;
 import org.sunbird.common.models.util.JsonKey;
+import org.sunbird.common.models.util.ProjectUtil;
 import org.sunbird.common.request.Request;
+import org.sunbird.dto.SearchDTO;
+import org.sunbird.helper.ServiceFactory;
+import org.sunbird.learner.actors.coursebatch.dao.UserCoursesDao;
+import org.sunbird.learner.actors.coursebatch.dao.impl.UserCoursesDaoImpl;
 import org.sunbird.learner.actors.search.SearchHandlerActor;
 
 // @Ignore
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ServiceFactory.class, ElasticSearchUtil.class, UserCoursesDaoImpl.class})
+@PowerMockIgnore({"javax.management.*"})
 public class SearchHandlerActorTest {
 
   private static ActorSystem system;
   private static final Props props = Props.create(SearchHandlerActor.class);
+  private static UserCoursesDao userCoursesDao;
+  private static CassandraOperationImpl cassandraOperation;
 
   @BeforeClass
   public static void setUp() {
-    //    SunbirdMWService.init();
     system = ActorSystem.create("system");
-    //    Util.checkCassandraDbConnections(JsonKey.SUNBIRD);
+    PowerMockito.mockStatic(ServiceFactory.class);
+    cassandraOperation = mock(CassandraOperationImpl.class);
+    mockStatic(UserCoursesDaoImpl.class);
+    userCoursesDao = PowerMockito.mock(UserCoursesDaoImpl.class);
+    when(UserCoursesDaoImpl.getInstance()).thenReturn(userCoursesDao);
+  }
+
+  @Before
+  public void beforeTest() {
+    PowerMockito.mockStatic(ElasticSearchUtil.class);
+    when(ElasticSearchUtil.complexSearch(
+            Mockito.any(SearchDTO.class),
+            Mockito.eq(ProjectUtil.EsIndex.sunbird.getIndexName()),
+            Mockito.anyVararg()))
+        .thenReturn(createResponseGet(true));
+
+    PowerMockito.mockStatic(ServiceFactory.class);
+    when(ServiceFactory.getInstance()).thenReturn(cassandraOperation);
+    when(cassandraOperation.getRecordsByProperties(
+            Mockito.anyString(), Mockito.anyString(), Mockito.anyMap(), Mockito.anyList()))
+        .thenReturn(getRecordByPropertyResponse());
+  }
+
+  private static Response getRecordByPropertyResponse() {
+
+    Response response = new Response();
+    List<Map<String, Object>> list = new ArrayList<>();
+    Map<String, Object> courseMap = new HashMap<>();
+    courseMap.put(JsonKey.ACTIVE, true);
+    courseMap.put(JsonKey.USER_ID, "anyUserId");
+    list.add(courseMap);
+    response.put(JsonKey.RESPONSE, list);
+    return response;
+  }
+
+  private static Map<String, Object> createResponseGet(boolean isResponseRequired) {
+    HashMap<String, Object> response = new HashMap<>();
+    List<Map<String, Object>> content = new ArrayList<>();
+    HashMap<String, Object> innerMap = new HashMap<>();
+    List<Map<String, Object>> orgList = new ArrayList<>();
+    Map<String, Object> orgMap = new HashMap<>();
+    orgMap.put(JsonKey.ORGANISATION_ID, "anyOrgId");
+
+    innerMap.put(JsonKey.ROOT_ORG_ID, "anyRootOrgId");
+    innerMap.put(JsonKey.ORGANISATIONS, orgList);
+    innerMap.put(JsonKey.HASHTAGID, "HASHTAGID");
+    content.add(innerMap);
+    response.put(JsonKey.CONTENT, content);
+    return response;
   }
 
   @Test
@@ -49,7 +115,38 @@ public class SearchHandlerActorTest {
     filters.put(JsonKey.ROOT_ORG_ID, "ORG_001");
     innerMap.put(JsonKey.FILTERS, filters);
     innerMap.put(JsonKey.LIMIT, 1);
+
+    Map<String, Object> contextMap = new HashMap<>();
+    contextMap.put(JsonKey.FIELDS, JsonKey.ORG_NAME);
+    reqObj.setContext(contextMap);
     reqObj.setRequest(innerMap);
+    subject.tell(reqObj, probe.getRef());
+    Response res = probe.expectMsgClass(duration("200 second"), Response.class);
+    Assert.assertTrue(null != res.get(JsonKey.RESPONSE));
+  }
+
+  @Test
+  public void searchCourse() {
+    TestKit probe = new TestKit(system);
+    ActorRef subject = system.actorOf(props);
+
+    Request reqObj = new Request();
+    reqObj.setOperation(ActorOperations.COMPOSITE_SEARCH.getValue());
+    HashMap<String, Object> innerMap = new HashMap<>();
+    innerMap.put(JsonKey.QUERY, "");
+    Map<String, Object> filters = new HashMap<>();
+    List<String> objectType = new ArrayList<String>();
+    objectType.add("cbatch");
+    filters.put(JsonKey.OBJECT_TYPE, objectType);
+    filters.put(JsonKey.ROOT_ORG_ID, "ORG_001");
+    innerMap.put(JsonKey.FILTERS, filters);
+    innerMap.put(JsonKey.LIMIT, 1);
+    reqObj.setRequest(innerMap);
+
+    Map<String, Object> contextMap = new HashMap<>();
+    contextMap.put(JsonKey.PARTICIPANTS, JsonKey.PARTICIPANTS);
+    reqObj.setContext(contextMap);
+
     subject.tell(reqObj, probe.getRef());
     Response res = probe.expectMsgClass(duration("200 second"), Response.class);
     Assert.assertTrue(null != res.get(JsonKey.RESPONSE));
@@ -62,18 +159,6 @@ public class SearchHandlerActorTest {
 
     Request reqObj = new Request();
     reqObj.setOperation("INVALID_OPERATION");
-
-    subject.tell(reqObj, probe.getRef());
-    ProjectCommonException exc = probe.expectMsgClass(ProjectCommonException.class);
-    Assert.assertTrue(null != exc);
-  }
-
-  @Test
-  public void testInvalidRequestData() {
-    TestKit probe = new TestKit(system);
-    ActorRef subject = system.actorOf(props);
-
-    Response reqObj = new Response();
 
     subject.tell(reqObj, probe.getRef());
     ProjectCommonException exc = probe.expectMsgClass(ProjectCommonException.class);

7aa6b9fa003b086653a81ecd2ac9ccee457e901f
Niharika Singh <49979976+niharikasingh84@users.noreply.github.com>
2019-05-14 13:48:11 +0530

    Update BackgroundJobManager.java

--- actors/common/src/main/java/org/sunbird/learner/actors/BackgroundJobManager.java
+++ actors/common/src/main/java/org/sunbird/learner/actors/BackgroundJobManager.java
@@ -554,9 +554,9 @@ public class BackgroundJobManager extends BaseActor {
    */
   private boolean insertDataToElastic(
       String index, String type, String identifier, Map<String, Object> data) {
-    ProjectLogger.log(
+    /* ProjectLogger.log(
         "making call to ES for type ,identifier ,data==" + type + " " + identifier + data,
-        LoggerEnum.INFO.name());
+        LoggerEnum.INFO.name()); */
     /*
      * if (type.equalsIgnoreCase(ProjectUtil.EsType.user.getTypeName())) { // now
      * calculate profile completeness and error filed and store it in ES

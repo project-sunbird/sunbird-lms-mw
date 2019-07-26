2660e9ef919be8aacb5ead12c4163d004043f3a3
anmol2302 <anmolgupta23021997@gmail.com>
2019-07-26 14:55:58 +0530

    Issue#SB-13803 check added to validate the nullify operation

--- actors/user/src/main/java/org/sunbird/user/actors/IdentifierFreeUpActor.java
+++ actors/user/src/main/java/org/sunbird/user/actors/IdentifierFreeUpActor.java
@@ -1,6 +1,7 @@
 package org.sunbird.user.actors;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.sunbird.actor.core.BaseActor;
 import org.sunbird.actor.router.ActorConfig;
 import org.sunbird.cassandra.CassandraOperation;
@@ -8,6 +9,7 @@ import org.sunbird.common.exception.ProjectCommonException;
 import org.sunbird.common.models.response.Response;
 import org.sunbird.common.models.util.ActorOperations;
 import org.sunbird.common.models.util.JsonKey;
+import org.sunbird.common.models.util.LoggerEnum;
 import org.sunbird.common.models.util.ProjectLogger;
 import org.sunbird.common.request.Request;
 import org.sunbird.common.responsecode.ResponseCode;
@@ -29,7 +31,7 @@ public class IdentifierFreeUpActor extends BaseActor {
     private CassandraOperation cassandraOperation = ServiceFactory.getInstance();
     private Util.DbInfo usrDbInfo = Util.dbInfoMap.get(JsonKey.USER_DB);
     private Map<String, Object> userDbMap = new HashMap<>();
-    public static final int FIRST_RECORD=0;
+    public static final int FIRST_RECORD = 0;
 
     @Override
     public void onReceive(Request request) {
@@ -49,7 +51,7 @@ public class IdentifierFreeUpActor extends BaseActor {
 
     private Response processUserAttribute(List<String> identifiers) {
 
-        identifiers.stream().forEach(val -> {
+        identifiers.forEach(val -> {
             if (val.equalsIgnoreCase(JsonKey.EMAIL)) {
                 nullifyEmail();
             } else if (val.equalsIgnoreCase(JsonKey.PHONE)) {
@@ -60,17 +62,21 @@ public class IdentifierFreeUpActor extends BaseActor {
     }
 
     private void nullifyEmail() {
-        userDbMap.replace(JsonKey.PREV_USED_EMAIL, userDbMap.get(JsonKey.EMAIL));
-        userDbMap.replace(JsonKey.EMAIL, null);
-        userDbMap.replace(JsonKey.MASKED_EMAIL, null);
-        userDbMap.replace(JsonKey.EMAIL_VERIFIED,false);
+        if (isNullifyOperationValid((String) userDbMap.get(JsonKey.EMAIL))) {
+            userDbMap.replace(JsonKey.PREV_USED_EMAIL, userDbMap.get(JsonKey.EMAIL));
+            userDbMap.replace(JsonKey.EMAIL, null);
+            userDbMap.replace(JsonKey.MASKED_EMAIL, null);
+            userDbMap.replace(JsonKey.EMAIL_VERIFIED, false);
+        }
     }
 
     private void nullifyPhone() {
-        userDbMap.replace(JsonKey.PREV_USED_PHONE, userDbMap.get(JsonKey.PHONE));
-        userDbMap.replace(JsonKey.PHONE, null);
-        userDbMap.replace(JsonKey.MASKED_PHONE, null);
-        userDbMap.replace(JsonKey.PHONE_VERIFIED,false);
+        if (isNullifyOperationValid((String) userDbMap.get(JsonKey.PHONE))) {
+            userDbMap.replace(JsonKey.PREV_USED_PHONE, userDbMap.get(JsonKey.PHONE));
+            userDbMap.replace(JsonKey.PHONE, null);
+            userDbMap.replace(JsonKey.MASKED_PHONE, null);
+            userDbMap.replace(JsonKey.PHONE_VERIFIED, false);
+        }
     }
 
     private Response updateUser() {
@@ -81,7 +87,7 @@ public class IdentifierFreeUpActor extends BaseActor {
         userDbMap = getUserById(id);
         Response response = processUserAttribute(identifiers);
         sender().tell(response, self());
-        ProjectLogger.log(String.format("%s:%s:USER MAP SUCCESSFULLY UPDATED IN CASSANDRA. WITH VALUES  %s", this.getClass().getSimpleName(), "freeUpUser", Collections.singleton(userDbMap.toString())));
+        ProjectLogger.log(String.format("%s:%s:USER MAP SUCCESSFULLY UPDATED IN CASSANDRA. WITH VALUES  %s", this.getClass().getSimpleName(), "freeUpUser", Collections.singleton(userDbMap.toString())), LoggerEnum.INFO.name());
         saveUserDetailsToEs();
     }
 
@@ -89,8 +95,11 @@ public class IdentifierFreeUpActor extends BaseActor {
         Request userUpdatedRequest = new Request();
         userUpdatedRequest.setOperation(ActorOperations.UPDATE_USER_INFO_ELASTIC.getValue());
         userUpdatedRequest.getRequest().put(JsonKey.ID, userDbMap.get(JsonKey.ID));
-        ProjectLogger.log(String.format("%s:%s:Trigger sync of user details to ES with user updated userMap %s", this.getClass().getSimpleName(), "saveUserDetailsToEs", Collections.singleton(userDbMap.toString())));
+        ProjectLogger.log(String.format("%s:%s:Trigger sync of user details to ES with user updated userMap %s", this.getClass().getSimpleName(), "saveUserDetailsToEs", Collections.singleton(userDbMap.toString())), LoggerEnum.INFO.name());
         tellToAnother(userUpdatedRequest);
     }
 
+    private boolean isNullifyOperationValid(String identifier){
+        return StringUtils.isNotBlank(identifier);
+    }
 }

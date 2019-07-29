ae46a187cb4cfb5ec7f501102677d5ea43f567b2
anmol2302 <anmolgupta23021997@gmail.com>
2019-07-29 12:07:30 +0530

    Issue#13803 resolved Pr comments and removed static initialization of UserHashMap in class Level

--- actors/user/src/main/java/org/sunbird/user/actors/IdentifierFreeUpActor.java
+++ actors/user/src/main/java/org/sunbird/user/actors/IdentifierFreeUpActor.java
@@ -16,8 +16,8 @@ import org.sunbird.common.responsecode.ResponseCode;
 import org.sunbird.helper.ServiceFactory;
 import org.sunbird.learner.util.Util;
 
+import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -26,11 +26,15 @@ import java.util.Map;
         tasks = {"freeUpUserIdentity"},
         asyncTasks = {}
 )
+
+/**
+ * this Actor class is being used to free Up used User Identifier
+ * for now it only free Up user Email, Phone.
+ */
 public class IdentifierFreeUpActor extends BaseActor {
 
     private CassandraOperation cassandraOperation = ServiceFactory.getInstance();
     private Util.DbInfo usrDbInfo = Util.dbInfoMap.get(JsonKey.USER_DB);
-    private Map<String, Object> userDbMap = new HashMap<>();
     public static final int FIRST_RECORD = 0;
 
     @Override
@@ -44,24 +48,26 @@ public class IdentifierFreeUpActor extends BaseActor {
         Response response = cassandraOperation.getRecordById(usrDbInfo.getKeySpace(), usrDbInfo.getTableName(), id);
         List<Map<String, Object>> responseList = (List) response.get(JsonKey.RESPONSE);
         if (CollectionUtils.isEmpty(responseList)) {
+            ProjectLogger.log(String.format("%s:%s:User not found with provided Id:%s", this.getClass().getSimpleName(), "getById", id), LoggerEnum.ERROR.name());
             ProjectCommonException.throwClientErrorException(ResponseCode.invalidUserId);
         }
         return responseList.get(FIRST_RECORD);
     }
 
-    private Response processUserAttribute(List<String> identifiers) {
+    private Response processUserAttribute(Map<String, Object> userDbMap, List<String> identifiers) {
 
-        identifiers.forEach(val -> {
-            if (val.equalsIgnoreCase(JsonKey.EMAIL)) {
-                nullifyEmail();
-            } else if (val.equalsIgnoreCase(JsonKey.PHONE)) {
-                nullifyPhone();
-            }
-        });
-        return updateUser();
+        if (identifiers.contains(JsonKey.EMAIL)) {
+            nullifyEmail(userDbMap);
+            ProjectLogger.log(String.format("%s:%s:Nullified Email. WITH ID  %s", this.getClass().getSimpleName(), "freeUpUserIdentifier", userDbMap.get(JsonKey.ID)), LoggerEnum.INFO.name());
+        }
+        if (identifiers.contains(JsonKey.PHONE)) {
+            nullifyPhone(userDbMap);
+            ProjectLogger.log(String.format("%s:%s:Nullified Phone. WITH ID  %s", this.getClass().getSimpleName(), "freeUpUserIdentifier", userDbMap.get(JsonKey.ID)), LoggerEnum.INFO.name());
+        }
+        return updateUser(userDbMap);
     }
 
-    private void nullifyEmail() {
+    private void nullifyEmail(Map<String, Object> userDbMap) {
         if (isNullifyOperationValid((String) userDbMap.get(JsonKey.EMAIL))) {
             userDbMap.replace(JsonKey.PREV_USED_EMAIL, userDbMap.get(JsonKey.EMAIL));
             userDbMap.replace(JsonKey.EMAIL, null);
@@ -70,37 +76,37 @@ public class IdentifierFreeUpActor extends BaseActor {
         }
     }
 
-    private void nullifyPhone() {
+    private void nullifyPhone(Map<String, Object> userDbMap) {
         if (isNullifyOperationValid((String) userDbMap.get(JsonKey.PHONE))) {
             userDbMap.replace(JsonKey.PREV_USED_PHONE, userDbMap.get(JsonKey.PHONE));
             userDbMap.replace(JsonKey.PHONE, null);
             userDbMap.replace(JsonKey.MASKED_PHONE, null);
             userDbMap.replace(JsonKey.PHONE_VERIFIED, false);
-            userDbMap.replace(JsonKey.COUNTRY_CODE,null);
+            userDbMap.replace(JsonKey.COUNTRY_CODE, null);
         }
     }
 
-    private Response updateUser() {
+    private Response updateUser(Map<String, Object> userDbMap) {
         return cassandraOperation.updateRecord(usrDbInfo.getKeySpace(), usrDbInfo.getTableName(), userDbMap);
     }
 
     private void freeUpUserIdentifier(String id, List<String> identifiers) {
-        userDbMap = getUserById(id);
-        Response response = processUserAttribute(identifiers);
+        Map<String, Object> userDbMap = getUserById(id);
+        Response response = processUserAttribute(userDbMap, identifiers);
         sender().tell(response, self());
-        ProjectLogger.log(String.format("%s:%s:USER MAP SUCCESSFULLY UPDATED IN CASSANDRA. WITH VALUES  %s", this.getClass().getSimpleName(), "freeUpUserIdentifier", Collections.singleton(userDbMap.toString())), LoggerEnum.INFO.name());
-        saveUserDetailsToEs();
+        ProjectLogger.log(String.format("%s:%s:USER MAP SUCCESSFULLY UPDATED IN CASSANDRA. WITH ID  %s", this.getClass().getSimpleName(), "freeUpUserIdentifier", userDbMap.get(JsonKey.ID)), LoggerEnum.INFO.name());
+        saveUserDetailsToEs(userDbMap);
     }
 
-    private void saveUserDetailsToEs() {
+    private void saveUserDetailsToEs(Map<String, Object> userDbMap) {
         Request userUpdatedRequest = new Request();
         userUpdatedRequest.setOperation(ActorOperations.UPDATE_USER_INFO_ELASTIC.getValue());
         userUpdatedRequest.getRequest().put(JsonKey.ID, userDbMap.get(JsonKey.ID));
-        ProjectLogger.log(String.format("%s:%s:Trigger sync of user details to ES with user updated userMap %s", this.getClass().getSimpleName(), "saveUserDetailsToEs", Collections.singleton(userDbMap.toString())), LoggerEnum.INFO.name());
+        ProjectLogger.log(String.format("%s:%s:Trigger for sync  user details to ES with user updated user Id:%s", this.getClass().getSimpleName(), "saveUserDetailsToEs", userDbMap.get(JsonKey.ID)), LoggerEnum.INFO.name());
         tellToAnother(userUpdatedRequest);
     }
 
-    private boolean isNullifyOperationValid(String identifier){
+    private boolean isNullifyOperationValid(String identifier) {
         return StringUtils.isNotBlank(identifier);
     }
 }

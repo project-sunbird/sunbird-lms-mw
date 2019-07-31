7ca65ebd85b229a3a006cc057b068f39e47505a4
anmol2302 <anmolgupta23021997@gmail.com>
2019-07-31 16:57:09 +0530

    Issue#SC-1253 added methods to encypt the data inside  filters and also lower the sunbird required fields

--- actors/common/src/main/java/org/sunbird/learner/actors/search/SearchHandlerActor.java
+++ actors/common/src/main/java/org/sunbird/learner/actors/search/SearchHandlerActor.java
@@ -86,6 +86,7 @@ public class SearchHandlerActor extends BaseActor {
           filterObjectType = EsType.organisation.getTypeName();
         }
       }
+      lowerAndEncryptORFilterData(searchQueryMap);
       SearchDTO searchDto = Util.createSearchDto(searchQueryMap);
       if (filterObjectType.equalsIgnoreCase(EsType.user.getTypeName())) {
         searchDto.setExcludedFields(Arrays.asList(ProjectUtil.excludes));
@@ -343,5 +344,20 @@ public class SearchHandlerActor extends BaseActor {
     map.put(JsonKey.PARAMS, params);
     map.put(JsonKey.TELEMETRY_EVENT_TYPE, "SEARCH");
     return map;
+  }
+  public void lowerAndEncryptORFilterData(Map<String, Object> searchQueryMap) throws Exception {
+    Map<String,Object>ORFilterMap= (Map<String, Object>) ((Map<String, Object>)(searchQueryMap.get(JsonKey.FILTERS))).get(JsonKey.ES_OR_OPERATION);
+    if(MapUtils.isNotEmpty(ORFilterMap)) {
+      Arrays.asList(
+              ProjectUtil.getConfigValue(JsonKey.SUNBIRD_API_REQUEST_LOWER_CASE_FIELDS).split(","))
+              .stream()
+              .forEach(
+                      field -> {
+                        if (StringUtils.isNotBlank((String) ORFilterMap.get(field))) {
+                          ORFilterMap.put(field, ((String) ORFilterMap.get(field)).toLowerCase());
+                        }
+                      });
+      UserUtility.encryptUserData(ORFilterMap);
+    }
   }
 }

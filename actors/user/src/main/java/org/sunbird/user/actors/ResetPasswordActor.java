37d5ba9459397c0f67ff52d94a7b9d54149ed006
amit <amit.kumar@tarento.com>
2019-07-31 11:53:08 +0530

    Issue #SB-14101 feat: added teletery for reset password api

--- actors/user/src/main/java/org/sunbird/user/actors/ResetPasswordActor.java
+++ actors/user/src/main/java/org/sunbird/user/actors/ResetPasswordActor.java
@@ -1,5 +1,8 @@
 package org.sunbird.user.actors;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import org.sunbird.actor.core.BaseActor;
 import org.sunbird.actor.router.ActorConfig;
 import org.sunbird.common.exception.ProjectCommonException;
@@ -7,10 +10,14 @@ import org.sunbird.common.models.response.Response;
 import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.models.util.LoggerEnum;
 import org.sunbird.common.models.util.ProjectLogger;
+import org.sunbird.common.models.util.TelemetryEnvKey;
+import org.sunbird.common.request.ExecutionContext;
 import org.sunbird.common.request.Request;
 import org.sunbird.common.responsecode.ResponseCode;
+import org.sunbird.learner.util.Util;
 import org.sunbird.services.sso.SSOManager;
 import org.sunbird.services.sso.SSOServiceFactory;
+import org.sunbird.telemetry.util.TelemetryUtil;
 
 /** This actor process the request for reset password. */
 @ActorConfig(
@@ -23,9 +30,13 @@ public class ResetPasswordActor extends BaseActor {
 
   @Override
   public void onReceive(Request request) throws Throwable {
+    // Generate Telemetry (initializing context)
+    Util.initializeContext(request, TelemetryEnvKey.USER);
+    ExecutionContext.setRequestId(request.getRequestId());
     String userId = (String) request.get(JsonKey.USER_ID);
     String password = (String) request.get(JsonKey.PASSWORD);
     resetPassword(userId, password);
+    generateTelemetry(request.getRequest());
   }
 
   private void resetPassword(String userId, String password) {
@@ -33,10 +44,22 @@ public class ResetPasswordActor extends BaseActor {
     if (ssoManager.updatePassword(userId, password)) {
       Response response = new Response();
       response.put(JsonKey.RESPONSE, JsonKey.SUCCESS);
+      sender().tell(response, self());
     } else {
       ProjectLogger.log(
           "ResetPasswordActor:resetPassword : reset password failed", LoggerEnum.INFO.name());
       ProjectCommonException.throwServerErrorException(ResponseCode.SERVER_ERROR);
     }
+  }
+
+  private void generateTelemetry(Map<String, Object> request) {
+    Map<String, Object> targetObject = null;
+    List<Map<String, Object>> correlatedObject = new ArrayList<>();
+    targetObject =
+        TelemetryUtil.generateTargetObject(
+            (String) request.get(JsonKey.USER_ID), TelemetryEnvKey.USER, JsonKey.UPDATE, null);
+    TelemetryUtil.generateCorrelatedObject(
+        (String) request.get(JsonKey.USER_ID), TelemetryEnvKey.USER, null, correlatedObject);
+    TelemetryUtil.telemetryProcessingCall(request, targetObject, correlatedObject);
   }
 }

01b62623d9f08d1bd9b83d9229a4ba7a552a31bc
Harikumar Palemkota <harip@ilimi.in>
2019-08-02 18:47:21 +0530

    Issue #SC-1258 certificate validation first cut

---
+++ actors/user/src/main/java/org/sunbird/user/actors/CertificateActor.java
@@ -0,0 +1,56 @@
+package org.sunbird.user.actors;
+
+import org.sunbird.cassandra.CassandraOperation;
+import org.sunbird.common.exception.ProjectCommonException;
+import org.sunbird.common.models.response.Response;
+import org.sunbird.common.models.util.*;
+import org.sunbird.common.request.Request;
+import org.sunbird.common.responsecode.ResponseCode;
+import org.sunbird.helper.ServiceFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CertificateActor extends UserBaseActor{
+
+  private CassandraOperation cassandraOperation = ServiceFactory.getInstance();
+  @Override
+  public void onReceive(Request request) throws Throwable {
+    if(request.getOperation().equals(ActorOperations.VALIDATE_CERTIFICATE.getValue())) {
+      getCertificate(request);
+    }
+
+  }
+
+  private void getCertificate(Request userRequest) {
+    Request request = (Request) userRequest.getRequest();
+    Map result = new HashMap<String, Object>();
+    String certificatedId = (String) request.get(JsonKey.CERT_ID);
+    String accessCode = (String) request.get(JsonKey.ACCESS_CODE);
+    Response response = cassandraOperation.getRecordById(JsonKey.SUNBIRD,JsonKey.USER_CERT,certificatedId);
+    Map<String, Object> record = response.getResult();
+    if(null != record && null != record.get(JsonKey.ACCESS_CODE)) {
+      if(accessCode.equals(record.get(JsonKey.ACCESS_CODE))) {
+        Map recordStore = (Map<String, Object>) record.get(JsonKey.STORE);
+        result.put("json",recordStore.get("json"));
+        result.put("pdf",recordStore.get("pdf"));
+        result.put(JsonKey.STATUS, JsonKey.SUCCESS);
+        response.put(JsonKey.RESULT, result);
+        // update mergee details in ES
+        sender().tell(response, self());
+      } else {
+        //Exception related column data
+      }
+    } else {
+      ProjectLogger.log(
+              "CertificateActor:getCertificate: access code is incorrect for certid : "
+                      + certificatedId,
+              LoggerEnum.ERROR.name());
+      throw new ProjectCommonException(
+              ResponseCode.accessCodeInvalid.getErrorCode(),
+              ResponseCode.accessCodeInvalid.getErrorMessage(),
+              ResponseCode.CLIENT_ERROR.getResponseCode());
+    }
+
+  }
+}

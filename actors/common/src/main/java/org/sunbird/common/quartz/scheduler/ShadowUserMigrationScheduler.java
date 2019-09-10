b4cd3f6eb67c1a32c00ea599915fdd84870e2f2e
anmol2302 <anmolgupta23021997@gmail.com>
2019-09-11 01:57:55 +0530

    Issue#SC-1323 STATUS MESSAGES ADDED

--- actors/common/src/main/java/org/sunbird/common/quartz/scheduler/ShadowUserMigrationScheduler.java
+++ actors/common/src/main/java/org/sunbird/common/quartz/scheduler/ShadowUserMigrationScheduler.java
@@ -45,42 +45,58 @@ public class ShadowUserMigrationScheduler {
 
 
     /**
-     *  - fetch rows from bulk upload table whose status is less than 2
-     *  - update the bulk upload table row status to processing
-     *  - start processing single row
-     *  - fetch the single row data
-     *  - Get List of Migration(csv) user from the row
-     *  - process each Migration(csv) user
-     *  - update the bulk upload table row status to completed
+     * - fetch rows from bulk upload table whose status is less than 2
+     * - update the bulk upload table row status to processing
+     * - start processing single row
+     * - fetch the single row data
+     * - Get List of Migration(csv) user from the row
+     * - process each Migration(csv) user
+     * - update the bulk upload table row status to completed and add message to success
+     * - if fails update the bulk upload row status to failed and add failureResult
      */
     public void processRecords() {
         List<Map<String, Object>> bulkDbRows = getRowsFromBulkUserDb();
         bulkDbRows.stream().forEach(row -> {
             BulkMigrationUser bulkMigrationUser = convertRowToObject(row);
-            updateStatusInUserBulkTable(bulkMigrationUser.getId(),ProjectUtil.BulkProcessStatus.IN_PROGRESS.getValue());
-            List<MigrationUser> migrationUserList = getMigrationUserAsList(bulkMigrationUser);
-            migrationUserList.stream().forEach(singleMigrationUser -> {
-                processSingleMigUser(bulkMigrationUser.getId(), singleMigrationUser);
-            });
-            updateStatusInUserBulkTable(bulkMigrationUser.getId(),ProjectUtil.BulkProcessStatus.COMPLETED.getValue());
+            try {
+                updateStatusInUserBulkTable(bulkMigrationUser.getId(), ProjectUtil.BulkProcessStatus.IN_PROGRESS.getValue());
+                List<MigrationUser> migrationUserList = getMigrationUserAsList(bulkMigrationUser);
+                migrationUserList.stream().forEach(singleMigrationUser -> {
+                    processSingleMigUser(bulkMigrationUser.getId(), singleMigrationUser);
+                });
+                updateMessageInBulkUserTable(bulkMigrationUser.getId(), JsonKey.SUCCESS_RESULT, JsonKey.SUCCESS);
+            } catch (Exception e) {
+                updateMessageInBulkUserTable(bulkMigrationUser.getId(), JsonKey.FAILURE_RESULT, e.getMessage());
+            }
         });
     }
+
     private void processSingleMigUser(String processId, MigrationUser singleMigrationUser) {
-        Map<String, Object> existingUserDetails = getShadowExistingUserDetails(singleMigrationUser.getChannel(), singleMigrationUser.getUserExternalId());
-        if (MapUtils.isEmpty(existingUserDetails)) {
-            insertShadowUserToDb(processId, singleMigrationUser);
-        } else {
-            ShadowUser shadowUser = mapper.convertValue(existingUserDetails, ShadowUser.class);
-            updateUser(singleMigrationUser, shadowUser);
-        } }
+        try {
+            Map<String, Object> existingUserDetails = getShadowExistingUserDetails(singleMigrationUser.getChannel(), singleMigrationUser.getUserExternalId());
+            if (MapUtils.isEmpty(existingUserDetails)) {
+                insertShadowUserToDb(processId, singleMigrationUser);
+            } else {
+                ShadowUser shadowUser = mapper.convertValue(existingUserDetails, ShadowUser.class);
+                updateUser(singleMigrationUser, shadowUser);
+            }
+        } catch (Exception e) {
+            ProjectLogger.log("ShadowUserMigrationScheduler:processSingleMigUser:error occurred while processing user".concat(e + "with user id:".concat(singleMigrationUser.toString())), LoggerEnum.ERROR.name());
+        }
+    }
 
 
+    /**
+     * this method will read rows from the bulk_upload_process table who has status less than 2
+     * @return list
+     */
     private List<Map<String, Object>> getRowsFromBulkUserDb() {
         Response response = cassandraOperation.getRecordWithCondition(bulkUploadDbInfo.getKeySpace(), bulkUploadDbInfo.getTableName(), JsonKey.STATUS, ProjectUtil.BulkProcessStatus.INTERRUPT.getValue());
-        List<Map<String, Object>> result = null;
+        List<Map<String, Object>> result = new ArrayList<>();
         if (!((List) response.getResult().get(JsonKey.RESPONSE)).isEmpty()) {
             result = ((List) response.getResult().get(JsonKey.RESPONSE));
         }
+        ProjectLogger.log("ShadowUserMigrationScheduler:getRowsFromBulkUserDb:got rows from Bulk user table is:".concat(result.size()+""),LoggerEnum.INFO.name());
         return result;
     }
 
@@ -142,7 +158,7 @@ public class ShadowUserMigrationScheduler {
                 .setUpdatedOn(null)
                 .build();
         Map<String, Object> dbMap = mapper.convertValue(shadowUser, Map.class);
-        dbMap.put(JsonKey.CREATED_ON,new Timestamp(System.currentTimeMillis()));
+        dbMap.put(JsonKey.CREATED_ON, new Timestamp(System.currentTimeMillis()));
         Response response = cassandraOperation.insertRecord(JsonKey.SUNBIRD, JsonKey.SHADOW_USER, dbMap);
         ProjectLogger.log("ShadowUserMigrationScheduler:insertShadowUser: record status in cassandra ".concat(response.getResult() + ""), LoggerEnum.INFO.name());
     }
@@ -162,13 +178,13 @@ public class ShadowUserMigrationScheduler {
         propertiesMap.put(JsonKey.ORG_EXT_ID, migrationUser.getOrgExternalId());
         propertiesMap.put(JsonKey.UPDATED_ON, new Timestamp(System.currentTimeMillis()));
         propertiesMap.put(JsonKey.CHANNEL, migrationUser.getChannel());
-        propertiesMap.put(JsonKey.ID,shadowUser.getId());
+        propertiesMap.put(JsonKey.ID, shadowUser.getId());
         updateUserInShadowDb(propertiesMap);
         updateUserInUserDb(migrationUser.getInputStatus(), shadowUser);
     }
 
-    private void updateUserInShadowDb(Map<String, Object> propertiesMap){
-        Response response=cassandraOperation.updateRecord(JsonKey.SUNBIRD, JsonKey.SHADOW_USER, propertiesMap);
+    private void updateUserInShadowDb(Map<String, Object> propertiesMap) {
+        Response response = cassandraOperation.updateRecord(JsonKey.SUNBIRD, JsonKey.SHADOW_USER, propertiesMap);
         ProjectLogger.log("ShadowUserMigrationScheduler:updateUserInShadowDb: record status in cassandra ".concat(response.getResult() + ""), LoggerEnum.INFO.name());
     }
 
@@ -195,7 +211,7 @@ public class ShadowUserMigrationScheduler {
             cassandraOperation.updateRecord(usrDbInfo.getKeySpace(), usrDbInfo.getTableName(), propertiesMap);
         } catch (Exception e) {
             e.printStackTrace();
-            ProjectLogger.log("ShadowUserMigrationScheduler:updateUserStatus: data failed to updates in cassandra  with userId:".concat(userId + "error:"+e), LoggerEnum.ERROR.name());
+            ProjectLogger.log("ShadowUserMigrationScheduler:updateUserStatus: data failed to updates in cassandra  with userId:".concat(userId + "error:" + e), LoggerEnum.ERROR.name());
         }
         syncUserToES(propertiesMap);
     }
@@ -219,10 +235,26 @@ public class ShadowUserMigrationScheduler {
             Map<String, Object> propertiesMap = new HashMap<>();
             propertiesMap.put(JsonKey.ID, processId);
             propertiesMap.put(JsonKey.STATUS, statusVal);
-            Response response = cassandraOperation.updateRecord(bulkUploadDbInfo.getKeySpace(), bulkUploadDbInfo.getTableName(), propertiesMap);
-            ProjectLogger.log("ShadowUserMigrationScheduler:updateStatusInUserBulkTable: status update result".concat(response.getResult() + ""), LoggerEnum.INFO.name());
+            updateBulkUserTable(propertiesMap);
         } catch (Exception e) {
             ProjectLogger.log("ShadowUserMigrationScheduler:updateStatusInUserBulkTable: status update failed".concat(e + ""), LoggerEnum.ERROR.name());
         }
+    }
+
+    private void updateBulkUserTable(Map<String, Object> propertiesMap) {
+        Response response = cassandraOperation.updateRecord(bulkUploadDbInfo.getKeySpace(), bulkUploadDbInfo.getTableName(), propertiesMap);
+        ProjectLogger.log("ShadowUserMigrationScheduler:updateBulkUserTable: status update result".concat(response.getResult() + ""), LoggerEnum.INFO.name());
+    }
+
+    private void updateMessageInBulkUserTable(String processId, String key, String value) {
+        Map<String, Object> propertiesMap = new HashMap<>();
+        propertiesMap.put(JsonKey.ID, processId);
+        propertiesMap.put(key, value);
+        if (value.equalsIgnoreCase(JsonKey.SUCCESS)) {
+            propertiesMap.put(JsonKey.STATUS, ProjectUtil.BulkProcessStatus.COMPLETED.getValue());
+        } else {
+            propertiesMap.put(JsonKey.STATUS, ProjectUtil.BulkProcessStatus.FAILED.getValue());
+        }
+        updateBulkUserTable(propertiesMap);
     }
 }

a24dc1fa61307b918ff2dde46225cf458d864486
Harikumar Palemkota <harip@ilimi.in>
2019-09-12 00:35:29 +0530

    Issue #SB-14537 replacing with constants

--- actors/common/src/main/java/org/sunbird/learner/util/UserFlagEnum.java
+++ actors/common/src/main/java/org/sunbird/learner/util/UserFlagEnum.java
@@ -1,14 +1,16 @@
 package org.sunbird.learner.util;
 
+import org.sunbird.common.models.util.JsonKey;
+
 /**
  * UserFlagEnum provides all the flags of user type
  * It contains flagtype and corresponding boolean value, reason for value is to provide flexibility of providing
  * either true or false and maintaining purpose.
  */
 public enum UserFlagEnum {
-  PHONE_VERIFIED("phoneVerified", true, 1),
-  EMAIL_VERIFIED("emailVerified", true, 2),
-  STATE_VALIDATED("isStateValidated", true, 4);
+  PHONE_VERIFIED(JsonKey.PHONE_VERIFIED, true, 1),
+  EMAIL_VERIFIED(JsonKey.EMAIL_VERIFIED, true, 2),
+  STATE_VALIDATED(JsonKey.STATE_VALIDATED, true, 4);
 
   private String userFlagType;
   private boolean flagEnabled;

2bfbc3883d3dc44d425e49c0fd77670c98d0cf07
anmol2302 <anmolgupta23021997@gmail.com>
2019-12-23 23:53:15 +0530

    reading data from ES

--- actors/user/src/main/java/org/sunbird/user/actors/UserProfileReadActor.java
+++ actors/user/src/main/java/org/sunbird/user/actors/UserProfileReadActor.java
@@ -45,8 +45,6 @@ import java.text.MessageFormat;
 import java.util.*;
 import java.util.Map.Entry;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 import static akka.pattern.Patterns.pipe;
@@ -882,32 +880,52 @@ public class UserProfileReadActor extends BaseActor {
   }
 
   private void checkUserExistence(Request request) {
-    Map<String,Object>searchMap=new HashMap<>();
-    String value=(String)request.get(JsonKey.VALUE);
+    Map<String, Object> searchMap = new HashMap<>();
+    String value = (String) request.get(JsonKey.VALUE);
     String encryptedValue = null;
+    long start = System.currentTimeMillis();
     try {
       encryptedValue = this.encryptionService.encryptData(StringUtils.lowerCase(value));
     } catch (Exception var11) {
       ProjectCommonException exception = new ProjectCommonException(ResponseCode.userDataEncryptionError.getErrorCode(), ResponseCode.userDataEncryptionError.getErrorMessage(), ResponseCode.SERVER_ERROR.getResponseCode());
       this.sender().tell(exception, this.self());
     }
-    searchMap.put((String) request.get(JsonKey.KEY),encryptedValue);
-    ProjectLogger.log("UserProfileReadActor:checkUserExistence: search map prepared "+searchMap,LoggerEnum.INFO.name());
-    CompletableFuture<Object>fut=CompletableFuture.supplyAsync(() -> {
-      Response response=cassandraOperation.getRecordsByProperties(JsonKey.SUNBIRD, JsonKey.USER, searchMap);
-      List<Map<String,Object>>respList=(List)response.get(JsonKey.RESPONSE);
-      Response resp=new Response();long size=respList.size();
-      if(size<=0) {
-        ProjectCommonException exception = new ProjectCommonException(ResponseCode.userNotFound.getErrorCode(),
-                ResponseCode.userNotFound.getErrorMessage(),
-                ResponseCode.RESOURCE_NOT_FOUND.getResponseCode());
-        return exception;
-      }
-      else {
-        resp.put(JsonKey.EXISTS, true);
-        return resp;
-      }
-    });
-    pipe(fut, getContext().dispatcher()).to(sender());
+    long end = System.currentTimeMillis();
+    System.out.println("time took for encryption:" + (end - start) + "");
+    searchMap.put((String) request.get(JsonKey.KEY), encryptedValue);
+    SearchDTO searchDTO=new SearchDTO();
+    searchDTO.getAdditionalProperties().put(JsonKey.FILTERS, searchMap);
+    ProjectLogger.log("UserProfileReadActor:checkUserExistence: search map prepared " + searchMap , LoggerEnum.INFO.name());
+    handleUserSearchAsyncRequest(EsType.user.getTypeName(),searchDTO);
+  }
+
+
+  private void handleUserSearchAsyncRequest(String indexType, SearchDTO searchDto) {
+    Future<Map<String, Object>> futureResponse = esUtil.search(searchDto, indexType);
+    Future<Object> response =
+            futureResponse.map(
+                    new Mapper<Map<String, Object>, Object>() {
+                      @Override
+                      public Object apply(Map<String, Object> responseMap) {
+                        ProjectLogger.log(
+                                "SearchHandlerActor:handleUserSearchAsyncRequest user search call ",
+                                LoggerEnum.INFO.name());
+                        Response response = new Response();
+                        List<Map<String, Object>> respList = (List) responseMap.get(JsonKey.CONTENT);
+                        long size = respList.size();
+                        if (size <= 0) {
+                          ProjectCommonException exception = new ProjectCommonException(ResponseCode.userNotFound.getErrorCode(),
+                                  ResponseCode.userNotFound.getErrorMessage(),
+                                  ResponseCode.RESOURCE_NOT_FOUND.getResponseCode());
+                          return exception;
+                        }
+                        else {
+                          response.put(JsonKey.EXISTS,true);
+                          return response;
+                        }
+                      }
+                    },
+                    getContext().dispatcher());
+    Patterns.pipe(response, getContext().dispatcher()).to(sender());
   }
 }
